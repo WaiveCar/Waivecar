e4703e103cec08a5fb104c100500c6f687520242
Matt Ginty <matt.ginty@gmail.com>
2015-06-17 00:14:29 +1000

    restructure app

---
+++ www/js/app/controllers/playlist-controller.js
@@ -0,0 +1,6 @@
+angular.module('app.controllers').controller('PlaylistController', [
+  '$scope',
+  '$stateParams',
+  function($scope, $stateParams) {
+  }
+]);
---
+++ www/js/app/main.js
@@ -0,0 +1,81 @@
+'use strict';
+
+angular.module('app.controllers', []);
+angular.module('app.directives', []);
+angular.module('app.filters', []);
+angular.module('app.providers', []);
+angular.module('app.services', []);
+
+angular.module('app', [
+  'ionic',
+  'app.controllers',
+  'app.services'
+])
+
+.run([
+  '$ionicPlatform',
+  function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  }
+])
+
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html"
+    })
+
+    .state('app.search', {
+      url: "/search",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/search.html"
+        }
+      }
+    })
+
+    .state('app.browse', {
+      url: "/browse",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/browse.html"
+        }
+      }
+    })
+      .state('app.playlists', {
+        url: "/playlists",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/playlists.html"
+          }
+        }
+      })
+
+    .state('app.single', {
+      url: "/playlists/:playlistId",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/playlist.html"
+        }
+      }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/playlists');
+  }
+]);
