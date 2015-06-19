7d71529aaca634d7b034be2141971fa20d679d66
Matt Ginty <matt.ginty@gmail.com>
2015-06-19 10:44:48 +1000

    tidy up

--- www/js/controllers/registration-controller.js
+++ www/js/controllers/registration-controller.js
@@ -2,6 +2,6 @@ angular.module('app.controllers').controller('RegistrationController', [
   '$scope',
   '$stateParams',
   function($scope, $stateParams) {
-    // will mainly call through to common SignupCOntrolle perhaps
+    'use strict';
   }
 ]);
