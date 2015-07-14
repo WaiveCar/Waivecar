74229f916c7cc3db80009f12456c204324751b44
Matt Ginty <matt.ginty@gmail.com>
2015-07-14 22:32:19 +1000

    restructure to be RESTful, etc.

---
+++ www/components/car-information/index.js
@@ -0,0 +1,22 @@
+function carInformationDirective(searchEvents, $state) {
+  function link(scope, element, attrs, ctrl){
+    if (!$state.params) {
+      return;
+    }
+    var details = $state.params.vehicleDetails;
+    scope.name = details.name;
+    scope.plate = details.plate;
+  }
+  return {
+    restrict: 'E',
+    link: link,
+    templateUrl: 'components/car-information/templates/index.html'
+  }
+}
+
+angular.module('app')
+.directive('carInformation', [
+  'searchEvents',
+  '$state',
+  carInformationDirective
+]);
