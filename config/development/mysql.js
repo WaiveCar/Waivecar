4cc06540e92bc83b4adbab758da2890b9849a004
Christoffer Rødvik <christoffer@kodemon.net>
2015-07-15 21:09:50 +0900

    fixed gitignore

---
+++ config/development/mysql.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+  /*
+   |--------------------------------------------------------------------------------
+   | MySql
+   |--------------------------------------------------------------------------------
+   |
+   | In our current version reach-api supports mysql out of the box, just add your
+   | database settings here and everything should boot up.
+   |
+   */
+
+  mysql : {
+    host     : 'localhost',
+    database : 'waivecar_development',
+    user     : 'waivecar',
+    password : 'eNwlGGl6g6V0w0qX3vx0S5GKbGvTtR3X'
+  }
+
+};
