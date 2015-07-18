fa6f008b0b70c4ed45b3481867f47c42870ce656
fabio <blackjackdevel@gmail.com>
2015-07-17 22:36:00 -0300

    Dashboard layout implemented

--- www/components/ride/index.js
+++ www/components/ride/index.js
@@ -1,1 +1,33 @@
+function RideController(){
+
+}
+function distanceTravelledDirective(){
+	function link(scope){
+		scope.value='100 miles'
+	}
+	return {
+		link:link,
+		template:'<span ng-bind="value"></span>',
+		scope:true
+	}
+}
+function batteryChargeDirective(){
+	function link(scope){
+		scope.value='65%'
+	}
+	return {
+		link:link,
+		template:'<span ng-bind="value"></span>',
+		scope:true
+	}
+}
 angular.module('app')
+.controller('RideController', [
+  RideController
+])
+.directive('batteryCharge', [
+  batteryChargeDirective
+])
+.directive('distanceTravelled', [
+  distanceTravelledDirective
+]);
