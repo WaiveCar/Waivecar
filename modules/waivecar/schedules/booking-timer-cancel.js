c110d82935a5591f2c6f781535e531f531a2650a
Christoffer Rødvik <christoffer@kodemon.net>
2015-07-25 03:51:39 +0900

    added schedule for auto cancelling bookings

---
+++ modules/waivecar/schedules/booking-timer-cancel.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let co      = require('co');
+let Booking = require('../lib/booking');
+let queue   = Reach.service('queue');
+let socket  = Reach.Socket;
+
+queue.process('booking-timer-cancel', function (job, done) {
+  co(function *() {
+    let id      = job.data.booking;
+    let user    = job.data.user;
+    let booking = yield Booking.getBooking(id, user);
+
+    yield Booking.setCarStatus('available', booking.carId, user);
+    yield booking.update({
+      state : 'cancelled'
+    });
+
+    socket.io.user(user.id).emit('booking:cancelled', booking.toJSON());
+
+    done();
+  });
+});
+
+module.exports = function *() {
+  yield queue.scheduler('booking-timer-cancel');
+};
