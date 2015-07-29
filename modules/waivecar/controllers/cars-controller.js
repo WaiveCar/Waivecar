060cccafbafce267b5acb75abf57dd173a0a114d
Christoffer Rødvik <christoffer@kodemon.net>
2015-07-30 02:12:01 +0900

    now adds location relation to the car list

--- modules/waivecar/controllers/cars-controller.js
+++ modules/waivecar/controllers/cars-controller.js
@@ -1,5 +1,9 @@
 'use strict';
 
+let Car            = Reach.model('Car');
+let CarLocation    = Reach.model('CarLocation');
+let CarDiagnostics = Reach.model('CarDiagnostics');
+
 module.exports = Reach.resource(function (_super) {
 
   Reach.extends(CarsController, _super);
@@ -11,6 +15,18 @@ module.exports = Reach.resource(function (_super) {
     _super.call(this, 'Car');
   }
 
- return CarsController;
+  /**
+   * @method index
+   * @param  {Object} options
+   */
+  CarsController.prototype.index = function *(options) {
+    let cars = yield Car.find(options);
+    if (cars) {
+      yield cars.hasOne(CarLocation, 'carId', 'location', ['longitude', 'latitude']);
+    }
+    return cars;
+  };
+
+  return CarsController;
 
 });
