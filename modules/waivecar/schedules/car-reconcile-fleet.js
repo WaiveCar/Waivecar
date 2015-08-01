ef6179a21cdafe9135380a76fb8c551378a3f112
Christoffer Rødvik <christoffer@kodemon.net>
2015-08-01 14:52:27 +0900

    updated job timers and added comments

--- modules/waivecar/schedules/car-reconcile-fleet.js
+++ modules/waivecar/schedules/car-reconcile-fleet.js
@@ -17,6 +17,11 @@ module.exports = function *() {
   });
 };
 
+// ### Car Reconcile Fleet
+// Pulls down fleet data from the GM API and upserts it into the local database.
+// This is done to ensure faster responses to client applications from our API.
+// NOTE! On a live build this job should run once or twice a day.
+
 scheduler.process('car-reconcile-fleet', function *(job) {
   log.info('Reconciling Car Fleet');
   let vehicles = yield service.listVehicles();
--- modules/waivecar/schedules/car-reconcile-location.js
+++ modules/waivecar/schedules/car-reconcile-location.js
@@ -10,13 +10,17 @@ module.exports = function *() {
   scheduler.add('car-reconcile-location', {
     init   : true,
     repeat : true,
-    silent : true,
+    // silent : true,
     timer  : {
-      value : 10,
-      type  : 'seconds'
+      value : 5,
+      type  : 'minutes'
     }
   });
 };
+
+// ### Car Reconcile Location
+// Loops through available cars in the database and updates their location randomly.
+// This is a mock job to simulate GM data.
 
 scheduler.process('car-reconcile-location', function *(job) {
   let cars = yield Car.find();
@@ -27,26 +31,26 @@ scheduler.process('car-reconcile-location', function *(job) {
   for (let i = 0, len = cars.length; i < len; i++) {
     let car          = cars[i];
     let prevLocation = car.location;
-    let coords       = prevLocation ? getLocation(prevLocation.latitude, prevLocation.longitude, 100) : getLocation(37.422292, -122.148153, 5000);
+    let coords       = prevLocation ? getRandomLocation(prevLocation.latitude, prevLocation.longitude, 100) : getRandomLocation(37.422292, -122.148153, 5000);
     let location     = new CarLocation({
       carId     : car.id,
       latitude  : coords[1].toFixed(8),
       longitude : coords[0].toFixed(8),
     });
     yield location.upsert();
-    if (!prevLocation) {
-      car.location = {};
-    }
-    car.location.latitude  = coords[1].toFixed(8);
-    car.location.longitude = coords[0].toFixed(8);
-    io.flux({
-      actionType : 'car:updated',
-      car        : car
-    });
   }
 });
 
-function getLocation(x0, y0, radius) {
+/**
+ * Generate a random location within the provided radius in meters.
+ * @private
+ * @method getRandomLocation
+ * @param  {Float} x0
+ * @param  {Float} y0
+ * @param  {Int}   radius
+ * @return {Array} [longitude, latitude]
+ */
+function getRandomLocation(x0, y0, radius) {
   let random          = Math.random();
   let radiusInDegrees = radius / 111300;
 
