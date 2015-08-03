7bc9aa03413b8186134964bd918429b0037c719a
Matt Ginty <matt.ginty@gmail.com>
2015-08-04 08:09:19 +1000

    rewrite directive to utilize data ref

--- www/modules/chargingStations/index.js
+++ www/modules/chargingStations/index.js
@@ -1,83 +1,65 @@
-function ChargingStationsService($rootScope, $q, locationService, $http) {
-  this.$q = $q;
-  this.locationService = locationService;
-  this.$http = $http;
-  this.$rootScope = $rootScope;
-}
-ChargingStationsService.prototype.getNearbyChargingStations = function(numNearby) {
-  var self = this;
-  return this.locationService.getLocation().then(
-            function(deviceLocation) { 
-              var ret = [];
-              numNearby = numNearby || 10;
-              var maxDiff = 0.005;
-              var idCount = 1;
-              var id=1;
-              var minDiff = 0.0005;
-              for (var i = 0; i < numNearby; i++) {
-                var diffA = Math.random() * (maxDiff - minDiff) + minDiff;
-                var diffB = Math.random() * (maxDiff - minDiff) + minDiff
-                if (Math.random() < .5) {
-                  diffA = diffA * -1;
-                }
-                if (Math.random() < .5) {
-                  diffB = diffB * -1;
-                }
-                ret.push(
-                        {
-                          latitude: deviceLocation.latitude + diffA,
-                          longitude: deviceLocation.longitude + diffB,
-                          name:'Station '+(id++),
-                          address:"St Street - 42"+id,
-                          distance:id*3
-                        }
-                    )
-              }
-              return ret;
-            }
-        );
-}
-function nearbyChargingStationsDirective(MapsLoader, $q, chargingStationsService){
+function nearbyChargingStationsDirective(MapsLoader, $q){
 	 function addMarkerClick(marker, info, onClickFn) {
     marker.on('mousedown', function(e) {
-      onClickFn({marker: marker, info: info});
+      onClickFn({ marker: marker, info: info });
     });
   }
-  
+
   function link(scope, element, attrs, ctrl) {
-    chargingStationsService.getNearbyChargingStations().then(function(stations) { 
+
+    scope.$watch('locations', function() {
+      if (!scope.locations || (_.isArray(scope.locations) && scope.locations.length === 0)) return;
+
       MapsLoader.getMap.then(function(L) {
+
+        var stationIcon = L.icon({
+          iconUrl       : 'img/charging-station.svg',
+          iconRetinaUrl : 'img/charging-station.svg',
+          iconSize      : [ 20, 25 ],
+          iconAnchor    : [ 10, 25 ],
+          popupAnchor   : [ 0 , 0 ]
+        });
+
         ctrl.mapInstance.then(function(mapInstance) {
-          var icon = L.icon({
-            iconUrl: 'img/charging-station.svg',
-            iconRetinaUrl: 'img/charging-station.svg',
-            iconSize: [40, 50],
-            iconAnchor: [20, 50],
-            popupAnchor: [0 , 0]
+
+          if (scope.group) {
+            console.log('locations: removing locations from map instance');
+            mapInstance.removeLayer(scope.group);
+            scope.markers.forEach(function(marker) {
+              self.mapInstance.removeLayer(marker);
+            });
+          }
+
+          scope.markers = [];
+          console.log('locations: adding locations to map instance');
+          scope.locations.forEach(function(location) {
+            // TODO: filter out on stations (if we add addtional location types).
+            var marker = L.marker([ location.latitude, location.longitude ], { icon: stationIcon }).addTo(mapInstance);
+            addMarkerClick(marker, location, scope.onClickMarker);
+            scope.markers.push(marker);
           });
-          var latLng;
-          var markers = [];
-          var marker;
-          stations.forEach(function(s) {
-            marker = L.marker([s.latitude, s.longitude], {icon: icon}).addTo(mapInstance);
-            addMarkerClick(marker, s, scope.onClickMarker);
-            markers.push(marker);
-          });
-        
+
+          if (scope.markers.length > 0) {
+            scope.group = new L.featureGroup(scope.markers);
+          }
         });
       });
-
-    });
+    }, true);
   }
+
   return {
-    restrict: 'E',
-    link: link,
-    require: '^map',
-    scope: {
-      onClickMarker: '&'
+    restrict : 'E',
+    link     : link,
+    require  : '^map',
+    scope    : {
+      locations     : '=',
+      onClickMarker : '&'
     }
   }
 }
-angular.module('ChargingStations', [])
-.service('chargingStationsService', ['$rootScope', '$q', 'locationService', '$http', ChargingStationsService])
-.directive('nearbyChargingStations', ['MapsLoader', '$q', 'chargingStationsService',nearbyChargingStationsDirective]);
+
+angular.module('ChargingStations', []).directive('nearbyChargingStations', [
+  'MapsLoader',
+  '$q',
+  nearbyChargingStationsDirective
+]);
