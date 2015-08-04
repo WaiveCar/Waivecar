ab260c0159fb7b242e858339b357f4b836bbd1b0
Christoffer Rødvik <christoffer@kodemon.net>
2015-08-05 00:10:23 +0900

    added more checks and successfull payment hook

--- config/payment/hooks/waivecar.js
+++ config/payment/hooks/waivecar.js
@@ -1,14 +1,16 @@
 'use strict';
 
-let payment = Reach.module('payment');
-let Booking = Reach.model('Booking');
-let error   = Reach.ErrorHandler;
+let PaymentModule = Reach.module('payment');
+let Booking       = Reach.model('Booking');
+let Payment       = Reach.model('Payment');
+let error         = Reach.ErrorHandler;
 
 /**
  * @hook waivecar:before
  * @param {Object} data The hook data provided for payment
+ * @param {User}   user The user attempting to perform the payment
  */
-payment.hook('waivecar:before', function *(data) {
+PaymentModule.hook('waivecar:before', function *(data, user) {
   let booking = yield Booking.find(data.bookingId);
   if (!booking) {
     throw error.parse({
@@ -22,7 +24,22 @@ payment.hook('waivecar:before', function *(data) {
       message : 'You cannot submit a payment to a booking that is not pending payment'
     }, 409);
   }
-  throw error.parse({}, 501);
+  if (booking.customerId !== user.id) {
+    throw error.parse({
+      code    : 'BOOKING_INVALID_USER',
+      message : 'You cannot submit payments belonging to another customer'
+    }, 409);
+  }
+  if (booking.paymentId) {
+    let payment = yield Payment.find(booking.paymentId);
+    if (payment) {
+      throw error.parse({
+        code     : 'PAYMENT_INVALID',
+        message  : 'The payment for this booking has already been processed',
+        solution : 'Send a request to /payments/'+ payment.id + ' to view payment details'
+      }, 409);
+    }
+  }
 });
 
 /**
@@ -30,6 +47,9 @@ payment.hook('waivecar:before', function *(data) {
  * @param {Object} data   The hook data provided for payment
  * @param {Object} charge The stripe charge object
  */
-payment.hook('waivecar', function *(data, charge) {
-  
+PaymentModule.hook('waivecar', function *(data, charge) {
+  let booking       = yield Booking.find(data.bookingId);
+  booking.paymentId = charge.id;
+  booking.state     = 'completed';
+  yield booking.update();
 });
