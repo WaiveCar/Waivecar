3e58a3af897524e3535b84148f8fa90c9c39ea64
fabio <blackjackdevel@gmail.com>
2015-08-03 22:02:45 -0300

    Made camera work with android

--- www/modules/camera/camera.js
+++ www/modules/camera/camera.js
@@ -1,20 +1,25 @@
-function CameraService($cordovaCamera){
+function CameraService($cordovaCamera,$q){
 	this.$cordovaCamera=$cordovaCamera;
+	this.$q=$q;
 }
-CameraService.prototype.getPicture = function() {
+CameraService.prototype.getPicture = function(width,height) {
+	if(typeof Camera =='undefined'){
+		this.$q.reject("This feature just works on mobile");
+	}
 	var options = {
 		quality : 75,
 		destinationType : Camera.DestinationType.DATA_URL,
 		sourceType : Camera.PictureSourceType.CAMERA,
-		allowEdit : true,
 		encodingType: Camera.EncodingType.JPEG,
-		targetWidth: 100,
-		targetHeight: 100,
+		targetWidth: width || 800,
+		targetHeight: height || 800,
 		saveToPhotoAlbum: false,
 		correctOrientation: true,
 		cameraDirection : Camera.Direction.BACK,
 	}
-	return  this.$cordovaCamera.getPicture(options);
+	return  this.$cordovaCamera.getPicture(options).then(function(picture){
+		return "data:image/jpeg;base64,"+picture;
+	})
 };
 angular.module('Camera',[])
-.service('CameraService',['$cordovaCamera',CameraService]);
+.service('CameraService',['$cordovaCamera','$q',CameraService]);
