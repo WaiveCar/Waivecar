e7f952de07cdf9d0cc286811c559ef76756a8be9
fabio <blackjackdevel@gmail.com>
2015-08-03 22:02:45 -0300

    Made camera work with android

--- www/components/cars/damage.js
+++ www/components/cars/damage.js
@@ -13,36 +13,40 @@ function takePictureDirective(){
 }
 function pictureContainerDirective(){
 	var link=function(scope, element, attrs, ctrl){
-		// var img=element.find('img');
-		// scope.getSetPictureFunction(function(){
-		// 	return function(imgData){
-
-		// 	}
-		// })
-		console.log(scope.damageImage);
+		var img=element.find('img')[0];
+		scope.getSetPictureFunction({setFn: function(base64Img){
+				img.setAttribute('src',base64Img);
+			}
+		});
 	};
 	return {
 		link:link,
 		restrict:'E',
    		templateUrl : 'components/cars/templates/directives/pictureContainer.html',
    		scope:{
-   			// 'getSetPictureFunction': '&'
-   			'damageImage':'@'
+   			//We are passing the function to the controller, setting ng-src as a scope parameter did not work.
+   			'getSetPictureFunction': '&'
+   			// 'damageImage':'@'
    		}
+
 	}
 }
-function DamageController($scope,CameraService){
+function DamageController($rootScope,$scope,CameraService){
 	this.$scope=$scope;
+	this.$rootScope=$rootScope;
 	this.CameraService=CameraService;
 }
+DamageController.prototype.setPicture = function(setFn) {
+	this.setPictureFunction=setFn;
+};
 DamageController.prototype.takePictureOfDamage = function() {
-	console.log('HERE');
 	var self=this;
-	this.CameraService.getPicture.then(function(picture){
-		self.damageImage=picture;
+
+	this.CameraService.getPicture().then(function(picture){
+		self.setPictureFunction(picture);
 	})
 };
 angular.module('app')
-.controller('damageController',['$scope','CameraService',DamageController])
+.controller('damageController',['$rootScope','$scope','CameraService',DamageController])
 .directive('pictureContainer',[pictureContainerDirective])
 .directive('takePicture',[takePictureDirective]);
