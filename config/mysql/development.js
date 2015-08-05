4d1a3be1becb93dcdfb76d5d89bc4defec925d34
Christoffer Rødvik <christoffer@kodemon.net>
2015-08-06 01:41:12 +0900

    temporary database reset

--- config/mysql/development.js
+++ config/mysql/development.js
@@ -16,7 +16,7 @@ module.exports = {
     database : 'waivecar_development',
     user     : 'waivecar',
     password : 'eNwlGGl6g6V0w0qX3vx0S5GKbGvTtR3X',
-    force    : false
+    force    : true
   }
 
 };
