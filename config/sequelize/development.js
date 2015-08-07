62ee1e6b74b1f0b40a7349a096d60137d141dd6e
Christoffer Rødvik <christoffer@kodemon.net>
2015-08-08 01:35:36 +0900

    and database -.-

--- config/sequelize/development.js
+++ config/sequelize/development.js
@@ -21,7 +21,7 @@ module.exports = {
    */
 
   sequelize : {
-    database : 'waivecar_dev',
+    database : 'waivecar_development',
     username : 'waivecar',
     password : 'eNwlGGl6g6V0w0qX3vx0S5GKbGvTtR3X'
   }
