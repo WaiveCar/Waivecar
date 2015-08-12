16e3c0d6da8b78dca35c12b3d5d6a8dcc6636a60
fabio <blackjackdevel@gmail.com>
2015-08-12 10:19:20 -0300

    Changed state to use a constant

--- www/app.js
+++ www/app.js
@@ -14,168 +14,15 @@ function Run($ionicPlatform,$rootScope,stateService) {
   });
 }
 
-function Config($stateProvider, $urlRouterProvider, $ionicConfigProvider, $httpProvider,$compileProvider) {
+function Config($stateProvider, $urlRouterProvider, $ionicConfigProvider, $httpProvider,$compileProvider,appStates) {
   'use strict';
   $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
   $httpProvider.interceptors.push('AuthInterceptor');
   $ionicConfigProvider.views.transition('platform');
-   $stateProvider
-    // FIND WAIVECARS
-    .state('fleet', {
-      url: '/fleet',
-      templateUrl: '/components/cars/templates/fleet.html'
-    })
-    // SHOW WAIVECAR
-    .state('cars-show', {
-      url: '/cars/:id',
-      templateUrl: '/components/cars/templates/show.html'
-    })
-    .state('users-new', {
-      url         : '/users/new',
-      templateUrl : '/components/users/templates/new.html',
-      params      : {
-        redirectUrl    : null,
-        redirectParams : null
-      }
-    })
-    .state('users-show', {
-      url: '/users/:id',
-      templateUrl: '/components/users/templates/show.html'
-    })
-    .state('credit-cards', {
-      url         : '/users/cards',
-      templateUrl : '/components/credit-cards/templates/index.html',
-      params      : {
-        redirectUrl    : null,
-        redirectParams : null
-      }
-    })
-    .state('credit-cards-new', {
-      url         : '/users/cards/new',
-      templateUrl : '/components/credit-cards/templates/new.html',
-      params      : {
-        redirectUrl    : null,
-        redirectParams : null
-      }
-    })
-    .state('licenses', {
-      url         : '/users/licenses',
-      templateUrl : '/components/licenses/templates/index.html',
-      params      : {
-        redirectUrl    : null,
-        redirectParams : null
-      }
-    })
-    .state('licenses-new', {
-      url         : '/users/licenses/new',
-      templateUrl : '/components/licenses/templates/new.html',
-      params      : {
-        redirectUrl    : null,
-        redirectParams : null
-      }
-    })
-    //Connect to car
-    .state('cars-connect', {
-      url: '/cars/connect/:id',
-      templateUrl: '/components/cars/templates/connect.html'
-    })
-    .state('cars-connecting', {
-      url: '/cars/connecting/:id',
-      templateUrl: '/components/cars/templates/connecting.html'
-    })
-    // .state('cars-edit', {
-    //   url: '/cars/:id/edit',
-    //   templateUrl: '/components/cars/templates/edit.html'
-    // })
-    // PAST RIDES
-    .state('bookings', {
-      url: '/bookings',
-      templateUrl: '/components/bookings/templates/index.html',
-      params      : {
-        redirectUrl    : null,
-        redirectParams : null
-      }
-    })
-    .state('bookings-new', {
-      url: '/bookings/new',
-      templateUrl: '/components/bookings/templates/new.html',
-      params      : {
-        redirectUrl    : null,
-        redirectParams : null
-      }
-    })
-    // GET TO YOUR CAR
-    .state('bookings-show', {
-      url: '/bookings/:id',
-      templateUrl: '/components/bookings/templates/show.html',
-      params : { vehicleDetails: null}
-    })
-    .state('bookings-edit', {
-      url: '/bookings/:id/edit',
-      templateUrl: '/components/bookings/templates/edit.html',
-      params : { vehicleDetails: null}
-    })
-    .state('vision', {
-      url: '/vision',
-      templateUrl: '/components/vision/templates/index.html'
-    })
-    .state('contact', {
-      url: '/contact',
-      templateUrl: '/components/contact/templates/index.html'
-    })
-    .state('points', {
-      url: '/my-points',
-      templateUrl: '/components/users/templates/reward-points.html'
-    })
-    .state('dashboard',{
-      url:'/ride/dashboard',
-      templateUrl: '/components/ride/templates/dashboard.html'
-    })
-    .state('summary',{
-      url:'/ride/summary',
-      templateUrl: '/components/ride/templates/summary.html'
-    })
-    .state('paid-ride',{
-      url:'/paid-ride',
-      templateUrl: '/components/ride/templates/paidDashboard.html'
-    })
-   .state('ride-end',{
-      url:'/ride/end',
-      templateUrl: '/components/ride/templates/endRide.html'
-    })
-    .state('ride-end-low-charge',{
-      url:'/ride/end/low-charge',
-      templateUrl: '/components/ride/templates/endRideLowCharge.html'
-    })
-    .state('ride-alert-low-battery',{
-      url:'/ride/alert/low-battery',
-      templateUrl: '/components/ride/templates/lowBatteryAlert.html'
-    })
-    .state('ride-alert-free-ride',{
-        url:'/ride/alert/free-ride',
-        templateUrl: '/components/ride/templates/freeRideAlert.html'
-    })
-    //All purposes ads
-    .state('ads',{
-      url:'/ads',
-      templateUrl: '/components/ads/templates/index.html',
-      params:{redirectUrl:null,redirectParams:null}
-    })
-    //Errors
-    .state('location-error', {
-      url: '/location-error',
-      templateUrl: '/components/errors/templates/index.html'
-    })
-    .state('unplugged-error', {
-      url: '/unplugged-error',
-      templateUrl: '/components/errors/templates/index.html'
-    })
-    .state('car-damage', {
-      url: '/car/damage',
-      templateUrl: '/components/cars/templates/damage.html'
-    });
+  for(var state in appStates){
+    $stateProvider.state(state,appStates[state]);
+  }
   $urlRouterProvider.otherwise('/fleet');
- 
 }
 
 angular.module('app', [
@@ -197,6 +44,129 @@ angular.module('app', [
   'WaiveCar.state.rules',
   'WaiveCar.state'
 ])
+.constant('appStates',{
+    // FIND WAIVECARS
+    'fleet':{
+      url: '/fleet',
+      templateUrl: '/components/cars/templates/fleet.html'
+    },
+    // SHOW WAIVECAR
+    'cars-show':{
+      url: '/cars/:id',
+      templateUrl: '/components/cars/templates/show.html'
+    },
+    'users-new':{
+      url         : '/users/new',
+      templateUrl : '/components/users/templates/new.html'
+    },
+    'users-show':{
+      url: '/users/:id',
+      templateUrl: '/components/users/templates/show.html'
+    },
+    'credit-cards':{
+      url         : '/users/cards',
+      templateUrl : '/components/credit-cards/templates/index.html'
+    },
+    'credit-cards-new':{
+      url         : '/users/cards/new',
+      templateUrl : '/components/credit-cards/templates/new.html'
+    },
+    'licenses':{
+      url         : '/users/licenses',
+      templateUrl : '/components/licenses/templates/index.html'
+    },
+    'licenses-new':{
+      url         : '/users/licenses/new',
+      templateUrl : '/components/licenses/templates/new.html'
+    },
+    //Connect to car
+    'cars-connect':{
+      url: '/cars/connect/:id',
+      templateUrl: '/components/cars/templates/connect.html'
+    },
+    'cars-connecting':{
+      url: '/cars/connecting/:id',
+      templateUrl: '/components/cars/templates/connecting.html'
+    },
+
+    'bookings':{
+      url: '/bookings',
+      templateUrl: '/components/bookings/templates/index.html'
+    },
+    'bookings-new':{
+      url: '/bookings/new',
+      templateUrl: '/components/bookings/templates/new.html'
+    },
+    // GET TO YOUR CAR
+    'bookings-show':{
+      url: '/bookings/:id',
+      templateUrl: '/components/bookings/templates/show.html',
+      params : { vehicleDetails: null},
+    },
+    'bookings-edit':{
+      url: '/bookings/:id/edit',
+      templateUrl: '/components/bookings/templates/edit.html'
+    },
+    'vision':{
+      url: '/vision',
+      templateUrl: '/components/vision/templates/index.html'
+    },
+    'contact':{
+      url: '/contact',
+      templateUrl: '/components/contact/templates/index.html'
+    },
+    'points':{
+      url: '/my-points',
+      templateUrl: '/components/users/templates/reward-points.html'
+    },
+    'dashboard':{
+      url:'/ride/dashboard',
+      templateUrl: '/components/ride/templates/dashboard.html'
+    },
+    'summary':{
+      url:'/ride/summary',
+      templateUrl: '/components/ride/templates/summary.html'
+    },
+    'paid-ride':{
+      url:'/paid-ride',
+      templateUrl: '/components/ride/templates/paidDashboard.html'
+    },
+   'ride-end':{
+      url:'/ride/end',
+      templateUrl: '/components/ride/templates/endRide.html'
+    },
+    'ride-end-low-charge':{
+      url:'/ride/end/low-charge',
+      templateUrl: '/components/ride/templates/endRideLowCharge.html'
+    },
+    'ride-alert-low-battery':{
+      url:'/ride/alert/low-battery',
+      templateUrl: '/components/ride/templates/lowBatteryAlert.html'
+    },
+    'ride-alert-free-ride':{
+        url:'/ride/alert/free-ride',
+        templateUrl: '/components/ride/templates/freeRideAlert.html'
+    },
+    //All purposes ads
+    'ads':{
+      url:'/ads',
+      templateUrl: '/components/ads/templates/index.html',
+      params:{redirectUrl:null,redirectParams:null},
+    },
+    'location-error':{
+      url: '/location-error',
+      templateUrl: '/components/errors/templates/index.html'
+    },
+    'unplugged-error':{
+      url: '/unplugged-error',
+      templateUrl: '/components/errors/templates/index.html'
+    },
+    'car-damage':{
+      url: '/car/damage',
+      templateUrl: '/components/cars/templates/damage.html'
+    }
+
+})
 // .directive('overlayDialog',dialogDirective)
-.config([ '$stateProvider', '$urlRouterProvider', '$ionicConfigProvider', '$httpProvider','$compileProvider', Config ])
+.config([ '$stateProvider', '$urlRouterProvider', '$ionicConfigProvider', '$httpProvider','$compileProvider','appStates',Config ])
 .run(['$ionicPlatform','$rootScope','WaiveCarStateService',Run]);
