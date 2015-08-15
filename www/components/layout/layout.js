f4f4e49b03ea18a652a8362e9e788ed643a9a7ec
fabio <blackjackdevel@gmail.com>
2015-08-14 21:13:47 -0300

    Added step control on current step

--- www/components/layout/layout.js
+++ www/components/layout/layout.js
@@ -28,9 +28,24 @@ function headerBarDirective(layoutIcons){
 	}
 }
 function stepByStepDirective(){
+	function link(scope){
+		if(typeof scope.currentStep=='undefined' || scope.currentStep<=0){
+			scope.currentStep=1;
+		}
+		if(scope.currentStep>4){
+			scope.currentStep=4;
+		}
+		for(var i=0;i<scope.currentStep;i++){
+			scope['step'+(i+1)]='passed';
+		}
+	}
 	return {
 		restrict:'E',
 		replace: true,
+		link:link,
+		scope:{
+			currentStep:'@',
+		},
 		templateUrl:'/components/layout/templates/directives/stepByStep.html'
 	}
 }
