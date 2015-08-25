5bfabdeebbaebdc7956ace21176e71c02ef70887
Christoffer Rødvik <christoffer@kodemon.net>
2015-08-25 22:21:45 +0900

    facebook development keys

---
+++ config/auth/development.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  /*
+   |--------------------------------------------------------------------------------
+   | Facebook
+   |--------------------------------------------------------------------------------
+   |
+   | facebook : object > The facebook authentication settings
+   |   - appId     : string > The facebook appId
+   |   - appSecret : string > The facebook appSecret
+   |
+   */
+
+  auth : {
+    facebook : {
+      appId     : '1022708504415468',
+      appSecret : '1112617da837aa59b2f096ca9e813dd3'
+    }
+  }
+
+};
