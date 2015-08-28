2136c12d8d8e8a21bc5401be5b52fa85dceb000c
Christoffer Rødvik <christoffer@kodemon.net>
2015-08-28 18:25:24 +0900

    moved logout to auth module

--- src/modules/auth/index.js
+++ src/modules/auth/index.js
@@ -1,14 +1,22 @@
 'use strict';
 
+import Reach    from 'reach-react';
 import policies from 'interface/policies';
 
 export default {
   component   : require('./layout'),
   childRoutes : [
     {
-      path      : 'login',
+      path      : '/login',
       onEnter   : policies.isAnonymous,
       component : require('./views/login')
+    },
+    {
+      path      : '/logout',
+      onEnter   : function (nextState, transition) {
+        Reach.Auth.logout();
+        transition.to('/', null);
+      }
     }
   ]
 };
