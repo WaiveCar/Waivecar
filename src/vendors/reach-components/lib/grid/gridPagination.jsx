223643c927d33deb7b4d66b87f3403d58cdb0b9f
Matt Ginty <matt.ginty@gmail.com>
2015-08-30 15:12:08 +1000

    support large page counts

--- src/vendors/reach-components/lib/grid/gridPagination.jsx
+++ src/vendors/reach-components/lib/grid/gridPagination.jsx
@@ -1,88 +1,98 @@
-/*
-   See License / Disclaimer https://raw.githubusercontent.com/DynamicTyped/Griddle/master/LICENSE
-*/
 var React = require('react');
 var _ = require('lodash');
 
-//needs props maxPage, currentPage, nextFunction, prevFunction
 var GridPagination = React.createClass({
-    getDefaultProps: function(){
-        return{
-            "maxPage": 0,
-            "nextText": "",
-            "previousText": "",
-            "currentPage": 0,
-            "useGriddleStyles": true,
-            "nextClassName": "griddle-next",
-            "previousClassName": "griddle-previous",
-            "nextIconComponent": null,
-            "previousIconComponent": null
-        }
-    },
-    pageChange: function(event){
-        this.props.setPage(parseInt(event.target.value, 10) -1);
-    },
 
-    render: function(){
-        let prevClass = 'btn btn-icon';
-        let nextClass = 'btn btn-icon';
-        let prevIsDisabled = false;
-        let nextIsDisabled = false;
-
-        if (this.props.currentPage < 1) {
-            prevClass += ' btn-disabled';
-            prevIsDisabled = true;
-        }
-
-        if (this.props.currentPage === (this.props.maxPage -1)) {
-            nextClass += ' btn-disabled';
-            nextIsDisabled = true;
-        }
-
-        let prev = <button type="button" className={ prevClass } disabled={ prevIsDisabled } onClick={this.props.previous}>
-                 <i className="material-icons">chevron_left</i>
-               </button>
-
-        let next = <button type="button" className={ nextClass } disabled={ nextIsDisabled } onClick={this.props.next}>
-                 <i className="material-icons">chevron_right</i>
-               </button>
-
-        var leftStyle = null;
-        var middleStyle = null;
-        var rightStyle = null;
-
-        if(this.props.useGriddleStyles === true){
-            var baseStyle = {
-                "float": "left",
-                minHeight: "1px",
-                marginTop: "5px"
-            };
-
-            rightStyle = _.extend({textAlign:"right", width: "34%"}, baseStyle);
-            middleStyle = _.extend({textAlign:"center", width: "33%"}, baseStyle);
-            leftStyle = _.extend({ width: "33%"}, baseStyle)
-        }
-
-        var options = [];
-
-        for(var i = 1; i<= this.props.maxPage; i++){
-            let className = 'btn btn-icon';
-            if (i === this.props.currentPage + 1) {
-                className += ' btn-primary';
-            }
-            options.push(<button className={ className } onClick={this.pageChange} value={i} key={i}>{i}</button>);
-        }
-
-        return (
-            <div style={this.props.useGriddleStyles ? { minHeight: "35px" } : null }>
-                <div className="griddle-page" style={middleStyle}>
-                    { prev }
-                    { options }
-                    { next}
-                </div>
-            </div>
-        )
+  getDefaultProps : function(){
+    return {
+      "maxPage": 0,
+      "nextText": "",
+      "previousText": "",
+      "currentPage": 0,
+      "useGriddleStyles": true,
+      "nextClassName": "griddle-next",
+      "previousClassName": "griddle-previous",
+      "nextIconComponent": null,
+      "previousIconComponent": null
     }
-})
+  },
+
+  pageChange : function(event){
+    this.props.setPage(parseInt(event.target.value, 10) -1);
+  },
+
+  renderButton : function(type, action, icon, value) {
+    let className  = 'btn btn-icon';
+    let isDisabled = false;
+
+    switch (type) {
+      case 'first': {
+        isDisabled = this.props.currentPage === 0;
+        break;
+      }
+      case 'last' : {
+        isDisabled = (this.props.currentPage + 1) === this.props.maxPage;
+        break;
+      }
+      case 'prev' : {
+        isDisabled = this.props.currentPage < 1;
+        break;
+      }
+      case 'next' : {
+        isDisabled = this.props.currentPage === (this.props.maxPage -1);
+        break;
+      }
+    }
+
+    icon = icon || 'more_horiz';
+    if (isDisabled) className += ' btn-disabled';
+
+    return (
+      <button type="button" className={ className } disabled={ isDisabled } value={ value } onClick={ action }>
+        <i className="material-icons">{ icon }</i>
+      </button>
+    );
+  },
+
+  render: function() {
+    let options = [];
+
+    for (var i = 1; i <= this.props.maxPage; i++) {
+      let className = 'btn btn-icon';
+      let min = 0;
+      let max = 6;
+      let current = this.props.currentPage + 1;
+
+      if (current > 3) {
+        min = current - 3;
+        max = current + 3;
+      }
+
+      if (i === this.props.currentPage + 1) {
+        className += ' btn-primary';
+      }
+
+      if (this.props.maxPage > 5) {
+        if (i > min && i < max) {
+          options.push(<button className={ className } onClick={this.pageChange} value={i} key={i}>{i}</button>);
+        }
+      } else {
+          options.push(<button className={ className } onClick={this.pageChange} value={i} key={i}>{i}</button>);
+      }
+    }
+
+    return (
+      <div>
+        <div className="griddle-page">
+          { this.renderButton('first', this.pageChange, 'more_horiz', 1) }
+          { this.renderButton('prev', this.props.previous, 'chevron_left') }
+          { options }
+          { this.renderButton('next', this.props.next, 'chevron_right') }
+          { this.renderButton('last', this.pageChange, 'more_horiz', this.props.maxPage) }
+        </div>
+      </div>
+    )
+  }
+});
 
 module.exports = GridPagination;
