aeb81774bd4bacb2126d9ced80b7fc12c61eccd0
Matt Ginty <matt.ginty@gmail.com>
2015-09-01 00:30:01 +1000

    reduce chart height

--- src/vendors/reach-components/lib/charts/lib/chart.jsx
+++ src/vendors/reach-components/lib/charts/lib/chart.jsx
@@ -26,14 +26,14 @@ export default class Chart extends React.Component {
     let width = this.props.width > 0 ? this.props.width - 28 : 0; // we know there is 15px padding, and we want 1px less. (x2)
 
     return (
-      <section className="card card-body-table">
+      <section className="card card-body-chart">
         <div className="card-header">
           <h2>{ this.props.title }</h2>
         </div>
         <div className="card-body">
           <div className={ chartContainerClass }>
             <div className={ chartClass }>
-              <Sparklines data={ this.props.data } width={ width } height={ 352 } key={ width }>
+              <Sparklines data={ this.props.data } width={ width } height={ 152 } key={ width }>
                 { lineRender }
               </Sparklines>
             </div>
