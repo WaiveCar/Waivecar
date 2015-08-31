492d717ed9febac0d9460f05d66333cc1eeb75d2
Matt Ginty <matt.ginty@gmail.com>
2015-08-31 12:50:21 +1000

    new bg

--- src/modules/auth/layout.jsx
+++ src/modules/auth/layout.jsx
@@ -6,7 +6,7 @@ export default class AuthLayout {
   render() {
     return (
       <div id="auth">
-        <video autoPlay className="bg-vid" loop>
+        <video autoPlay className="bg-vid" loop poster="/images/auth/login.jpg">
           <source src="/images/auth/login.webm" type="video/webm" />
           <source src="/images/auth/login.mp4" type="video/mp4" />
         </video>
