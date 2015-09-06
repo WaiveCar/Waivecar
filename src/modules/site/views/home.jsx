ba23f6074d030f570fb3731c77bc5e2bb8555cb6
Matt Ginty <matt.ginty@gmail.com>
2015-09-06 19:51:20 +1000

    revert home to design

--- src/modules/site/views/home.jsx
+++ src/modules/site/views/home.jsx
@@ -62,11 +62,6 @@ export default class HomeView extends React.Component {
     return (
       <Container isFluid={ true }>
         <Row id="download" className="section section-1 inverted">
-          <video autoPlay className="bg-vid" loop poster="/images/auth/login.jpg">
-            <source src="/images/auth/login.webm" type="video/webm" />
-            <source src="/images/auth/login.mp4" type="video/mp4" />
-          </video>
-          <div className="vid-overlay"></div>
           <Column>
             { this.renderNavigation() }
             <Row>
