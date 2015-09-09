f74a53ef2ad83c5460031f9d2974f3e495485399
Adnan Ibrisimbegovic <adnan@clevertech.biz>
2015-09-09 10:46:15 +0200

    Refactored license module and camera service

---
+++ www/js/services/camera-service.js
@@ -0,0 +1,97 @@
+angular.module('app.services').factory('CameraService', [
+  '$cordovaCamera',
+  '$q',
+  '$config',
+  '$window',
+  function ($cordovaCamera, $q, $config, $window) {
+    'use strict';
+
+    var fileUploadURL = $config.uri.api + '/files/local';
+
+    function getPicture(width, height, upload) {
+      console.log('$window.Camera', JSON.stringify(window.Camera));
+      // console.log('navigator.Camera', window.Camera, navigator.Camera, navigator.camera, window.cordova);
+
+      if (!$window.Camera) {
+        return $q.reject('This feature works only on mobile');
+      }
+
+      var sourceType = $window.Camera.PictureSourceType.CAMERA;
+      if (upload) {
+        sourceType = $window.Camera.PictureSourceType.PHOTOLIBRARY;
+      }
+
+      var options = {
+        quality: 75,
+        destinationType: $window.Camera.DestinationType.FILE_URI,
+        sourceType: sourceType,
+        encodingType: $window.Camera.EncodingType.JPEG,
+        targetWidth: width || 800,
+        targetHeight: height || 800,
+        saveToPhotoAlbum: true,
+        correctOrientation: true,
+        cameraDirection: $window.Camera.Direction.BACK
+      };
+
+      return $cordovaCamera.getPicture(options);
+
+    }
+
+    function uploadFile(width, height) {
+      return getPicture(width, height, true);
+    }
+
+    function savePicture(fileUri) {
+      var options = new $window.FileUploadOptions();
+      options.headers = {
+        Connection: 'close'
+      };
+      options.fileKey = 'file';
+      // options.fileName = fileUri.substr(fileUri.lastIndexOf('/') + 1);
+      options.fileName = 'license';
+      options.mimeType = 'image/jpeg';
+      options.withCredentials = true;
+
+      // fileUri = $window.FileEntry.toURL(fileUri);
+      // console.log('Uploading', fileUri, 'with', JSON.stringify(options), fileUploadURL);
+
+      var ft = new $window.FileTransfer();
+
+      var defered = $q.defer();
+
+      var successCb = function (response) {
+        console.log('File upload success', JSON.stringify(response));
+        return $cordovaCamera.cleanup()
+          .then(function () {
+            defered.resolve(response);
+          });
+
+      };
+
+      var errorCb = function (response) {
+        console.log('File upload error', JSON.stringify(response));
+        defered.reject(response);
+      };
+
+    // window.resolveLocalFileSystemURL(fileUri, function (fileEntry) {
+    //   console.log('fileEntry', JSON.stringify(fileEntry));
+    //   var url = fileEntry.toURL();
+    //   console.log('url', url);
+
+    //   url = url.replace('/storage/emulated/0', '/sdcard');
+
+    // }, defered.reject);
+      ft.upload(fileUri, encodeURI(fileUploadURL), successCb, errorCb, options, true);
+
+      return defered.promise;
+
+    }
+
+    return {
+      getPicture: getPicture,
+      savePicture: savePicture,
+      uploadFile: uploadFile
+    };
+
+  }
+]);
--- www/js/services/socket-service.js
+++ www/js/services/socket-service.js
@@ -1,45 +1,51 @@
 angular.module('app')
-.factory('$socket', [
-  '$config',
-  'socketFactory',
-  function ($config, socketFactory) {
-    var remote;
-    if ($config.uri.api.indexOf('localhost') > 0) {
-      remote = io.connect('http://localhost:5000');
-    } else {
-      remote = io.connect($config.uri.api, { path: '/socket/socket.io' });
+  .factory('$socket', [
+    '$config',
+    'socketFactory',
+    function ($config, socketFactory) {
+      'use strict';
+
+      var remote;
+
+      if ($config.uri.api.indexOf('localhost') > 0) {
+        remote = io.connect('http://localhost:5000');
+      } else {
+        remote = io.connect($config.uri.api, {
+          path: '/socket/socket.io'
+        });
+      }
+
+      console.log('connecting to ' + $config.uri.api);
+
+      var socket = socketFactory({
+        ioSocket: remote
+      });
+
+      // socket.disconnect();
+
+      socket.forward('error');
+
+      socket.on('subscribed', function () {
+        console.log('subscribed');
+      });
+
+      socket.on('unsubscribed', function () {
+        console.log('unsubscribed');
+      });
+
+      socket.on('event', function () {
+        console.log('event');
+      });
+
+      socket.on('connect', function () {
+        console.log('connected');
+      });
+
+      socket.on('disconnect', function () {
+        console.log('disconnected');
+      });
+
+      return socket;
+
     }
-
-    console.log('connecting to ' +  $config.uri.api);
-
-    var socket = socketFactory({
-      ioSocket: remote
-    });
-
-    socket.forward('error');
-
-    socket.on('subscribed', function() {
-      console.log('subscribed');
-    });
-
-    socket.on('unsubscribed', function() {
-      console.log('unsubscribed');
-    });
-
-    socket.on('event', function() {
-      console.log('event');
-    });
-
-    socket.on('connect', function() {
-      console.log('connected');
-    });
-
-    socket.on('disconnect', function() {
-      console.log('disconnected');
-    });
-
-    return socket;
-
-  }
-]);
-
+  ]);
