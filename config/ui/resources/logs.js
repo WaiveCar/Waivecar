c6fba3f7bea299b6c23b0ac92f1a064194e37419
Matt Ginty <matt.ginty@gmail.com>
2015-09-14 14:23:02 +1000

    ui - logs is named log (lacks plural for route)

--- config/ui/resources/logs.js
+++ config/ui/resources/logs.js
@@ -2,29 +2,14 @@
 
 module.exports = {
   name  : 'logs',
-  store : {
-    key    : 'log',
-    method : 'POST',
-    uri    : '/logs'
-  },
   index : {
     key    : 'logs',
     method : 'GET',
-    uri    : '/logs'
-  },
-  show : {
-    key    : 'log',
-    method : 'GET',
-    uri    : '/logs/:id'
-  },
-  update : {
-    key    : 'log',
-    method : 'PUT',
-    uri    : '/logs/:id'
+    uri    : '/log'
   },
   delete : {
     key    : 'log',
     method : 'DELETE',
-    uri    : '/logs/:id'
+    uri    : '/log/:id'
   }
 };
