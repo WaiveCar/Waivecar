7769196b01ea77eb76733fc1a658bcc664f10b86
Christoffer Rødvik <christoffer@kodemon.net>
2015-09-16 04:28:36 +0900

    Refactored policies

--- interface/policies.js
+++ interface/policies.js
@@ -6,10 +6,7 @@ let error    = Reach.Error;
 /* istanbul ignore next: reach-api does not include modules utilizing these features */
 
 module.exports = function *() {
-
-  // ### Authenticate
-  // Authenticates the incoming request before allowing access to the route
-
+  
   yield register.policy('authenticate', function *() {
     if (!this.auth.check()) {
       throw error.parse({
@@ -27,10 +24,5 @@ module.exports = function *() {
       }, 401);
     }
   });
-
-  // ### Custom Policies
-  // Add your custom policies, usually modules will register their own
-  // policies if needed but there should be no issue registering your
-  // policies in the interface.
 
 };
