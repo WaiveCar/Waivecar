a07b48e5b22be5dbd2a7d2d23f5c00e346e39d33
Christoffer Rødvik <christoffer@kodemon.net>
2015-09-22 02:31:48 +0900

    reach-mongo added

---
+++ config/mongo/default.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+  /*
+   |--------------------------------------------------------------------------------
+   | Mongo
+   |--------------------------------------------------------------------------------
+   |
+   | host     : String > The host address to your mongodb instance.
+   | port     : Number > The port number for you mongodb instance.
+   | database : String > The database name for your mongodb instance.
+   |
+   */
+
+  mongo : {
+    host     : 'localhost',
+    port     : 27017,
+    database : null
+  }
+
+};
