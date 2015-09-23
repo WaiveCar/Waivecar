423a8c156cbaa4002d496794034aa4d3367fa59a
Christoffer Rødvik <christoffer@kodemon.net>
2015-09-24 02:37:24 +0900

    keys are no longer used

--- config/user/resources/users.js
+++ config/user/resources/users.js
@@ -1,26 +1,21 @@
 module.exports = {
   store : {
-    key    : 'user',
     method : 'POST',
     uri    : '/users'
   },
   index : {
-    key    : 'users',
     method : 'GET',
     uri    : '/users'
   },
   show : {
-    key    : 'user',
     method : 'GET',
     uri    : '/users/:id'
   },
   update : {
-    key    : 'user',
     method : 'PUT',
     uri    : '/users/:id'
   },
   delete : {
-    key    : 'user',
     method : 'DELETE',
     uri    : '/users/:id'
   }
