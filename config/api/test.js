94ab25ed72f6012c9ca46e737c14b7ed94281f3a
Christoffer Rødvik <christoffer@kodemon.net>
2015-09-29 03:23:54 +0900

    0.4.20 Authentication Updates
     - Authentication core now supports `Role` header.

--- config/api/test.js
+++ config/api/test.js
@@ -1,38 +1,11 @@
 'use strict';
 
 module.exports = {
-
-  /*
-   |--------------------------------------------------------------------------------
-   | API Settings
-   |--------------------------------------------------------------------------------
-   |
-   | The various API settings that define how your API reacts to incoming requests.
-   |
-   | name    : The name of your API service
-   | version : The version of your API service
-   | port    : The port on which to run the API service
-   | uri     : The full url address including port to your API service
-   | cors    : Origins and headers allowed to access the API service
-   | sites   : The application type of incoming requests
-   |
-   */
-
   api : {
     port : 8082,
     uri  : 'http://localhost:8082'
   },
-
-  /*
-   |--------------------------------------------------------------------------------
-   | Log Settings
-   |--------------------------------------------------------------------------------
-   |
-   | This API by default uses winston to log events, here you can define at what
-   | error levels the winston adapters should trigger a logging event.
-   |
-   */
-
+  
   log : {
     level : {
       console : 'info',
@@ -40,13 +13,7 @@ module.exports = {
       email   : 'ignore'
     }
   },
-
-  /*
-   |--------------------------------------------------------------------------------
-   | Test
-   |--------------------------------------------------------------------------------
-   */
-
+  
   test : {
     custom : [
       'interface/models/user'
@@ -62,5 +29,4 @@ module.exports = {
       'sms'
     ]
   }
-
 };
