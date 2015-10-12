5898107167197c6600c21a7aac68d451c0a11430
Christoffer Rødvik <christoffer@kodemon.net>
2015-10-13 06:38:19 +0900

    Added user-verification provider that works between two modules.

---
+++ providers/user-verification/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+let queue  = Reach.provider('queue');
+let tokens = Reach.provider('token');
+let config = Reach.config;
+
+// ### Require Verification Jobs
+
+require('./jobs/request-email-verification');
+require('./jobs/request-phone-verification');
+
+module.exports = class UserVerification {
+
+  /**
+   * Creates and sends a new phone verification request.
+   * @param  {Mixed}  id
+   * @param  {String} phone
+   * @return {Void}
+   */
+  static *requestPhoneVerification(id, phone) {
+    let token = yield tokens.create({
+      user        : id,
+      purpose     : 'phone-verification',
+      tokenType   : 'base10',
+      tokenLength : 6
+    });
+
+    let job = queue.create('sms:user:request-phone-verification', {
+      to      : phone,
+      message : `WaiveCar: Your verification code is ${ token }. Do not reply by SMS.`
+    }).save();
+
+    job.on('complete', () => {
+      job.remove();
+    });
+  }
+
+  /**
+   * Creates and sends a new email verification request.
+   * @param  {Mixed}  id
+   * @param  {String} email
+   * @param  {String} name
+   * @return {Void}
+   */
+  static *requestEmailVerification(id, email, name) {
+    let token = yield tokens.create({
+      user        : id,
+      purpose     : 'email-verification',
+      tokenType   : 'base10',
+      tokenLength : 6
+    });
+
+    let job = queue.create('email:user:request-email-verification', {
+      to       : email,
+      from     : config.email.sender,
+      subject  : 'Email Verificaton Required',
+      template : 'request-email-verification',
+      context  : {
+        name    : name,
+        token   : token,
+        company : config.api.name,
+        confirm : `${ config.api.uri }/users/verify`
+      }
+    }).save();
+
+    job.on('complete', () => {
+      job.remove();
+    });
+  }
+
+}
