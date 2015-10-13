4a3d2e4547c0104c56f19b9937d20285244774cd
Christoffer Rødvik <christoffer@kodemon.net>
2015-10-13 20:29:41 +0900

    Configuration adjustments, added production configuration.

--- config/api/default.js
+++ config/api/default.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = {
 
   /*
@@ -20,7 +18,7 @@ module.exports = {
 
   api : {
     name    : 'WaiveCar',
-    version : '0.4.0',
+    version : '0.5.3',
     port    : 8081,
     uri     : 'http://localhost:8081',
     cors    : {
@@ -58,33 +56,6 @@ module.exports = {
   support : {
     email : 'info@waivecar.com',
     phone : '555 55 555'
-  },
-
-  /*
-   |--------------------------------------------------------------------------------
-   | SSL Settings
-   |--------------------------------------------------------------------------------
-   |
-   | If you are running your server using HTTPS you will need to set up your
-   | certificates. Add your certifications to the server and add the paths in the
-   | ssl settings.
-   |
-   | Both the socket and server will make use of these settings.
-   |
-   */
-
-  ssl : {
-    active : false, // If set to false the server will run in http
-    certs  : {
-      path : 'path/to/ssl',
-      key  : 'certification.key',
-      cert : 'certification.crt',
-      ca   : [
-        'certification.crt',
-        'certification.crt',
-        'certification.crt'
-      ]
-    }
   },
 
   /*
--- config/commander/hooks/sequelize.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let commander = Reach.module('commander');
-let sequelize = Reach.provider('sequelize/commander');
-
-commander.hook('sequelize', function *(command) {
-  return yield sequelize(command);
-});
