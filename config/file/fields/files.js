6acd3ef6ecdb367442b7d287a8d7f11c033ddd7d
Matt Ginty <matt.ginty@gmail.com>
2015-10-15 16:21:00 +1000

    file ui config

---
+++ config/file/fields/files.js
@@ -0,0 +1,18 @@
+module.exports = {
+  id : {
+    name      : 'id',
+    component : 'input',
+    type      : 'text',
+    required  : true,
+    label     : 'Id',
+    helpText  : null
+  },
+  path : {
+    name      : 'path',
+    component : 'input',
+    type      : 'text',
+    required  : true,
+    label     : 'Path',
+    helpText  : null
+  }
+};
