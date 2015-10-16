d1a99b5cf43e4939884e7dee9d7262d52469d266
Christoffer Rødvik <christoffer@kodemon.net>
2015-10-16 21:12:20 +0900

    File sandbox adjustments

--- src/views/sandbox/upload.jsx
+++ src/views/sandbox/upload.jsx
@@ -6,16 +6,51 @@ import { Dropzone } from 'reach-components';
 import config       from 'config';
 
 export default class SandboxUpload extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      images : []
+    }
+  }
+
+  componentDidMount() {
+    this.images();
+  }
+
+  images() {
+    api.get('/files', (err, res) => {
+      if (err) {
+        return console.log(err);
+      }
+      this.setState({
+        images : res
+      });
+    }.bind(this));
+  }
+
   render() {
     return (
       <div className="container">
+        <h3 style={{ margin : '30px 0' }}>Files</h3>
+        <div className="clearfix">
+        {
+          this.state.images.map((file, key) => {
+            return (
+              <div
+                key   = { key }
+                style = {{
+                  background : `url('http://local.io:8081/files/${ file.id }') center center / cover`,
+                  float      : 'left',
+                  height     : 100,
+                  width      : 100
+                }}
+              />
+            )
+          })
+        }
+        </div>
         <h3 style={{ margin : '30px 0' }}>DropZone</h3>
-        <Dropzone
-          options = {{
-            url         : `${ config.api.uri }:${ config.api.port }/files`,
-            maxFilesize : 2
-          }}
-        />
         <Dropzone
           options = {{
             url         : `${ config.api.uri }:${ config.api.port }/files`,
@@ -25,4 +60,5 @@ export default class SandboxUpload extends React.Component {
       </div>
     );
   }
+
 }
