8a19a11a42804214770c657787aac7376c69ab36
Christoffer Rødvik <christoffer@kodemon.net>
2015-10-20 02:10:07 +0900

    Added default actions to dynamic resources

--- src/modules/reach-ui/lib/resources.js
+++ src/modules/reach-ui/lib/resources.js
@@ -66,31 +66,113 @@ Resources.getSelectList = function (targets) {
  * @param  {String} id
  */
 Resources.prepare = function (id) {
+
+  // ### Resource Reducer
+
   relay.resource(id, (state = [], action) => {
     switch (action.type) {
-      case 'store':
-        return [
-          ...state,
-          action.data
-        ];
-      case 'index':
-        return action.data;
-      case 'update':
-        return state.map(function (obj) {
-          if (obj.id === action.data.id) {
-            return action.data;
-          }
-          return obj;
-        });
-      case 'delete':
-        return state.map(function (obj) {
-          if (obj.id === action.data.id) {
-            return;
-          }
-          return obj;
-        });
-      default:
-        return state;
+      case 'store'  : return reduserStore(state, action.data);
+      case 'index'  : return action.data;
+      case 'update' : return reducerUpdate(state, action.data);
+      case 'delete' : return reducerDelete(state, action.data);
+      default       : return state;
     }
   });
+
+  // ### Resource Actions
+
+  relay.actions(id, {
+    
+    /**
+     * Adds the provided data to the state.
+     * @param {Object} data
+     */
+    store: (data) => {
+      relay.dispatch(id, {
+        type : 'store',
+        data : data
+      });
+    },
+
+    /**
+     * Adds the entire index to the reducer.
+     * @param {Object} data
+     */
+    index: (data) => {
+      relay.dispatch(id, {
+        type : 'index',
+        data : data
+      });
+    },
+
+    /**
+     * Updates a record in the current state.
+     * @param {Object} data
+     */
+    update: (data) => {
+      relay.dispatch(id, {
+        type : 'update',
+        data : data
+      });
+    },
+
+    /**
+     * Removes a record from the current state.
+     * @param {Object} data
+     */
+    delete: (data) => {
+      relay.dispatch(id, {
+        type : 'delete',
+        data : data
+      });
+    }
+
+  });
+
 };
+
+// ### Relay Reducer Actions
+
+/**
+ * Returns state with provided data.
+ * @param  {Array}  state
+ * @param  {Object} data
+ * @return {Array}
+ */
+function reducerStore(state, data) {
+  return [
+    ...state,
+    ...data
+  ]
+}
+
+/**
+ * Returns a new array with updated data in update position.
+ * @param  {Array}  state
+ * @param  {Object} data
+ * @return {Array}
+ */
+function reducerUpdate(state, data) {
+  return state.map(function (val) {
+    if (val.id === data.id) {
+      return data;
+    }
+    return val;
+  });
+}
+
+/**
+ * Returns a new result array with the provided data removed.
+ * @param  {Array}  state
+ * @param  {Object} data
+ * @return {Array}
+ */
+function reducerDelete(state, data) {
+  let result = [];
+  for (let i = 0, len = state.length; i < len; i++) {
+    if (state.id !== data.id) {
+      result.push(state[i]);
+    }
+  }
+  return result;
+}
