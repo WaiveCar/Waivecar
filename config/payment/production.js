f71ce87b43652a6af0c0d2224c8337a044e76b09
Matt Ginty <matt.ginty@gmail.com>
2015-10-21 16:38:18 +1000

    WaiveCar - Payment - Stripe Account

--- config/payment/production.js
+++ config/payment/production.js
@@ -8,8 +8,8 @@ module.exports = {
 
   payment : {
     stripe : {
-      secret : 'sk_test_n9MTqk5eeeQeqwN19XVnTjhN',
-      pub    : 'pk_test_zO3lYXra7dNIcI4JyFBitshk'
+      secret : 'sk_live_cJmUPQAyZcQG67pnUEH81Bi5',
+      pub    : 'pk_live_aT8u3UGOje5ryCk1Q0R9rleK'
     }
   }
 
