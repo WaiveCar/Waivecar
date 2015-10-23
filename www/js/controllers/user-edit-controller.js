2ba865b43dbf661e85980c3a4c319d4f5099f63b
Adnan Ibrisimbegovic <adnan@clevertech.biz>
2015-10-23 12:31:04 +0200

    Cleaned up user edit ctrl

--- www/js/controllers/user-edit-controller.js
+++ www/js/controllers/user-edit-controller.js
@@ -15,79 +15,23 @@ module.exports = angular.module('app.controllers').controller('UserEditControlle
   '$data',
   'FaceBookService',
   '$message',
-  'ezfb',
-  '$stateParams',
-  function($rootScope, $scope, $state, $auth, $data, FaceBookService, $message, ezfb, $stateParams) {
+  function($rootScope, $scope, $state, $auth, $data, FaceBookService, $message) {
 
     $scope.save = function(form) {
       if (form.$invalid) {
         return $message.error('Please fix form errors and try again.');
       }
 
-      var identifier = $scope.user.email;
-      var pass = $scope.user.password;
       $scope.user.$save()
         .then(function() {
-          if ($auth.isAuthenticated()) {
-            return $message.success('Saved!');
-          }
-
-          return $auth.login({
-              identifier: identifier,
-              password: pass
-            })
-            .then(function() {
-              return $state.go('licenses-photo-new', {
-                step: 3
-              });
-            });
-
+          return $message.success('Saved!');
         })
         .catch($message.error);
 
     };
 
-
-    $scope.registerWithFacebook = function() {
-      return ezfb.getLoginStatus()
-        .then(function(response){
-          if (response.status !== 'connected') {
-            return ezfb.login();
-          }
-          return response;
-
-        })
-        .then(function(res) {
-          if (res.status === 'connected') {
-            // return $auth.registerWithFacebook(res.authResponse);
-            return $state.go('users-new-facebook', {
-              authResponse: res.authResponse
-            });
-          }
-        })
-        // .then(function() {
-        //   return $state.go('licenses-photo-new', {
-        //     step: 3
-        //   });
-
-        // })
-        .catch($message.error);
-
-    };
 
     $scope.init = function() {
-
-      if (!$state.params.id) {
-        $scope.user = new $data.resources.users();
-
-        if($stateParams.authResponse){
-          $scope.user.firstName = $stateParams.authResponse.firstName;
-          $scope.user.lastName = $stateParams.authResponse.lastName;
-          $scope.user.email = $stateParams.authResponse.email;
-        }
-
-        return false;
-      }
 
       return $data.resources.users.get({
           id: $state.params.id
