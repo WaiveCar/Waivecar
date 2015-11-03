939a5e0bc9cf4b088047a301d2fca0ce9925b6ee
Matt Ginty <matt.ginty@gmail.com>
2015-11-03 20:40:34 +1000

    utilize common errors for auth hooks

--- hooks/auth/index.js
+++ hooks/auth/index.js
@@ -4,17 +4,14 @@ let bcrypt = Bento.provider('bcrypt');
 let User   = Bento.model('User');
 let auth   = Bento.Auth;
 let hooks  = Bento.Hooks;
-let error  = Bento.Error;
+let errors = require('./errors');
 
 // ### User Hook
 
 hooks.set('auth:user', function *(id, group) {
   let user = yield User.findById(id);
   if (!user) {
-    throw error.parse({
-      type    : `AUTH_INVALID_USER`,
-      message : `The token provided belongs to a user that is no longer accessible.`
-    }, 400);
+    errors.invalidUser();
   }
   return user;
 });
@@ -24,18 +21,12 @@ hooks.set('auth:user', function *(id, group) {
 hooks.set('auth:login', function *(identifier, password, options) {
   let user = yield User.findOne({ where : { email : identifier }});
   if (!user) {
-    throw error.parse({
-      type    : 'AUTH_INVALID_CREDENTIALS',
-      message : 'The email and/or password provided is invalid.'
-    }, 400);
+    errors.invalidCredentials();
   }
 
   let validPassword = yield bcrypt.compare(password, user.password);
   if (!validPassword) {
-    throw error.parse({
-      type    : 'AUTH_INVALID_CREDENTIALS',
-      message : 'The email and/or password provided is invalid.'
-    }, 400);
+    errors.invalidCredentials();
   }
 
   user       = user.toJSON();
