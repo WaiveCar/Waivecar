212f2f7097dd761ab01a5d27c70e2ef2a3d6ec62
Christoffer Rødvik <christoffer@kodemon.net>
2015-11-13 12:27:17 +0900

    Added required parameters to licenses post route.

--- modules/license/routes.js
+++ modules/license/routes.js
@@ -1,6 +1,11 @@
 'use strict';
 
-Route.pst('/licenses',     [ 'authenticate', 'LicensesController@store' ]);
+Route.post('/licenses', {
+  policy :  [ 'authenticate' ],
+  uses   : 'LicensesController@store',
+  params : [ 'number', 'firstName', 'lastName', 'birthDate', 'country', 'state' ]
+});
+
 Route.get('/licenses',     [ 'authenticate', 'LicensesController@index' ]);
 Route.get('/licenses/:id', [ 'authenticate', 'LicensesController@show' ]);
 Route.put('/licenses/:id', [ 'authenticate', 'LicensesController@update' ]);
