ceb1e3b6d443f80637317f9dedfb9c29b2f12642
Matt Ginty <matt.ginty@gmail.com>
2015-11-14 16:14:12 +1000

    prod key for checkr

---
+++ config/license/production.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  /*
+   |--------------------------------------------------------------------------------
+   | License
+   |--------------------------------------------------------------------------------
+   |
+   | The various API settings that define how your API reacts to incoming requests.
+   |
+   | @param {Array}  services A list of available license verification services.
+   | @param {Object} checkr   Checkr configuration.
+   |
+   */
+
+  license : {
+    checkr : {
+      key : '68f0b1098f2d0b2e99ca67bee946df9b365155b8'
+    }
+  }
+
+};
