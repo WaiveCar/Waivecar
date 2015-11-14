e3dc710fb7473dd2c16e1847265bd40c1631782f
Matt Ginty <matt.ginty@gmail.com>
2015-11-14 16:13:05 +1000

    test key for checkr

--- config/license/default.js
+++ config/license/default.js
@@ -5,11 +5,18 @@ module.exports = {
    | License
    |--------------------------------------------------------------------------------
    |
-   | @param {Object} ui The bentojs UI configuration.
+   | The various API settings that define how your API reacts to incoming requests.
+   |
+   | @param {Array}  services A list of available license verification services.
+   | @param {Object} checkr   Checkr configuration.
    |
    */
 
   license : {
+    services : [ 'checkr' ],
+    checkr   : {
+      key : '84aaba691da7f191f5922c470a666d372d6d8b3b'
+    },
     ui : {
       resources : {
         licenses : require('./resources/licenses')
@@ -21,4 +28,3 @@ module.exports = {
   }
 
 };
-
