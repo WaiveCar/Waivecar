874da29cf1cc0e7527739c6e66b995d847061049
Christoffer Rødvik <christoffer@kodemon.net>
2015-11-23 19:04:55 +0900

    Added bento paths to socket instance

--- socket.js
+++ socket.js
@@ -4,6 +4,24 @@ let path   = require('path');
 let server = require('bentojs-socket');
 let config = require('bentojs-config');
 
+// ### Bento
+// Expose bento on the global scope.
+
+GLOBAL.Bento = module.exports = {};
+
+// ### Paths
+// Absolute paths to the various core concept folders of the bento api.
+
+Bento.ROOT_PATH      = path.join(__dirname);
+Bento.CONFIG_PATH    = path.join(Bento.ROOT_PATH, 'config');
+Bento.INTERFACE_PATH = path.join(Bento.ROOT_PATH, 'interface');
+Bento.MODULE_PATH    = path.join(Bento.ROOT_PATH, 'modules');
+Bento.POLICY_PATH    = path.join(Bento.ROOT_PATH, 'policies');
+Bento.PROVIDER_PATH  = path.join(Bento.ROOT_PATH, 'providers');
+Bento.HOOKS_PATH     = path.join(Bento.ROOT_PATH, 'hooks');
+Bento.STORAGE_PATH   = path.join(Bento.ROOT_PATH, 'storage');
+Bento.TEST_PATH      = path.join(Bento.ROOT_PATH, 'test');
+
 // ### Configuration
 
 config = config(path.resolve('./config'));
