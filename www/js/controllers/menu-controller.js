6e9e532aa4c485e72dff92de2538292589f15cf9
Jeduan Cornejo <jeduan@gmail.com>
2015-11-24 17:51:49 -0600

    Refactors MenuController

--- www/js/controllers/menu-controller.js
+++ www/js/controllers/menu-controller.js
@@ -2,19 +2,19 @@
 var angular = require('angular');
 require('../services/auth-service.js');
 
+function MenuController ($scope, $auth, $state) {
+  this.$auth = $auth;
+
+  this.logout = function logout () {
+    $auth.logout();
+    $state.go('auth');
+  };
+}
+
 module.exports =
   angular.module('app.controllers').controller('MenuController', [
     '$scope',
     '$auth',
     '$state',
-    function ($scope, $auth, $state) {
-      $scope.$auth = $auth;
-
-      $scope.logout = function(){
-        $auth.logout();
-        $state.go('auth');
-      };
-
-    }
-
+    MenuController
   ]);
