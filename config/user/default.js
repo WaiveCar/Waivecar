29fa2e49c0086e18202136f0b8e451612307341d
Christoffer Rødvik <christoffer@kodemon.net>
2015-11-24 09:51:12 +0900

    Updated default user configuration.
     - User list is now moved into their own environments.
     - Previous default user list is now part of development.
     - Updated required fields and filter retained in default.

--- config/user/default.js
+++ config/user/default.js
@@ -15,42 +15,11 @@ module.exports = {
   user : {
     users : [
       {
-        firstName     : 'Matt',
-        lastName      : 'Ginty',
-        email         : 'matt.ginty@clevertech.biz',
-        password      : 'lollipop0',
-        phone         : '430099449',
-        role          : 'admin',
-        verifiedEmail : true,
-        status        : 'active'
-      },
-      {
-        firstName     : 'Christoffer',
-        lastName      : 'Rødvik',
-        email         : 'christoffer@clevertech.biz',
-        password      : 'password',
-        role          : 'admin',
-        verifiedEmail : true,
-        status        : 'active',
-        facebook      : '10153354349045449'
-      },
-      {
-        firstName     : 'Zoli',
-        lastName      : 'Honig',
-        email         : 'zoli@waivecar.com',
-        password      : 'password',
-        role          : 'admin',
-        verifiedEmail : true,
-        status        : 'active'
-      },
-      {
-        firstName     : 'Issac',
-        lastName      : '',
-        email         : 'ideutsch@waivecar.com',
-        password      : 'password',
-        role          : 'admin',
-        verifiedEmail : true,
-        status        : 'active'
+        role      : 'admin',
+        firstName : 'John',
+        lastName  : 'Doe',
+        email     : 'admin@fixture.none',
+        password  : 'admin'
       }
     ],
 
---
+++ config/user/development.js
@@ -0,0 +1,43 @@
+module.exports = {
+  user : {
+    users : [
+      {
+        firstName     : 'Matt',
+        lastName      : 'Ginty',
+        email         : 'matt.ginty@clevertech.biz',
+        password      : 'lollipop0',
+        phone         : '430099449',
+        role          : 'admin',
+        verifiedEmail : true,
+        status        : 'active'
+      },
+      {
+        firstName     : 'Christoffer',
+        lastName      : 'Rødvik',
+        email         : 'christoffer@clevertech.biz',
+        password      : 'password',
+        role          : 'admin',
+        verifiedEmail : true,
+        status        : 'active'
+      },
+      {
+        firstName     : 'Zoli',
+        lastName      : 'Honig',
+        email         : 'zoli@waivecar.com',
+        password      : 'password',
+        role          : 'admin',
+        verifiedEmail : true,
+        status        : 'active'
+      },
+      {
+        firstName     : 'Issac',
+        lastName      : '',
+        email         : 'ideutsch@waivecar.com',
+        password      : 'password',
+        role          : 'admin',
+        verifiedEmail : true,
+        status        : 'active'
+      }
+    ]
+  }
+};
