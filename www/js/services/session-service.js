c7ab5082c78d2410c9ade5bd7b2c7bcc64931fcd
Jeduan Cornejo <jeduan@gmail.com>
2015-11-24 17:52:04 -0600

    Refactors session-service

--- www/js/services/session-service.js
+++ www/js/services/session-service.js
@@ -2,81 +2,66 @@
 var angular = require('angular');
 var _ = require('lodash');
 
+function session ($rootScope, $window) {
+  function Session () {
+    this.data = {};
+    this.load();
+  }
+
+  Session.prototype.load = function load () {
+    angular.forEach($window.localStorage, function (value, key) {
+
+      if ($window.localStorage.hasOwnProperty(key)) {
+        try {
+          session.data[key] = value && JSON.parse(value);
+        } catch (err) {
+          $rootScope.$emit('sessionParseError', err);
+        }
+      }
+    });
+  };
+
+  Session.prototype.has = function has (key) {
+    if (_.isEmpty(this.data)) {
+      this.load();
+    }
+    return !_(this.data[key]).isUndefined();
+  };
+
+  Session.prototype.get = function get (key) {
+    if (_.isEmpty(this.data)) {
+      this.load();
+    }
+    return _(this.data[key]).isUndefined() ? {} : this.data[key];
+  };
+
+  Session.prototype.save = function save () {
+    angular.forEach(this.data, function (value, key) {
+      // stripped of angular-specific $$ properties
+      var val = angular.fromJson(angular.toJson(value));
+      $window.localStorage[key] = JSON.stringify(val);
+    });
+
+    return this;
+  };
+
+  Session.prototype.purge = function purge (key) {
+    if (key) {
+      delete this.data[key];
+      delete $window.localStorage[key];
+    } else {
+      this.data = {};
+      $window.localStorage.clear();
+    }
+
+    return this;
+  };
+
+  return new Session();
+}
+
 angular.module('app.services').factory('$session', [
   '$rootScope',
   '$window',
-  function ($rootScope, $window) {
-
-    var session = {
-
-      data: {},
-
-      load: function () {
-        angular.forEach($window.localStorage, function (value, key) {
-
-          if ($window.localStorage.hasOwnProperty(key)) {
-            try {
-              session.data[key] = value && JSON.parse(value);
-            } catch (err) {
-              $rootScope.$emit('sessionParseError', err);
-            }
-          }
-
-        });
-
-        return this;
-
-      },
-
-      has: function (key) {
-        if (_.isEmpty(this.data)) {
-          this.load();
-        }
-        return !_(this.data[key]).isUndefined();
-
-      },
-
-      get: function (key) {
-        if (_.isEmpty(this.data)) {
-          this.load();
-        }
-        return _(this.data[key]).isUndefined() ? {} : this.data[key];
-
-      },
-
-      set: function (key, value) {
-        this.data[key] = value;
-        return this;
-      },
-
-      save: function () {
-        angular.forEach(this.data, function (value, key) {
-          // stripped of angular-specific $$ properties
-          var val = angular.fromJson(angular.toJson(value));
-          $window.localStorage[key] = JSON.stringify(val);
-        });
-
-        return this;
-
-      },
-
-      purge: function (key) {
-        if (key) {
-          delete this.data[key];
-          delete $window.localStorage[key];
-        } else {
-          this.data = {};
-          $window.localStorage.clear();
-        }
-
-        return this;
-
-      }
-
-    };
-
-    return session.load();
-
-  }
-
+  session
 ]);
