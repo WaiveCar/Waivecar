df8b4d008d66ad42f4806cda6c6ab470d684b400
Christoffer Rødvik <snapgravy@gmail.com>
2015-11-26 02:16:47 +0900

    Allow all origins on development

--- config/api/development.js
+++ config/api/development.js
@@ -1,7 +1,10 @@
 module.exports = {
   api : {
     port : 3000,
-    uri  : 'http://localhost:3000'
+    uri  : 'http://localhost:3000',
+    cors : {
+      origins : '*'
+    }
   },
   socket : {
     auth : 'http://localhost:3000/users/me'
