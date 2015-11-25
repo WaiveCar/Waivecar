2924c454f201908c72a6155493f55f0817ea0a90
Christoffer Rødvik <snapgravy@gmail.com>
2015-11-26 02:15:30 +0900

    Added wildcard support to cors origin

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,17 +4,27 @@ let cors = require('koa-cors');
 
 /* istanbul ignore next: its cors man! */
 
-module.exports = function(app) {
+module.exports = (app) => {
   app.use(cors({
     credentials : true,
     headers     : Bento.config.api.cors.headers,
-    origin      : function(req) {
+    origin      : (req) => {
       let origin  = req.headers.origin;
       let origins = Bento.config.api.cors.origins;
-      let index   = origins.indexOf(origin);
+      
+      // ### Wildcard
+      
+      if (!origins || origins === '*') {
+        return origin;
+      }
+      
+      // ### Verify Origin
+      
+      let index = origins.indexOf(origin);
       if (index !== -1) {
         return origins[index];
       }
+      
       return null;
     }
   }));
