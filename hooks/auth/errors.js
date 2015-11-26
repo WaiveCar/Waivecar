9c165ed697f0ab40105d4fa8d819a764034690cd
Christoffer Rødvik <christoffer@kodemon.net>
2015-11-26 16:25:54 +0900

    Added auth tag to error codes. #97

--- hooks/auth/errors.js
+++ hooks/auth/errors.js
@@ -5,14 +5,14 @@ let error = Bento.Error;
 module.exports = {
   invalidUser() {
     throw error.parse({
-      code    : `INVALID_USER`,
+      code    : `AUTH_INVALID_USER`,
       message : `The token provided belongs to a user that is no longer accessible.`
     }, 400);
   },
 
   invalidCredentials() {
     throw error.parse({
-      code    : `INVALID_CREDENTIALS`,
+      code    : `AUTH_INVALID_CREDENTIALS`,
       message : `The email and/or password provided is invalid.`
     }, 400);
   }
