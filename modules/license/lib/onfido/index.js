129fba335bc2d4af94fa35911c35f36a3182de03
Matt Ginty <matt.ginty@gmail.com>
2015-11-28 07:58:18 +1000

    remove debugs

--- modules/license/lib/onfido/index.js
+++ modules/license/lib/onfido/index.js
@@ -73,12 +73,7 @@ module.exports = class OnfidoService {
     if (license.gender) candidate.gender = license.gender;
     /*eslint-enable */
 
-    log.debug('sending to onfido');
-    console.dir(candidate);
-
     let response = yield this.request('/applicants', 'POST', candidate);
-    log.debug('response from onfido');
-    console.dir(response);
     return response;
   }
 
