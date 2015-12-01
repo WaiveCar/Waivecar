3a32fdee1f9322493a36e314665c4ede27c0c7c5
Christoffer Rødvik <christoffer@kodemon.net>
2015-12-01 13:16:26 +0900

    Core updates, log updates, force sequelize for new model updates.

--- config/api/default.js
+++ config/api/default.js
@@ -17,7 +17,7 @@ module.exports = {
 
   api : {
     name    : 'WaiveCar',
-    version : '0.7.4',
+    version : '0.7.7',
     port    : 8081,
     uri     : 'http://localhost:8081',
     cors    : {
