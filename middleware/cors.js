c7c0d999e4864e0ee8badcee6b8e0489c7176ce6
Christoffer Rødvik <christoffer@kodemon.net>
2015-12-10 20:50:38 +0900

    Reverted cors, problem is elsewhere.

--- middleware/cors.js
+++ middleware/cors.js
@@ -6,11 +6,12 @@ let cors = require('koa-cors');
 
 module.exports = (app) => {
   app.use(cors({
-    headers : Bento.config.api.cors.headers,
-    origin  : (req) => {
+    credentials : true,
+    headers     : Bento.config.api.cors.headers,
+    origin      : (req) => {
       let origin  = req.headers.origin;
       let origins = Bento.config.api.cors.origins;
-      if (!origins || origins === '*' || origins.indexOf(origin) !== -1) {
+      if (!origins || origins === '*' || origins.indexOf(origin) !== 1) {
         return origin;
       }
       return null;
