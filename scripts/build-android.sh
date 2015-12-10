406653ef9784f52ee8e21c1dbfad9920bf39dab5
Jeduan Cornejo <jeduan@gmail.com>
2015-12-09 18:41:57 -0600

    Improvements on android build

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -5,6 +5,7 @@ export KEYSTORE_PASS="yEt7Mon3I9Swi5woY4Wu"
 export KEYSTORE="certs/waivecar.keystore"
 export KEYSTORE_ALIAS="waivecar"
 export APK_NAME="waivecar"
+export APK_LOCATION="platforms/android/build/outputs/apk/android-release-unsigned.apk"
 
 echo "\n > cordova build android\n"
 cordova build android --release
@@ -15,8 +16,11 @@ jarsigner \
   -sigalg SHA1withRSA \
   -digestalg SHA1 \
   -keystore $KEYSTORE \
-  platforms/android/build/outputs/apk/android-release-unsigned.apk \
+  $APK_LOCATION \
   $KEYSTORE_ALIAS
+
+echo "\n > verifying APK"
+jarsigner -verify -certs $APK_LOCATION
 
 echo "\n > creating release file in releases/$APK_NAME.apk\n"
 mkdir -p releases
