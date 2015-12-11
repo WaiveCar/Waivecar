1583510bc0237720827742c265aba316626c9f4a
Matt Ginty <matt.ginty@gmail.com>
2015-12-11 15:43:31 +1000

    wip

---
+++ src/views/app/admin/cars/show.jsx
@@ -0,0 +1,130 @@
+
+'use strict';
+
+import React                from 'react';
+import { auth, relay, dom } from 'bento';
+import { Form }             from 'bento-web';
+import Car                  from '../lib/car-service';
+
+let formFields = {
+  car : require('./form-fields/car'),
+};
+
+module.exports = class CarsShowView extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+    this.state = {};
+    dom.setTitle('Car');
+    this.license = new License(this);
+  }
+
+  handleChange = (item, value) => {
+    const newState = {};
+    newState[item] = value;
+    this.setState(newState);
+  }
+
+  componentDidMount() {
+    this.license.setLicenses();
+  }
+
+  componentWillUnmount() {
+    relay.unsubscribe(this, 'app');
+  }
+
+  renderLicenseRegistration() {
+    const user = auth.user();
+    let license = {
+      firstName : user.firstName,
+      lastName  : user.lastName
+    };
+
+    return (
+      <div className="box">
+        <h3>
+          Driver's License Registration <small>Register your license to gain access to the waivecar app.</small>
+        </h3>
+        <div className="box-content">
+          <Form
+            ref       = "license"
+            className = "bento-form-static"
+            fields    = { formFields.license }
+            default   = { license }
+            buttons   = {[
+              {
+                value : 'Register License',
+                type  : 'submit',
+                class : 'btn btn-primary btn-profile-submit'
+              }
+            ]}
+            submit = { this.license.submitLicense }
+          />
+        </div>
+      </div>
+    );
+  }
+
+  renderLicense(license) {
+    return (
+      <div className="box">
+        <h3>
+          License
+        </h3>
+        <div className="box-content">
+          <Form
+            ref       = "license"
+            className = "bento-form-static"
+            fields    = { formFields.license }
+            default   = { license }
+          />
+          { this.renderStatus(license.status, license.outcome) }
+        </div>
+      </div>
+    )
+  }
+
+    /**
+   * Render Verification CTA
+   * @return {Object}
+   */
+  renderStatus(status, outcome) {
+    if (status !== 'provided') {
+      return (
+        <div className="license-verification text-center">
+          { !outcome               && <p className="bg-info p-a">Pending Verification</p> }
+          { outcome === 'consider' && <p className="bg-danger p-a">At this time your license has failed verification and you are unable to book a car. <br />If you would like a copy of your report or further explanation, please contact us.</p> }
+          { outcome === 'clear'    && <p className="bg-success p-a">Your License has been cleared.</p> }
+        </div>
+      );
+    }
+
+    return (
+      <div className="license-verification text-center">
+        <p className="box-info text-center">
+          Your license must be verified before you can book a WaiveCar
+        </p>
+        <button type="button" onClick={ this.license.validateLicense }>Request Validation</button>
+      </div>
+    );
+  }
+
+
+  render() {
+    let licenses = this.license.getState('licenses');
+
+    if (this.state.isLoading) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div className="profile">
+        {
+          licenses && licenses.length > 0
+            ? this.renderLicense(licenses[0])
+            : this.renderLicenseRegistration()
+        }
+      </div>
+    );
+  }
+
+}
---
+++ src/views/app/lib/car-service.js
@@ -0,0 +1,83 @@
+'use strict';
+
+import { api, auth, dom, helpers } from 'bento';
+import Service                     from './component-service';
+import async                       from 'async';
+
+module.exports = class Car extends Service {
+
+  /**
+   * Stores the provided context.
+   * @param {Object} ctx
+   */
+  constructor(ctx) {
+    super(ctx, 'car', {
+      cars : []
+    });
+    this.update = this.update.bind(this);
+    this.executeCommand = this.executeCommand(this)
+  }
+
+  /**
+   * Form submission method for bento-web form component.
+   * @param  {Object}   data
+   * @param  {Function} reset
+   */
+  update(data) {
+    this.save(auth.user(), data, function (err, model) {
+      if (err) {
+        return this.error(err.data ? err.data : err.message);
+      }
+
+      let models = this.getState('cars');
+      let cars = [ model ];
+      models.forEach((m) => {
+        if (m.id !== model.id) {
+          cars.push(model);
+        }
+      });
+
+
+      this.setState('car', [
+        ...this.getState('car'),
+        model
+      ]);
+      this.success(`Car was updated successfully.`);
+    }.bind(this));
+  }
+
+  /**
+   * Save updates made to the Car form
+   * @param {Object}   user
+   * @param {Object}   car
+   * @param {Function} done
+   */
+  save(user, car, done) {
+    api.put(`/cars/${ car.id }`, {
+      make         : car.make,
+      model        : car.model,
+      year         : car.year,
+      manufacturer : car.manufacturer,
+      license      : car.license,
+      vin          : car.vin,
+      isAvailable  : car.isAvailable,
+    }, function (err, model) {
+      if (err) {
+        return done(err);
+      }
+      return done(null, model);
+    }.bind(this));
+  }
+
+  /**
+   * Loads car from the api and adds them to the array on the ctx.
+   */
+  setCar(id) {
+    api.get(`/cars/${ id }`, function (err, cards) {
+      if (err) {
+        return this.error(err.message);
+      }
+      this.setState('activeCar', cards);
+    }.bind(this));
+  }
+}
