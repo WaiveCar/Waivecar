c500f343d9570e46329b60b6f5e9f23a9c0f767c
Matt Ginty <matt.ginty@gmail.com>
2015-12-15 12:45:08 +1000

    lint, fix

--- www/js/controllers/landing-controller.js
+++ www/js/controllers/landing-controller.js
@@ -4,16 +4,19 @@ require('angular-ui-router');
 require('../services/auth-service');
 
 module.exports = angular.module('app.controllers').controller('LandingController', [
-  '$scope',
-  '$state',
-  '$auth',
-  function ($scope, $state, $auth) {
+  '$injector',
+  function ($injector) {
+
+    var $state = $injector.get('$state');
+    var $auth = $injector.get('$auth');
+    // var $ride = $injector.get('$ride');
 
     function init() {
       if (!$auth.isAuthenticated()) {
         return $state.go('auth');
       } else {
         return $state.go('cars');
+       // return $ride.reroute();
       }
     };
 
