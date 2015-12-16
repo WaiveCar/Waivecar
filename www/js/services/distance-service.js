e5dfc0ae5ccc289010e8994b10e742a5c867e581
Matt Ginty <matt.ginty@gmail.com>
2015-12-16 16:22:05 +1000

    fix for real location data

--- www/js/services/distance-service.js
+++ www/js/services/distance-service.js
@@ -37,8 +37,9 @@ DistanceService.prototype.toMiles = function toMiles (m) {
 };
 
 function isValidLocation (loc) {
-  console.log(loc);
-  //if (!loc) return false;
+  if (!loc) {
+    return false;
+  }
   return (loc.latitude && loc.longitude) ||
     (Array.isArray(loc) && loc.length === 2 &&
       typeof loc[0] === 'number' &&
