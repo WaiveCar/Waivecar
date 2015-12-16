0eba96e0a7d2d29eec1b5178c80724ab6d63f4fb
Matt Ginty <matt.ginty@gmail.com>
2015-12-17 07:50:29 +1000

    use live twilio in dev

--- config/sms/development.js
+++ config/sms/development.js
@@ -2,9 +2,9 @@ module.exports = {
   sms : {
     transportName : 'twilio',
     transport     : {
-      phoneNumber : '+16692440099',
-      sid         : 'AC60aae35b99f8413d4e70d7d20ab9d05d',
-      token       : 'ef5715c5a49f6c5479105d2133976806'
+      phoneNumber : '+13234585511',
+      sid         : 'ACa061f336122514af845ea65fb1e6c2bb',
+      token       : 'b39d95c162c1e9ad1893acbb61af8bb4'
     }
   }
 };
