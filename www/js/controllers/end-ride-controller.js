4db3a69804b396b498f1d3076a3f61520c43315b
Jeduan Cornejo <jeduan@gmail.com>
2016-01-15 12:26:33 -0800

    Geocodes the current address in end ride

--- www/js/controllers/end-ride-controller.js
+++ www/js/controllers/end-ride-controller.js
@@ -5,6 +5,7 @@ require('angular-ui-router');
 require('../services/auth-service');
 require('../services/data-service');
 require('../services/ride-service');
+require('../services/geocoding-service');
 
 module.exports = angular.module('app.controllers').controller('EndRideController', [
   '$rootScope',
@@ -13,7 +14,9 @@ module.exports = angular.module('app.controllers').controller('EndRideController
   '$auth',
   '$data',
   '$ride',
-  function ($rootScope, $scope, $state, $auth, $data, $ride) {
+  '$geocoding',
+  '$progress',
+  function ($rootScope, $scope, $state, $auth, $data, $ride, $geocoding, $progress) {
 
     // Concepts:
     // $scope is used to store ref. to the service and the active models in the data svc.
@@ -27,16 +30,34 @@ module.exports = angular.module('app.controllers').controller('EndRideController
       $scope.showVideo = !$scope.showVideo;
     };
 
+    this.geocode = function () {
+      $progress.showSimple(true);
+      if (!($rootScope.currentLocation && $rootScope.currentLocation.latitude)) {
+        return null;
+      }
+      return $geocoding($rootScope.currentLocation.latitude, $rootScope.currentLocation.longitude)
+        .then(function (location) {
+          $ride.state.parkingLocation.addressLine1 = location.display_name;
+        })
+        .finally(function () {
+          $progress.hide();
+        });
+    };
+
     this.init = function () {
       var rideServiceReady = $scope.$watch('service.isInitialized', function(isInitialized) {
-        console.log(isInitialized);
-        if (isInitialized === true) {
-          rideServiceReady();
-          if ($state.current.name === 'end-ride') {
-            $scope.service.processEndRide();
-          }
+        console.log('[end-ride] Service initialized: %s', isInitialized);
+        if (isInitialized !== true) {
+          return;
         }
-      });
+        rideServiceReady();
+        if ($state.current.name === 'end-ride-location') {
+          this.geocode();
+        }
+        if ($state.current.name === 'end-ride') {
+          $scope.service.processEndRide();
+        }
+      }.bind(this));
     };
 
     this.init();
