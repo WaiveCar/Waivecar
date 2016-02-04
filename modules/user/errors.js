9ccb5dc75276dd0586ddbf3dca63588716e25164
Gabriel Schroder <gabe.schroder@gmail.com>
2016-02-04 06:02:22 -0800

    added error messages for handler

--- modules/user/errors.js
+++ modules/user/errors.js
@@ -1,11 +1,17 @@
 'use strict';
 
 Bento.Error.handler('POST /users', (err) => {
-  if (err.code === 'SEQUELIZE_SAVE_ERROR' && err.data.type === 'UNIQUE_VIOLATION') {
+  if (err.code === 'SEQUELIZE_SAVE_ERROR' && err.data.type === 'SEQUELIZE_UNIQUE_CONSTRAINT_ERROR') {
+	  var jsonError = JSON.parse(JSON.stringify(err));
+		var badField = Object.keys(jsonError.data.fields)[0];
     err.code    = `USER_DUPLICATE`;
     err.status  = 400;
-    err.message = `A unique constraint error occured during registration.`;
     err.data    = err.fields;
+    if (badField === 'email') {
+    	err.message = `The ${badField} address you entered is already attached to an account.`;
+    } else if (badField === 'phone') {
+    	err.message = `The ${badField} number you entered is already attached to an account.`;
+    }
   }
   return err;
 });
