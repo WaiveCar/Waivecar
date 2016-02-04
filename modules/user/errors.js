ed0144ac5d77574d6211909e6f2d86db77fbe145
Gabriel Schroder <gabe.schroder@gmail.com>
2016-02-04 07:42:32 -0800

    Added error handling for updating user info

--- modules/user/errors.js
+++ modules/user/errors.js
@@ -17,3 +17,21 @@ Bento.Error.handler('POST /users', (err) => {
 
   return err;
 });
+
+Bento.Error.handler('PUT /users/:id', (err) => {
+  if (err.code === 'SEQUELIZE_UPDATE_ERROR' && err.data.type === 'SEQUELIZE_UNIQUE_CONSTRAINT_ERROR') {
+	  let jsonError = JSON.parse(JSON.stringify(err));
+		let badField = Object.keys(jsonError.data.fields)[0];
+    err.code    = `USER_DUPLICATE`;
+    err.status  = 400;
+    err.data    = err.fields;
+
+    if (badField === 'email') {
+    	err.message = `The ${badField} address you entered is already attached to an account.`;
+    } else if (badField === 'phone') {
+    	err.message = `The ${badField} number you entered is already attached to an account.`;
+    }
+  }
+
+  return err;
+});
