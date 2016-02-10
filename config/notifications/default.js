f642d4c77fa840137aa44f8bc88176e5b8b5f144
Steven White <swhitewvu24@gmail.com>
2016-02-10 09:47:46 -0500

    [f:30m] Add setup for notifications, new timer

--- config/notifications/default.js
+++ config/notifications/default.js
@@ -11,11 +11,8 @@ module.exports = {
    */
   notification : {
     reasons : {
-      OUTSIDE_RANGE : [ 'Hi there! Seems like you\'ve just driven outside of WaiveCar\'s max driving range.',
-                        'We don\'t want you to get stuck or not be able to return the car in our return zone',
-                        'so please turn back at your earliest convenience.' ].join(' '),
-      LOW_CHARGE : [ 'Hey there! Looks like your WaiveCar battery is getting really low. Please head to a',
-                    'charger ASAP. You can locate the nearest ones in the app.' ].join(' ')
+      OUTSIDE_RANGE : `Hi there, looks like you are driving your WaiveCar outside of Santa Monica. As a reminder, all rentals must be completed in Santa Monica, and the max driving range is 20 miles from WaiveCar HQ. Thanks & enjoy your drive!`,
+      LOW_CHARGE    : `Hey there! Looks like your WaiveCar battery is getting really low. Please head to a charger ASAP. You can locate the nearest ones in the app.`
     }
   }
 };
