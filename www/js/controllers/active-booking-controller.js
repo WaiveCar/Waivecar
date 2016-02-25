b3259c3945e9182b89c37d2e63b93fbc413f8e92
Steven White <swhitewvu24@gmail.com>
2016-02-25 14:07:32 -0500

    [f:3h] Migrate address information to active booking screen; ensure timer is created and visible

--- www/js/controllers/active-booking-controller.js
+++ www/js/controllers/active-booking-controller.js
@@ -29,6 +29,7 @@ function ActiveBookingController ($scope, $rootScope, $injector) {
   $scope.service = $ride;
 
   // $data is used to interact with models, never directly. If direct is required, $data should be refreshed.
+  var ctrl = this;
   this.data = $data.active;
 
   var expired;
@@ -36,6 +37,7 @@ function ActiveBookingController ($scope, $rootScope, $injector) {
     if (!isInitialized) {
       return;
     }
+    ctrl.car = $data.active.cars;
     stopServiceWatch();
     stopServiceWatch = null;
     watchForWithinRange();
