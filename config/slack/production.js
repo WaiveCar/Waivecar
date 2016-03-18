792710454550140188436919e896f73f6093ce2f
Steven White <swhitewvu24@gmail.com>
2016-03-18 09:14:04 -0400

    [f:30m] Fix slack messaging for production

--- config/slack/production.js
+++ config/slack/production.js
@@ -1,5 +1,6 @@
 module.exports = {
   slack : {
+    default  : 'https://hooks.slack.com/services/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',
     channels : {
       notifications : 'https://hooks.slack.com/services/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',
       'api-errors'  : 'https://hooks.slack.com/services/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'
