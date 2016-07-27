973c0cc8a35502dba4875cecfdb9a68e1a0e741c
Chris McKenzie <kristopolous@yahoo.com>
2016-07-27 08:40:22 -0700

    in-housing the development configs to waivecar

--- config/auth/development.js
+++ config/auth/development.js
@@ -2,7 +2,7 @@ module.exports = {
   auth : {
     facebook : {
       appId    : '1022708504415468',
-      redirect : 'https://waivecar-dev.cleverbuild.biz/auth/facebook'
+      redirect : 'https://stage.waivecar.com/auth/facebook'
     }
   }
 };
