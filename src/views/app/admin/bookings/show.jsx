4436326a7e5a1cb23515c78e54ce132a9d0b8ff0
Chris McKenzie <kristopolous@yahoo.com>
2016-07-29 17:43:46 -0700

    Adding "start booking" to be adjacent to cancel in the reservation state

--- src/views/app/admin/bookings/show.jsx
+++ src/views/app/admin/bookings/show.jsx
@@ -130,7 +130,7 @@ module.exports = class BookingsView extends React.Component {
     let action = false;
     switch (booking.status) {
       case 'reserved' : {
-        action = <button type="button" onClick={ () => { this.cancel() } } className="btn btn-primary">Cancel</button>;
+        action = <button type="button" onClick={ () => { this.cancel() } } className="btn btn-primary">Cancel</button> <button type="button" onClick={ () => { this.update('ready') } } className="btn btn-link">Start Ride</button>;
         break;
       }
       case 'ready'   : {
