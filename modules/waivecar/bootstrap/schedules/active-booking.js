6fbf46140ee59fd0a25eee4344ec606fa2bc1f19
Chris McKenzie <kristopolous@yahoo.com>
2016-07-29 17:00:12 -0700

    Alertsl for excess of 3 hours rentals by users of under 5 bookings.

--- modules/waivecar/bootstrap/schedules/active-booking.js
+++ modules/waivecar/bootstrap/schedules/active-booking.js
@@ -10,6 +10,7 @@ let Car       = Bento.model('Car');
 let User      = Bento.model('User');
 let log       = Bento.Log;
 let config    = Bento.config;
+let moment    = require('moment');
 let geolib    = require('geolib');
 let _         = require('lodash');
 
@@ -41,19 +42,36 @@ scheduler.process('active-booking', function *(job) {
   for (let i = 0, len = bookings.length; i < len; i++) {
     let booking = bookings[i];
     try {
-      let details = BookingDetails.find({ where : { bookingId : booking.id } });
+      let details = yield BookingDetails.find({ where : { bookingId : booking.id } });
       let start = _.find(details, { type : 'start' });
       let car = yield Car.findById(booking.carId);
       let device = yield cars.getDevice(car.id);
       let user = yield User.findById(car.userId);
+      let duration = 0;
+      let booking_history = null;
       
       if (!device || !car || !user) return;
 
-      // Check that battery use is changing as expected
       if (start) {
+        // Check that battery use is changing as expected
         let milesDriven = car.mileage - start.mileage;
         if (milesDriven >= 7 && car.charge === device.charge) {
-        yield notify.notifyAdmins(`${ car.license } has been driven ${ milesDriven } miles since last change reported, but charge level has not changed. ${ config.api.uri }/cars/${ car.id }`, [ 'slack' ], { channel : '#rental-alerts' });
+          yield notify.notifyAdmins(`${ car.license } has been driven ${ milesDriven } miles since last change reported, but charge level has not changed. ${ config.api.uri }/cars/${ car.id }`, [ 'slack' ], { channel : '#rental-alerts' });
+        }
+
+        //
+        // New user rental warning (under 5 rentals, over 3 hours) #463
+        //
+        // Send an alert to 'rental alerts' if a new user (less than 5 trips) has taken a car for longer than 3 hours. 
+        //
+        duration = moment().utc().diff(start.createdAt, 'minutes');
+        // TODO: Make sure that the user isn't flagged more than once per booking
+        if (duration >= 180 && duration < 181) {
+          booking_history = yield Booking.find({ where : { userId : user.id }});
+
+          if(booking_history.length < 5) {
+            yield notify.notifyAdmins(`${ car.license } has been driven for ${ duration } minutes by ${ user.name() } who has only rented ${ booking_history.length } times. Driver: ${ user.name() } <${ user.phone || user.email }>`, [ 'slack' ], { channel : '#rental-alerts' });
+          }
         }
       }
 
