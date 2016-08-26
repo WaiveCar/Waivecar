8d27f66330f8f55547b4a56d0f9d78fd8448b8ab
Chris McKenzie <kristopolous@yahoo.com>
2016-08-26 15:34:58 -0700

    Adding configs for the staging env

---
+++ config/auth/staging.js
@@ -0,0 +1,9 @@
+module.exports = {
+  auth : {
+    facebook : {
+      appId    : '1022708504415468',
+      redirect : 'https://staging.waivecar.com/auth/facebook'
+      //redirect : 'https://waivecar-dev.cleverbuild.biz/auth/facebook'
+    }
+  }
+};
