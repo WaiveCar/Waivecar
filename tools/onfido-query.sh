5f2e09b6127b5c9e3c689a45a19b9b19ebf2ec9f
Chris McKenzie <kristopolous@yahoo.com>
2016-08-31 16:53:30 -0700

    Adding a tool to query onfido easily

---
+++ tools/onfido-query.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+url=https://api.onfido.com/v2/"$1"
+echo $url
+out=`curl -s https://api.onfido.com/v2/"$1" -H "Authorization:Token token=live_as_IHLB4xIpsv281PmGRfg57U5welUN_"`
+echo $out | python -mjson.tool
