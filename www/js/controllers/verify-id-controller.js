b2ec2f3a0c339428dc84613cb7df9e6df407df32
Chris McKenzie <kristopolous@yahoo.com>
2016-09-09 16:26:24 -0700

    More work on the verify id controller

--- www/js/controllers/verify-id-controller.js
+++ www/js/controllers/verify-id-controller.js
@@ -1,103 +1,166 @@
 'use strict';
 var angular = require('angular');
+var _ = require('lodash');
 require('angular-ui-router');
-require('../services/auth-service');
 require('../services/data-service');
-require('../services/upload-image-service');
 
-function VerifyIdController ($stateParams, $injector) {
-  var $auth = $injector.get('$auth');
-  var $data = $injector.get('$data');
-  var USStates = $injector.get('USStates');
-  var $timeout = $injector.get('$timeout');
-  var $state = $injector.get('$state');
-  var $modal = $injector.get('$modal');
-  var $message = $injector.get('$message');
-  var $q = $injector.get('$q');
+module.exports = angular.module('app.controllers').controller('VerifyIdController', [
+  '$scope',
+  '$settings',
+  '$window',
+  '$stateParams',
+  '$ionicLoading',
+  '$injector',
+  function($scope, $settings, $window, $stateParams, $ionicLoading, $injector) {
+    var $uploadImage = $injector.get('$uploadImage');
+  
+    var $timeout = $injector.get('$timeout');
+    var $modal = $injector.get('$modal');
+    var $message = $injector.get('$message');
+    var $data = $injector.get('$data');
+    var $q = $injector.get('$q');
 
-  this.isWizard = !!$stateParams.step;
-  this.fromBooking = !!$stateParams.fromBooking;
-  this.license = this.license || new $data.resources.licenses({
-    country: 'USA',
-    userId: $auth.me.id
-  });
-  if ($auth.me.firstName) {
-    this.license.firstName = $auth.me.firstName;
-  }
-  if ($auth.me.lastName) {
-    this.license.lastName = $auth.me.lastName;
-  }
-  this.states = USStates;
+    var ctrl = this;
 
-  this.nextState = function nextState () {
-    if (this.isWizard) {
-      $state.go('credit-cards-new', {step: 4});
-      return;
-    }
-    $state.go('users-edit');
-  };
+    ctrl.images = {
+      license: null,
+      selfie: null
+    };
 
-  var self = this;
-
-  this.submit = function submit (form) {
-    if (!form.$valid) {
-      $message.error('Please fill the fields');
-    }
-    return this.license.$create()
-    .then(function () {
-      var modal;
-      return $modal('result', {
-        icon: 'check-icon',
-        title: 'License info received'
-      })
-      .then(function (_modal) {
-        modal = _modal;
-        modal.show();
-        return modal;
-      })
+    this.submit = function submit (form) {
+      return this.license.$create()
       .then(function () {
-        return $timeout(1000);
+        var modal;
+        return $modal('result', {
+          icon: 'check-icon',
+          title: 'License info received',
+          message: 'Thanks! During normal business hours this will only take a minute or two.'/*,
+          actions: [{
+            text: 'Ok',
+            className: 'button-balanced',
+            handler: function () {
+              ignitionOnModal.remove();
+            }
+         }]
+          */
+        })
+        .then(function (_modal) {
+          modal = _modal;
+          modal.show();
+          return modal;
+        })
+        .then(function () {
+          return $timeout(1000);
+        })
+        .then(function () {
+          modal.remove();
+        });
       })
-      .then(function () {
-        modal.remove();
-        this.nextState();
-      }.bind(this));
-    }.bind(this))
-    .catch(function onUploadFailed (err) {
-      var modal, message = 'Looks like the formatting of your license is wrong, please try again.';
-      if('data' in err && 'message' in err.data) {
-        message = err.data.message;
-      }
+      .catch(function onUploadFailed (err) {
+        var modal, message = 'Looks like the formatting of your license is wrong, please try again.';
+        if('data' in err && 'message' in err.data) {
+          message = err.data.message;
+        }
+        $modal('result', {
+          icon: 'x-icon',
+          title: message,
+          actions: [{
+            className: 'button-balanced',
+            text: 'Retry',
+            handler: function () {
+              modal.remove();
+            }
+          }, {
+            className: 'button-dark',
+            text: 'Skip',
+            handler: function () {
+              modal.remove();
+              ctrl.nextState();
+            }
+          }]
+        })
+        .then(function (_modal) {
+          modal = _modal;
+          modal.show();
+        });
+        $q.reject(err);
+      });
+    };
+
+    function addPicture (param, name) {
+      $uploadImage({
+        endpoint: '/files?' + param,
+        filename: [name, Date.now()].join('_') + '.jpg',
+      })
+      .then(function (result) {
+        if (result && Array.isArray(result)) result = result[0];
+
+        result.style = {
+          'background-image': 'url(' + $settings.uri.api + '/file/' + result.id + ')'
+        };
+        ctrl.street.streetSignImage = result;
+      })
+      .catch(function (err) {
+        var message = err.message;
+        if (err instanceof $window.FileTransferError) {
+          if (err.body) {
+            var error = angular.fromJson(err.body);
+            if (error.message) {
+              message = error.message;
+            }
+          }
+        }
+        submitFailure(message);
+      });
+    };
+
+    ctrl.addLicense = function () {
+      addPicture('licenseId=' + $scope.license.id, ['license', $scope.license.id].join('_'));
+    };
+
+    ctrl.addSelfie = function () {
+      addPicture('isAvatar=true&userId=' + $scope.user.id, ['selfie', $scope.user.id].join('_'));
+    };
+
+    $scope.init = function() {
+
+      // this insane thingie is how licenses for a user are found.are detected.
+      return $data.resources.users.me().$promise
+        .then(function(me) {
+          $scope.user = me;
+          return $data.resources.licenses.query().$promise;
+        }).then(function(licenses) {
+          $scope.license = _(licenses)
+            .filter({userId: $scope.user.id})
+            .sortBy('createdAt')
+            .last();
+          console.log($scope);
+        }).catch($message.error);
+    };
+
+    $scope.init();
+
+
+    function submitFailure(message) {
+      $ionicLoading.hide();
+      var endRideModal;
+
       $modal('result', {
         icon: 'x-icon',
         title: message,
         actions: [{
+          text: 'Ok',
           className: 'button-balanced',
-          text: 'Retry',
           handler: function () {
-            modal.remove();
-          }
-        }, {
-          className: 'button-dark',
-          text: 'Skip',
-          handler: function () {
-            modal.remove();
-            self.nextState();
+            endRideModal.remove();
           }
         }]
       })
       .then(function (_modal) {
-        modal = _modal;
-        modal.show();
+        _modal.show();
+        endRideModal = _modal;
+        endRideModal.show();
       });
-      $q.reject(err);
-    });
-  };
-
-}
-
-module.exports = angular.module('app.controllers').controller('VerifyIdController', [
-  '$stateParams',
-  '$injector',
-  VerifyIdController
+    }
+  }
 ]);
