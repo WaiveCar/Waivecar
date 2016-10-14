9494c41bc0471ee89da71a17e028750ba06db138
Chris McKenzie <kristopolous@yahoo.com>
2016-10-13 17:25:28 -0700

    Updated faq

--- src/views/site/faq.jsx
+++ src/views/site/faq.jsx
@@ -6,77 +6,124 @@ module.exports = class Faq extends Component {
       <div className='container'>
         <div className='row'>
           <div className="col-xs-12 col-md-8 col-md-push-2">
-            <h1>Waivecar FAQ</h1>
-
-            <h3>Is WaiveCar really free?</h3>
-            <p>
-              Yup! No need to adjust your screens, kids! WaiveCar is free for the first 2 hours. After the 2 hours are up, we only charge a low rate of $5.99 an hour after that. Mic drop!
-            </p>
-
-            <h3>Where is WaiveCar Available?</h3>
-            <p>
-              For now, Waivecar is only available in Santa Monica, CA, but just like your ex, you can bet you will soon see us around every corner! In a totally non creepy way of course..
-            </p>
-
-            <h3>How long can I use a WaiveCar for?</h3>
-            <p>
-              You can use WaiveCar as long as your little heart desires! Just remember it's an electric car and will need a charge after 80 miles, and will be charged $5.99 per hour after the 2 free hours is up.
-            </p>
-
-            <h3>Where can I find a WaiveCar to rent?</h3>
-            <p>
-              To rent a WaiveCar, choose the closest vehicle to you from the map on our app, then we will direct you to its location! But get it while it’s hot! You must arrive at your vehicle within 15 minutes to preserve the booking.
-            </p>
-
-            <h3>How far can I drive my WaiveCar?</h3>
-            <p>
-              Sorry, no Vegas trips! You can drive your WaiveCar 20 miles from the WaiveCar headquarters as long as you return it in Santa Monica. There is a map displayed on the dashboard and you will receive a text when you have reached the boundaries.
-            </p>
-
-            <h3>Where can I return my WaiveCar?</h3>
-            <p>
-              You can return your WaiveCar at any electric charging station in Santa Monica, 3 hour minimum meter or legal parking spot in Santa Monica, or at our home base.. You guessed it.. In SANTA MONICA! Important: If you leave it at a 3 hr meter or legal parking spot, it must be at least 25% charged.
-            </p>
-
-            <h3>What do I need to start driving?</h3>
-            <p>
-              All you need is your smartphone, credit/debit card, driver’s license, and a cool attitude to start driving WaiveCar!
-            </p>
-
-            <h3>Where is the key?</h3>
-            <p>
-              You can find the key in the glove compartment of the WaiveCar connected to an RFID key holder.
-            </p>
-
-            <h3>Do I need my own auto insurance policy?</h3>
-            <p>
-              Nope! In true WaiveCar fashion, Insurance is provided for you during both free and paid portions of your trip at ZERO additional cost. The insurance applies during your booking and while driving your WaiveCar.
-            </p>
-
-            <h3>I've never operated an electric car. How do I charge it?</h3>
-            <p>
-              Don’t. Stress. Out. The easiest way to charge your WaiveCar would be to bring it back to our headquarters in Santa Monica and let us do the work for you! Or, if you’re feeling adventurous, you can charge the car by locating the nearest public charging stations from the WaiveCar or Plugshare app.
-            </p>
-
-            <h3>How can I advertise with WaiveCar?</h3>
-            <p>
-              Get on board champs! Email us at <a href="mailto:advertise@waivecar.com">advertise@waivecar.com</a>.
-            </p>
-
-            <h3>What car do you use?</h3>
-            <p>
-              The WaiveCar fleet consists of Chevy Spark EVs: four-door, emission free, 100% electric cars. Not gunna lie..they’re pretty adorable.
-            </p>
-
-            <h3>How much range do your cars get?</h3>
-            <p>
-              The perfect amount of space to distance yourself from that ex.. A WaiveCar has a range of 80 miles per charge.
-            </p>
-
-            <h3>Is there a minimum amount of time that must pass between WaiveCar rentals?</h3>
-            <p>
-              Nope! As long as your last rental was ended without a hitch, you can rent again. (AKA see ya tomorrow, buddy!) 
-            </p>
+          <h2 id="what-is-waivecar">What is WaiveCar?</h2>
+          <p>WaiveCar is the first ever FREE &amp; 100% electric car sharing program. We are ad-supported so the funds we receive from Advertisers to display their ads on our roof-mounted screens and body wraps allow users to drive our cars FREE for 2 hours then $5.99/hr...MIC DROP.</p>
+          <h2 id="signing-up">Signing up</h2>
+          <h3 id="what-do-i-need-to-sign-up">What do I need to sign up?</h3>
+          <p>All you need to sign up with WaiveCar is a smart phone, credit/debit card, and a valid drivers license. Simply download the app from your app store!</p>
+          <h3 id="does-it-cost-any-money-to-sign-up">Does it cost any money to sign up?</h3>
+          <p>Nope! We promise there REALLY are no catches. We're that magical.</p>
+          <h3 id="why-do-i-need-a-creditdebit-card-to-sign-up-if-its-free">Why do I need a credit/debit card to sign up if it's free?</h3>
+          <p>We charge a temporary $1 hold on your card when you book a car that will be refunded as soon as your ride is over just to make sure there are funds in case you go over the 2 free hours!</p>
+          <h3 id="how-old-do-i-need-to-be-to-drive-waivecar">How old do I need to be to drive WaiveCar?</h3>
+          <p>You need to be 21 to drive with WaiveCar.</p>
+          <h3 id="can-i-use-my-ipadtablet-to-use-waivecar">Can I use my iPad/tablet to use WaiveCar?</h3>
+          <p>No. Unfortunately we require a smart phone so that a phone number can be attached to the account as well as GPS services.</p>
+          <h3 id="is-insurance-included">Is insurance included?</h3>
+          <p>Yep! Insurance with a $1,000 deductible is included in the signup! The information is in the glove box!</p>
+          <h3 id="can-i-use-an-out-of-state-drivers-license-to-drive-with-waivecar">Can I use an out of state drivers license to drive with WaiveCar?</h3>
+          <p>Yep! However, if you have a Pennsylvania license, please email us at support@waive.car as Pennsylvania requires a separate background check form to be filled out.</p>
+          <h3 id="can-i-use-an-out-of-country-drivers-license">Can I use an out of country drivers license?</h3>
+          <p>Yep! However you will need to contact your DMV and obtain a driving record to email to us at support@waive.car</p>
+          <h3 id="can-i-use-a-temporary-drivers-license">Can I use a temporary drivers license?</h3>
+          <p>Unfortunately you will need a permanent license to drive with WaiveCar.</p>
+          <h3 id="how-long-does-the-signup-take">How long does the signup take?</h3>
+          <p>It's very quick! Should only take about 10 minutes for the signup and the background check to complete. If the background check seems to be taking longer, please give us a call at 855-WAIVE55</p>
+          <h2 id="booking-cars">Booking Cars</h2>
+          <h3 id="what-are-waivecars-hours">What are WaiveCar's hours?</h3>
+          <p>Our cars are available 24/7 however the HQ is open from 9am-10pm and the customer service line is open from 9am-midnight. If you need assistance at other hours, simply send us an email at support@waive.car and we will reach you at our earliest convenience.</p>
+          <h3 id="how-do-i-book-a-waivecar">How do I book a WaiveCar?</h3>
+          <p>Log onto the app and find the closest available WaiveCar to you. Hit &quot;Book it&quot;! You then have 15 minutes to get to the car before the reservation times out.</p>
+          <h3 id="where-can-i-get-a-waivecar">Where can I get a WaiveCar?</h3>
+          <p>Right now our cars are only dispersed around Santa Monica, CA. In the near future, you will be able to pick them up anywhere in LA and other major cities!</p>
+          <h3 id="how-long-do-i-have-to-get-to-a-waivecar">How long do I have to get to a WaiveCar?</h3>
+          <p>You have 15 minutes to get to a WaiveCar before the reservation times out.</p>
+          <h3 id="can-i-reserve-a-car-in-advance">Can I reserve a car in advance?</h3>
+          <p>Unlike a rental company, you CANNOT reserve a car in advance. WaiveCar believes in the convenience of giving you a free car NOW! Get it while it's hot!</p>
+          <h3 id="can-i-book-multiple-cars-a-day">Can I book multiple cars a day?</h3>
+          <p>Yep! And even better, you will get the free 2 hours for EVERY ride!</p>
+          <h3 id="does-the-free-2-hours-start-when-i-reserve-a-car-or-when-i-start-the-ride">Does the free 2 hours start when I reserve a car or when I start the ride?</h3>
+          <p>When you start the ride! Enjoy your leisurely walk!</p>
+          <h3 id="is-the-free-2-hours-only-when-im-driving-the-car">Is the free 2 hours only when I'm driving the car?</h3>
+          <p>Nope! The free 2 hours counts for having the car while it's driving or parked. Use your time wisely!</p>
+          <h3 id="what-happens-if-i-dont-make-it-to-the-car-in-15-minutes">What happens if I don't make it to the car in 15 minutes?</h3>
+          <p>Then your reservation will expire! So hurry up and try to rebook the same car or another WaiveCar before it's gone! We're a hot commodity!</p>
+          <h3 id="whats-with-that-1-charge-when-i-book-a-waivecar">What's with that $1 charge when I book a WaiveCar?</h3>
+          <p>No worries! It's just a $1 hold on your card we will refund right back to you when your ride is complete!</p>
+          <h3 id="how-many-waivecars-are-there">How many WaiveCars are there?</h3>
+          <p>We currently have 20 in Santa Monica, however are expanding to about 150 very soon in LA and several major cities!</p>
+          <h2 id="driving-cars">Driving Cars</h2>
+          <h3 id="how-far-can-i-drive-with-a-waivecar">How far can I drive with a WaiveCar?</h3>
+          <p>You can drive up to 20 miles outside our HQ in Santa Monica, which includes most of LA. You will get a text message alert when you have passed the boundaries!</p>
+          <h3 id="where-are-the-keys">Where are the keys?</h3>
+          <p>You don't need keys to start the car, however if you need to lock the car DURING your rental, the keys are on a sensor in the glovebox! Just make sure you replace them when you're done.</p>
+          <h3 id="ive-never-driven-an-electric-car.-how-many-miles-per-charge-do-i-get">I've never driven an electric car. How many miles per charge do I get?</h3>
+          <p>Our Chevy Spark EVs have 80 miles per charge.</p>
+          <h3 id="what-should-i-do-if-im-running-low-on-charge">What should I do if I'm running low on charge?</h3>
+          <p>Check the app for the closest fast charger, or download the app &quot;Plugshare&quot;. Give us a call at 855-WAIVE55 if there is any confusion and we can direct you to one!</p>
+          <h3 id="do-you-guys-cover-the-charging-station-costs">Do you guys cover the charging station costs?</h3>
+          <p>Nope! Just like other services, we don't compensate to refuel/recharge, however just bring it back to the HQ where they will charge for FREE and see if you can switch out for a higher charged car!</p>
+          <h3 id="how-much-does-it-usually-cost-to-charge-an-electric-car">How much does it usually cost to charge an electric car?</h3>
+          <p>Fast chargers typically cost around $10 and take about 15 minutes. Level 2 chargers take about 6 hours to fully charge but can be free depending on the station. Call us if there's ANY confusion!</p>
+          <h3 id="is-it-true-i-can-drive-in-the-carpool-lane-on-the-highway">Is it true I can drive in the carpool lane on the highway?</h3>
+          <p>Yep! More magic.</p>
+          <h3 id="can-someone-else-drive-using-my-account">Can someone else drive using my account?</h3>
+          <p>This one's a big N.O. The ONLY person allowed to drive a WaiveCar is the person on the account who has booked the car. Big consequences if we find out otherwise!</p>
+          <h3 id="what-happens-if-theres-a-parking-ticket">What happens if there's a parking ticket?</h3>
+          <p>If you get a ticket, you have 10 days to pay it. If you don't pay it within that time, we will charge your account for the amount due. If you see a ticket from a previous user, simply put it in the glove box.</p>
+          <h2 id="ending-ride">Ending Ride</h2>
+          <h3 id="where-can-i-park-when-im-done">Where can I park when I'm done?</h3>
+          <p>If the battery is over 25% charged (20 miles), you can park at any 3 hour public parking spot in Santa Monica without paying the meter. If the battery is under 25% charged (20 miles) you MUST return it at the headquarters at 1547 7th Street.</p>
+          <p>However, after midnight, parking must be valid until 10:00 AM that morning.</p>
+          <h3 id="where-can-i-not-park">Where can I <strong>NOT</strong> park?</h3>
+          <p>Private parking spaces, parking garages, permit-only spots, any business parking lot, or ANYWHERE not in Santa Monica! Make sure to check street signs so that there are no parking restrictions on the spot for 3 hours.</p>
+          <p>Restricted parking includes:</p>
+          <ul>
+          <li>Street parking restricted by signs such as during street sweeping hours</li>
+          <li>Private lots (Whole Foods, Ralphs, etc.)</li>
+          <li>Private driveways &amp; Permit-only zones</li>
+          <li>Indoor public lots with less than 3 hours free parking</li>
+          <li>Upside down and engulfed in flames</li>
+          </ul>
+          <h3 id="is-it-true-i-dont-have-to-pay-any-parking-meter-in-santa-monica">Is it true I don't have to pay any parking meter in Santa Monica?</h3>
+          <p>Yep! Since we have the bumper sticker for fully electric cars, all parking meters in Santa Monica ONLY are free!</p>
+          <h3 id="how-do-i-end-my-ride">How do I end my ride?</h3>
+          <p>First make sure that you are parked in an accurate spot (see above). Turn off the car, put the key back on the sensor in the glovebox, grab your belongings, and exit the car. Select &quot;End Ride&quot; from the app and input the appropriate information. If you haven't received a confirmation text please call us at 855-WAIVE55!</p>
+          <h3 id="do-i-need-to-clean-the-car-when-im-done">Do I need to clean the car when I'm done?</h3>
+          <p>Yes! No need for an extensive cleaning, but make sure all your belongings are out of the car and clean up any trash you may leave. If you leave a mess, there could be a cleaning fee!</p>
+          <h3 id="do-i-need-to-charge-the-car-before-i-end-my-ride">Do I need to charge the car before I end my ride?</h3>
+          <p>Nope! Follow the rules above. If the car is under 25% charged (20 miles), simply return it at the HQ!</p>
+          <h3 id="fees">Fees</h3>
+          <p>There are some modest fees in order to keep our fleet bright and shiny.</p>
+          <ul>
+          <li><strong>Ending with under 20% charge and not brought back to WaiveCar's lot</strong> $20</li>
+          <li><strong>Dead Battery / Lights Left On</strong> $100</li>
+          <li><strong>Missing Key (Failure to return equipment)</strong> $300</li>
+          <li><strong>Parking Violation (Any violation of our parking rules)</strong> $40</li>
+          <li><strong>Special Cleaning</strong></li>
+          <li><strong>Smoking</strong> $200</li>
+          <li><strong>Pet not in carrier/odor</strong> $50</li>
+          <li><strong>Damage to the interior</strong> Our costs to repair</li>
+          <li><strong>Vehicle Recovery (if left outside Santa Monica)</strong> $300 minimum</li>
+          <li><strong>Declined Credit Card</strong> Charges + $5 penalty fee</li>
+          <li><strong>Excess Time (Non Waive time)</strong> $1 for every 10 mins. ($5.99/hour)</li>
+          </ul>
+          <p>There's a few unfortunate events that can happen where we have pass the costs on to you:</p>
+          <ul>
+          <li><strong>Public Garage Parking Fee</strong> Cost to remove vehicle from garage + $40 parking violation</li>
+          <li><strong>Vehicle Recovery (inside Santa Monica)</strong> Miscellaneous Recovery Costs</li>
+          <li><strong>Parking Ticket</strong> Ticket Price + $20.00 Handling fee (unless user pays themselves)</li>
+          <li><strong>Impounded Vehicle</strong> Cost of impound fee + $100 recovery</li>
+          </ul>
+          <p>If the car is physically damaged during the rental due to things such as an accident, we will hold $1,000 deposit and will return it to you if the insurance determines you are not at fault.</p>
+          <h3 id="restricted-parking">Restricted Parking</h3>
+          <p>Restricted parking includes:</p>
+          <ul>
+          <li>Street parking restricted by signs such as during street sweeping hours</li>
+          <li>Private lots (Whole Foods, Ralphs, etc.)</li>
+          <li>Private driveways &amp; Permit-only zones</li>
+          <li>Indoor public lots with less than 3 hours Free Parking</li>
+          </ul>
           </div>
         </div>
       </div>
