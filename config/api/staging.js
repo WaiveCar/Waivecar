58f2615c309c1a0054850d3c6d12957841d72e90
Chris McKenzie <kristopolous@yahoo.com>
2016-10-19 16:42:13 -0700

    adding http instead of https for staging

--- config/api/staging.js
+++ config/api/staging.js
@@ -1,10 +1,10 @@
 module.exports = {
   api : {
-    uri    : 'https://api-staging.waivecar.com',
+    uri    : 'http://api-staging.waivecar.com',
     //uri    : 'https://api-waivecar-dev.cleverbuild.biz',
     port   : 443,
     socket : {
-      uri     : 'https://api-staging.waivecar.com',
+      uri     : 'http://api-staging.waivecar.com',
       //uri     : 'https://api-waivecar-dev.cleverbuild.biz',
       options : {
         path : '/socket/socket.io'
