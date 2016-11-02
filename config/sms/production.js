0e9809765d657f31b780576c82affb628f78d908
Chris McKenzie <kristopolous@yahoo.com>
2016-11-01 18:23:24 -0700

    Changing twilio number to the 855 one

--- config/sms/production.js
+++ config/sms/production.js
@@ -2,7 +2,7 @@ module.exports = {
   sms : {
     transportName : 'twilio',
     transport     : {
-      phoneNumber : '+13234585511',
+      phoneNumber : '+18559248355',
       sid         : 'ACa061f336122514af845ea65fb1e6c2bb',
       token       : 'b39d95c162c1e9ad1893acbb61af8bb4'
     }
