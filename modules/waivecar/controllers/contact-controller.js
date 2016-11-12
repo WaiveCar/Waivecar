e433fc7b57b63732c8e4c46705ee1eeaa4f61113
Chris McKenzie <kristopolous@yahoo.com>
2016-11-11 19:05:04 -0800

    Adding an sms endpoint

--- modules/waivecar/controllers/contact-controller.js
+++ modules/waivecar/controllers/contact-controller.js
@@ -4,13 +4,13 @@ let service = require('../lib/contact-service');
 
 Bento.Register.Controller('ContactController', function(controller) {
 
-  /**
-   * Deliver contact message
-   * @return {Object}
-   */
   controller.send = function *() {
     return yield service.deliverMessage(this.payload, this.auth.user);
   };
+
+  controller.sms = function *() {
+    return yield service.deliverSms(this.url);
+  }
 
   return controller;
 
