c09fc7379db3ee78c4b7919954fdfc6939880b50
Chris McKenzie <kristopolous@yahoo.com>
2016-11-11 19:05:16 -0800

    Adding a lib to deliver sms and forward it

--- modules/waivecar/lib/contact-service.js
+++ modules/waivecar/lib/contact-service.js
@@ -1,13 +1,35 @@
 'use strict';
 
-let Email  = Bento.provider('email');
-let config = Bento.config;
-let notify       = require('./notification-service');
+let Email   = Bento.provider('email');
+let User    = Bento.model('User');
+let config  = Bento.config;
+let notify  = require('./notification-service');
+let request = require('co-request');
+let url     = require('url');
 
 module.exports = {
   *deliverMessage(payload, _user) {
     yield notify.slack({ text : `From: ${ _user.name() } <${ _user.email }> ${ _user.info() }\n Subject: ${ payload.subject || '_(none)_' }\n${ payload.message || '_(none)_' }` }, { channel : '#app_support' });
-  }/*,
+  },
+
+  *deliverSms(payload) {
+    let aircall_url = 'https://webhook.frontapp.com/sms/18f55053c49e4d3d27bbc4af0e7d78e97205c88981d8f954579682a382654912';
+    let params = url.parse(payload, true);
+    let phone = params.query.From;
+    let user = yield User.findOne({ where : { phone: phone } });
+    let who = user ? user.name() : '_unkonwn_';
+    let message = `${ who } (${ phone }): ${ params.query.Body }`;
+    yield notify.slack({ text : message }, { channel : '#app_support' });
+
+    params.query.Body = 'THIS IS A TEST!! ' + params.query.Body;
+
+    let response = yield request({
+      url     : aircall_url,
+      method  : 'POST',
+      form    : params.query
+    });
+  }
+  /*,
 
   *deliverMessageByEmail(payload, _user) {
     let email = new Email();
