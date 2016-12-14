c5957bd706251103c04339b0631dd3a021199410
Chris McKenzie <kristopolous@yahoo.com>
2016-12-13 16:51:56 -0800

    641: Admin should be able to update birthdates

--- src/views/app/admin/users/user-license.jsx
+++ src/views/app/admin/users/user-license.jsx
@@ -159,7 +159,7 @@ module.exports = class UserDetails extends React.Component {
             <div className="form-group row">
               <FormInput className="col-md-4 bento-form-input">
                 <label>Birth Date</label>
-                <input type="date" name="birthDate" className="form-control" defaultValue={ moment(license.birthDate.slice(0,-1)).format('YYYY-MM-DD') } disabled={ !license.provided } required />
+                <input type="date" name="birthDate" className="form-control" defaultValue={ moment(license.birthDate.slice(0,-1)).format('YYYY-MM-DD') } required />
               </FormInput>
               <FormInput className="col-md-4 bento-form-input">
                 <label>Gender</label>
