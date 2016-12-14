e93c543002cd7bccc37a06f93bb3f189a59ed093
Chris McKenzie <kristopolous@yahoo.com>
2016-12-13 23:01:44 -0800

    Adding prod2 to the deploy set

--- tools/build-prod.sh
+++ tools/build-prod.sh
@@ -2,5 +2,8 @@
 nvmsh on
 NODE_ENV=production node_modules/webpack/bin/webpack.js -p --config ./webpack/config/deployment.js 
 set -x
-scp -C app/scripts/{vendors,bundle}.js waive-prod:
+
+for i in `seq 1 2`; do
+  scp -C app/scripts/{vendors,bundle}.js waive-prod$i:
+done
 
