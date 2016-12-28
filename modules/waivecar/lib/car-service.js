b732831a2fde5a47cdf3a71c6da77f23f4d8c42f
Chris McKenzie <kristopolous@yahoo.com>
2016-12-27 23:53:44 -0800

    diagnostics on car page loadtime

--- modules/waivecar/lib/car-service.js
+++ modules/waivecar/lib/car-service.js
@@ -93,6 +93,7 @@ module.exports = {
    * @return {Object}
    */
   *show(id, _user) {
+    console.log(">> show start ", id, +new Date());
     let car = yield Car.findById(id, {
       include : [
         {
@@ -102,7 +103,9 @@ module.exports = {
       ]
     });
 
+    console.log(">> show find ", id, +new Date());
     let data  = yield hooks.call('cars:show:after', car);
+    console.log(">> show hooks ", id, +new Date());
 
     return data;
   },
