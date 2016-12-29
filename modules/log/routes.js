7ee9effb7d21da5732c62e9e51d2d1cfe191e8ca
Chris McKenzie <kristopolous@yahoo.com>
2016-12-28 20:44:31 -0800

    First pass at #623 - analytics reporting

--- modules/log/routes.js
+++ modules/log/routes.js
@@ -5,6 +5,7 @@ Route.post('/logs/:type', {
   params : [ 'origin' ]
 });
 
+Route.get('/stats/:day',     [ 'LogsController@stats' ]);
 Route.get('/logs/:type',     [ 'isAuthenticated', 'LogsController@index' ]);
 Route.get('/logs/:type/:id', [ 'isAuthenticated', 'LogsController@show' ]);
 Route.put('/logs/:type/:id', [ 'isAuthenticated', 'LogsController@update' ]);
