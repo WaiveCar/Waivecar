d42ef0de21da4fd9b16082d7a61d7c26611ff8de
Chris McKenzie <kristopolous@yahoo.com>
2017-01-13 19:46:48 -0800

    Moving the general editing to another screen

---
+++ www/js/controllers/user-edit-general-controller.js
@@ -0,0 +1,54 @@
+'use strict';
+var angular = require('angular');
+require('angular-ui-router');
+require('../services/auth-service');
+require('../services/data-service');
+require('../services/message-service');
+var _ = require('lodash');
+
+module.exports = angular.module('app.controllers').controller('UserEditGeneralController', [
+  '$rootScope',
+  '$scope',
+  '$state',
+  '$auth',
+  '$data',
+  '$message',
+  function($rootScope, $scope, $state, $auth, $data, $message) {
+
+    $scope.save = function(form) {
+      if (form.$invalid) {
+        return $message.error('Please fix form errors and try again.');
+      }
+
+      $scope.user.$save()
+        .then(function() {
+          $scope.init(function() {
+            return $message.success('Your details have been successfully updated');
+          });
+        })
+        .catch($message.error);
+    };
+
+
+    $scope.init = function(next) {
+
+      return $data.resources.users.me().$promise
+        .then(function(me) {
+          $scope.user = me;
+          return $data.resources.licenses.query().$promise;
+        }).then(function(licenses) {
+          $scope.license = _(licenses)
+            .filter({userId: $scope.user.id})
+            .sortBy('createdAt')
+            .last();
+          if (next) {
+            return next();
+          }
+        }).catch($message.error);
+    };
+
+    $scope.init();
+
+  }
+
+]);
