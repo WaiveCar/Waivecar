e71d8aa635d613b5d6282748774da03bbcfb6771
Chris McKenzie <kristopolous@yahoo.com>
2017-02-14 16:33:04 -0800

    Adding a way to deploy to prod

---
+++ tools/deploy-prod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+nvmsh on
+NODE_ENV=production node_modules/webpack/bin/webpack.js -p --config ./webpack/config/deployment.js 
+set -x
+
+for i in `seq 1 2`; do
+  scp -C app/scripts/{vendors,bundle}.js waive-prod$i:web-js
+  ssh waive-prod$i "sudo ./deploy-web.sh"
+done
+
