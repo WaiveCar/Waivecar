277f9411e33b75183010404a3d5bb22966d7ebb5
Chris McKenzie <kristopolous@yahoo.com>
2017-02-14 18:44:55 -0800

    Removing renamed file

--- tools/build-prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-nvmsh on
-NODE_ENV=production node_modules/webpack/bin/webpack.js -p --config ./webpack/config/deployment.js 
-set -x
-
-for i in `seq 1 2`; do
-  scp -C app/scripts/{vendors,bundle}.js waive-prod$i:web-js
-done
-
