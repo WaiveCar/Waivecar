f7739e527f1007949adb01ef364bbf58e49d77d1
Chris McKenzie <kristopolous@yahoo.com>
2017-03-06 10:53:59 -0800

    Adding a date to the stripe log

--- modules/shop/lib/stripe/charges.js
+++ modules/shop/lib/stripe/charges.js
@@ -8,7 +8,7 @@ module.exports = class StripeCharges {
   }
 
   log(action, options, response, extra) {
-    let payload = [action, options, response];
+    let payload = [new Date(), action, options, response];
 
     if(extra) {
       payload.push(extra);
