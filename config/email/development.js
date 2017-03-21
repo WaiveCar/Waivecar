008fed7248da47a30ac832ee12eca2218b9550c1
Chris McKenzie <kristopolous@yahoo.com>
2017-03-21 15:14:22 -0700

    Adding a single recipient to the email debug config

---
+++ config/email/development.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+  /*
+   |--------------------------------------------------------------------------------
+   | Email
+   |--------------------------------------------------------------------------------
+   |
+   | Provides an easy to use email transporter that can be used by any bento api
+   | module.
+   |
+   | @param {String}  templateFolder The location of email templates relative to the
+   |                                 api root.
+   | @param {Object}  sender         The default email address of the sender.
+   | @param {String}  transportName
+   | @param {Object}  transport
+   |
+   */
+
+  email : {
+    templateFolder : 'templates/email',
+    sender         : 'WaiveCar <support@waivecar.com>',
+    transportName  : 'mandrill',
+    recipient      : 'chris@waivecar.com',
+    transport      : {
+      auth : {
+        apiKey : 'gHCYiqaKVIzJqfPqbrtPOA'
+      }
+    }
+  }
+
+};
