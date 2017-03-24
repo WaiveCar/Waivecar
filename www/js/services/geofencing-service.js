ed26569b73ac9dabdeb418b01857f06c633a2c8e
Chris McKenzie <kristopolous@yahoo.com>
2017-03-23 18:49:07 -0700

    Potentially addressing #655

--- www/js/services/geofencing-service.js
+++ www/js/services/geofencing-service.js
@@ -11,7 +11,11 @@ function GeofencingService(LocationService, geofenceCoords) {
 
   this.insideBoundary = function insideBoundary() {
     return LocationService.getLocation().then(function(coords) {
-      return inside([coords.longitude, coords.latitude], geofenceCoords);
+      if(coords && 'longitude' in coords) {
+        return inside([coords.longitude, coords.latitude], geofenceCoords);
+      } else {
+        return false;
+      }
     });
   };
 }
