eec830a43bdbab9d5bbd96f91fdd4a9cbb067237
Chris McKenzie <kristopolous@yahoo.com>
2017-04-12 08:02:43 -0700

    Changing the verbaige of the second warning to the car being started

--- modules/waivecar/bootstrap/schedules/booking-forfeiture-second-warning.js
+++ modules/waivecar/bootstrap/schedules/booking-forfeiture-second-warning.js
@@ -4,7 +4,7 @@ let notify    = require('../../lib/notification-service');
 let scheduler = Bento.provider('queue').scheduler;
 
 scheduler.process('booking-forfeiture-second-warning', function *(job) {
-  yield notify.sendTextMessage(job.data.phone, `Hi, it looks like you haven't started your ride yet. Unless you start in about 5 minutes, we'll have to end your booking and make the car available for other users. Please call us if there's difficulty`);
+  yield notify.sendTextMessage(job.data.userId, `Hi, it looks like you haven't started driving yet. Please start in the next 5 minutes or we'll have to end your booking and make the car available for other users. Please call us if there's difficulty`);
 });
 
 module.exports = function *() {
