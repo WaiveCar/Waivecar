2c58de072bf4fc882c050478e763fbac34857c99
Chris McKenzie <kristopolous@yahoo.com>
2017-04-12 08:04:42 -0700

    Fixing a bug in how text messages were being sent for forfeitures

--- modules/waivecar/bootstrap/schedules/booking-forfeiture-first-warning.js
+++ modules/waivecar/bootstrap/schedules/booking-forfeiture-first-warning.js
@@ -4,7 +4,7 @@ let notify    = require('../../lib/notification-service');
 let scheduler = Bento.provider('queue').scheduler;
 
 scheduler.process('booking-forfeiture-first-warning', function *(job) {
-  yield notify.sendTextMessage(job.data.phone, `Hi, we notice you haven't started the car. Feel free to call us if you are having troubles.`);
+  yield notify.sendTextMessage(job.data.userId, `Hi, we notice you haven't started the car. Feel free to call us if you are having troubles.`);
 });
 
 module.exports = function *() {
