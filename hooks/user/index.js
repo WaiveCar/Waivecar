47121c4819d61ba48f074cdcbde12eeee28ef5a5
Chris McKenzie <kristopolous@yahoo.com>
2017-06-20 17:30:38 -0700

    miscellaneous fixes

--- hooks/user/index.js
+++ hooks/user/index.js
@@ -197,7 +197,7 @@ hooks.set('user:update:before', function *(prevUser, nextUser, _user) {
     if (reason.length) {
       who += ' (' + reason.join(', ') + ')';
     }
-    yield UserLog.addUserEvent(prevUser, 'PENDING', null, [reason || []].join(', '));
+    yield UserLog.addUserEvent(prevUser, 'PENDING', _user.id, [reason || []].join(', '));
     yield notify.notifyAdmins(`:construction: ${ prevUser.name() }, a previously active user, has been moved to pending ${ who } | ${ Bento.config.web.uri }/users/${ prevUser.id }`, [ 'slack' ], { channel : '#user-alerts' });
   }
 
