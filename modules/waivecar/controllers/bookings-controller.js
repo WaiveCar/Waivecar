04c24332bfa259751573539cb9b581b00c4a8d2f
Chris McKenzie <kristopolous@yahoo.com>
2017-08-09 22:23:44 -0700

    push forward android req to build 873

--- modules/waivecar/controllers/bookings-controller.js
+++ modules/waivecar/controllers/bookings-controller.js
@@ -17,7 +17,7 @@ Bento.Register.Controller('BookingsController', function(controller) {
     let iPhone = header.match(/iPhone/);
 
     // App versions between android and iphone are dramatically different for some reason.
-    let minApp = iPhone ? 91 : 868; 
+    let minApp = iPhone ? 91 : 872; 
     let minMarketLink = 797; 
     let version = parseInt(payload.version, 10) || 0;
     var copy;
