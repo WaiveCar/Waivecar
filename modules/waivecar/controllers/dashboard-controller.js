9935e992158976154c8758d5961e3e8f945b0fc7
paladin4ua <vkubiv@emp.org.ua>
2017-08-11 16:41:13 +0300

    fix dashboard Users and Bookings counts #886

---
+++ modules/waivecar/controllers/dashboard-controller.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let error          = Bento.Error;
+let User           = Bento.model('User');
+let Booking        = Bento.model('Booking');
+
+Bento.Register.Controller('DashboardController', function(controller) {
+
+
+  controller.index = function *() {
+
+    var oneWeekAgo = new Date(new Date() - 24 * 60 * 60 * 1000 * 7);
+
+    var nowQuery = {};
+
+    var weekAgoQuery = { where : { createdAt: { $lt: oneWeekAgo }}};
+
+
+    var bookingsCount = {
+      now: yield Booking.count(nowQuery),
+      weekAgo: yield Booking.count(weekAgoQuery)
+    };
+
+    var usersCount = {
+      now: yield User.count(nowQuery),
+      weekAgo: yield User.count(weekAgoQuery)
+    };
+
+    return {
+      bookingsCount : bookingsCount,
+      usersCount : usersCount
+    }
+  };
+
+
+  return controller;
+
+});
