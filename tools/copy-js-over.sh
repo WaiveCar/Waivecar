853d9a22bff73bb378167d96e5734c2ed9f4df59
Chris McKenzie <kristopolous@yahoo.com>
2017-08-15 15:52:18 -0700

    adding a rollback to the copy

--- tools/copy-js-over.sh
+++ tools/copy-js-over.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
+# This is run on the production machines.  The
+# new versions of the js should be in web-js/new
 set -x
 cd web-js
-sudo cp vendors.js bundle.js /opt/waivecar-web/scripts/
-sudo cp *.* last
+
+goforward() {
+  # We preserve the timestamp and copy over whatever was
+  # last being served over the wire as our previous version
+  cp -p /opt/waivecar-web/scripts/{vendors,bundle.js} last
+
+  # Now we move over the new stuff
+  sudo cp new/{vendors,bundle}.js /opt/waivecar-web/scripts/
+}
+
+undo() {
+  # We preserve the timestamp and copy over whatever was
+  # last being served over the wire as our previous version
+  cp -p last/{vendors,bundle}.js /opt/waivecar-web/scripts/
+}
+
+# if any argument is given then we undo our changes
+if [ $1 ]; then
+  undo
+else
+  goforward
+fi
