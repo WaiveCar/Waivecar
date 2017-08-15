41af2a32698ea5cb55adb41298af42f139408a02
Chris McKenzie <kristopolous@yahoo.com>
2017-08-15 15:06:42 -0700

    Adding a deploy history to the deploy script.

--- tools/deploy-prod.sh
+++ tools/deploy-prod.sh
@@ -2,6 +2,7 @@
 nvmsh on
 echo -n "Last was: "; cat .last-deploy
 git describe > .last-deploy
+echo `date` `git describe` >> .deploy-history
 echo -n "This is: "; cat .last-deploy
 
 NODE_ENV=production node_modules/webpack/bin/webpack.js -p --config ./webpack/config/deployment.js 
