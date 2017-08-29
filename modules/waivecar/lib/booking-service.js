7f0bcfee9cff58b185a4904cb43f1451836d08b6
Chris McKenzie <kristopolous@yahoo.com>
2017-08-29 16:07:21 -0700

    changing percentages to miles in sms messages

--- modules/waivecar/lib/booking-service.js
+++ modules/waivecar/lib/booking-service.js
@@ -490,7 +490,7 @@ module.exports = class BookingService extends Service {
         yield notify.sendTextMessage(user, `Thanks for using WaiveWork! Your booking has started.`);
       }
       else{
-        yield notify.sendTextMessage(user, `Your WaiveCar rental has started! The first 2 hours are completely FREE! After that, it's $5.99 / hour. Make sure to return the car in Santa Monica, don't drain the battery under 25%, and keep within our driving borders to avoid any charges. Thanks for renting with WaiveCar!`);
+        yield notify.sendTextMessage(user, `Your WaiveCar rental has started! The first 2 hours are completely FREE! After that, it's $5.99 / hour. Make sure to return the car in Santa Monica, don't drain the battery under 25mi, and keep within our driving borders to avoid any charges. Thanks for renting with WaiveCar!`);
       }
 
       // ### Relay Update
