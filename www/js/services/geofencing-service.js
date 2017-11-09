394f79e721f7934a4b4bbb70677b9bcc6b7d9986
Chris McKenzie <kristopolous@yahoo.com>
2017-11-09 02:06:34 -0800

    Adding a non-promised fast check to the geofence service

--- www/js/services/geofencing-service.js
+++ www/js/services/geofencing-service.js
@@ -5,19 +5,28 @@ var inside = require('point-in-polygon');
 angular.module('app.services')
   .service('GeofencingService', GeofencingService);
 
-GeofencingService.$inject = ['LocationService', 'geofenceCoords'];
+GeofencingService.$inject = ['LocationService', 'geofenceCoords', '$rootScope'];
 
-function GeofencingService(LocationService, geofenceCoords) {
+function GeofencingService(LocationService, geofenceCoords, $rootScope) {
+
+  this.insideFastCheck = function insideBoundary(car, shape) {
+    shape = shape || geofenceCoords;
+    coords = $rootScope.currentLocation || car;
+    if(coords && 'longitude' in coords) {
+      return inside([coords.longitude, coords.latitude], shape);
+    }
+  };
 
   // backup is used if the location services fail to get a gps read
   // this should probably be the car gps
-  this.insideBoundary = function insideBoundary(backup) {
+  this.insideBoundary = function insideBoundary(backup, shape) {
+    shape = shape || geofenceCoords;
     return LocationService.getLocation().then(function(coords) {
       if(!coords && backup) {
         coords = backup;
       }
       if(coords && 'longitude' in coords) {
-        return inside([coords.longitude, coords.latitude], geofenceCoords);
+        return inside([coords.longitude, coords.latitude], shape);
       } else {
         return false;
       }
