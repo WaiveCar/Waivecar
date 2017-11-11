2ad94c6e6c3d57d9d201495b40d0966f2de59f6a
Chris McKenzie <kristopolous@yahoo.com>
2017-11-10 20:21:56 -0800

    finishing touches for the damage gallery

--- www/js/controllers/show-problem-controller.js
+++ www/js/controllers/show-problem-controller.js
@@ -1,26 +1,30 @@
 'use strict';
 var angular = require('angular');
+var moment = require('moment');
 
-
-function ShowProblemController ($injector) {
-
+function ShowProblemController ($injector, $sce) {
   var $data = $injector.get('$data');
+  var $window = $injector.get('$window');
 
+  var description = $data.active.damagePhoto.description;
+  if(description) {
+    description = $sce.trustAsHtml(description.replace(/\n/g, '<br>'));
+  } else {
+    description = '(No description provided)';
+  }
   this.model = {
-    file: prepareResult($data.active.damagePhoto),
-    comment: $data.active.damagePhoto.description
+    file: $data.active.damagePhoto.url,
+    comment: description,
+    time: moment($data.active.damagePhoto.createdAt).format('MMM D h:mm:ssA')
   };
 
-  function prepareResult (file) {
-    file.style = {
-      'background-image': 'url(' + file.url + ')'
-    };
-    return file;
-  }
+  this.back = function() {
+    $window.history.back();
+  };
 }
 
 module.exports = angular.module('app.controllers').controller('ShowProblemController', [
   '$injector',
-  '$stateParams',
+  '$sce',
   ShowProblemController
 ]);
