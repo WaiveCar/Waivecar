611f461c9390e712bf1024ffd30456ee239ccd3b
Chris McKenzie <kristopolous@yahoo.com>
2017-12-13 20:11:54 -0800

    Fixing a state bug in the menu

--- www/js/controllers/menu-controller.js
+++ www/js/controllers/menu-controller.js
@@ -6,7 +6,21 @@ function MenuController ($scope, $auth, $state, $data) {
   this.$auth = $auth;
   $scope.$data = $data;
 
-  this.logout = function logout () {
+  this.currentRide = function() {
+    var booking = $data.active.bookings;
+    var stateMap = {
+      reserved: 'bookings-active',
+      started: 'dashboard',
+      ended: 'end-ride'
+    };
+    if(stateMap[booking.status]) { 
+      $state.go(stateMap[booking.status], {id: booking.id});
+    } else {
+      console.log('UNHANDLED', booking.status);
+    }
+  }
+
+  this.logout = function() {
     $auth.logout();
     $data.resources.cars.disconnect();
     $state.go('auth');
