aea4cb9866cf26c0459e29ce0abb0f90726cb3d0
Chris McKenzie <kristopolous@yahoo.com>
2017-12-13 23:06:13 -0800

    minor booking flow cleanup

--- www/js/directives/car-charge-status-directive.js
+++ www/js/directives/car-charge-status-directive.js
@@ -10,7 +10,7 @@ module.exports = angular.module('app.directives')
 
         function setInfo(car){
           var ratio;
-          if(!car){
+          if(!car) {
             return;
           } 
           if(car.model === 'Spark EV') {
@@ -18,14 +18,14 @@ module.exports = angular.module('app.directives')
           } else {
             ratio = 1.35
           }
+           
+          Object.assign($scope, car);
           car.charge = car.charge || 0;
           car.range = car.range || 0;
 
           $scope.chargeLevel = Math.min(car.charge, 100) + '%';
           $scope.chargeState = car.isCharging ? 'is charging' : 'is not charging';
           $scope.chargeReach = (car.range || car.charge * ratio).toFixed(2) + ' miles ';
-          $scope.license = car.license;
-
         }
 
         $scope.$watch('car', setInfo);
