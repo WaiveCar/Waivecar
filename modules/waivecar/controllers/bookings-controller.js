f5238d619e42ead9e331737b93129ba8583686e5
Chris McKenzie <kristopolous@yahoo.com>
2017-12-14 21:07:44 -0800

    no longer forcing android update

--- modules/waivecar/controllers/bookings-controller.js
+++ modules/waivecar/controllers/bookings-controller.js
@@ -17,7 +17,7 @@ Bento.Register.Controller('BookingsController', function(controller) {
     let iPhone = header.match(/iPhone/);
 
     // App versions between android and iphone are dramatically different for some reason.
-    let minApp = iPhone ? 112 : 892;
+    let minApp = iPhone ? 112 : 792;
     let minMarketLink = 797; 
     let version = parseInt(payload.version, 10) || 0;
     var copy;
