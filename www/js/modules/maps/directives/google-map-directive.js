651cfbb55c7752d88444ed6f5b7468dd95f70924
paladin4ua <vkubiv@emp.org.ua>
2017-12-27 18:03:20 +0200

    fix zone marker

--- www/js/modules/maps/directives/google-map-directive.js
+++ www/js/modules/maps/directives/google-map-directive.js
@@ -564,6 +564,14 @@ function directive($rootScope, MapsLoader, RouteService, $q, $timeout, $window, 
           anchor: new google.maps.Point(12, 12),
           origin: new google.maps.Point(0, 0)
         };
+      case 'zone':
+        return {
+          url: '',
+          iconRetinaUrl: '',
+          scaledSize: new google.maps.Size(0, 0),
+          anchor: new google.maps.Point(0, 0),
+          origin: new google.maps.Point(0, 0)
+        };
       default:
         return {
           url: 'img/user-location' + fileExt,
