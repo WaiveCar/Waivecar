e37542afc260b8cddc53c5bf29eb725171b5ea89
Andrii Machniev <amachnew@lvivcode.com>
2018-02-09 17:24:49 +0200

    Added stations for testing

--- modules/waivecar/controllers/evgo-controller.js
+++ modules/waivecar/controllers/evgo-controller.js
@@ -14,20 +14,9 @@ Bento.Register.Controller('EvgoController', function(controller) {
         //return yield service.authorize();
 
         //for test
-        return (yield Location.find({ where:
-            {
-                type: { $in: ['hub', 'zone', 'homebase'] }
-                // only return entries after the date below ... we are considering
-                // all the older ones to essentially be bullshit
-                //created_at: { $gt: new Date(2017, 8, 1) }
-            }
-            })).map((row) => {
-                if(row.shape) {
-            row.shape = JSON.parse(row.shape);
-        }
-        return row;
-    });
+        return (yield Location.find({
+            where: {type: { $in: ['station']}}
+        }));
     };
-
     return controller;
 });
