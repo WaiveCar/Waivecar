568aa3067e225ccb22b7374329be00907a6863ee
Chris McKenzie <kristopolous@yahoo.com>
2018-03-16 01:43:00 -0700

    Simplifying some monitoring script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ -e app/scripts/vendors.js ] && rm app/scripts/vendors.js
 [ -e ~/bin/nvmsh ] && source ~/bin/nvmsh
-nvm use v4.2.6
+#nvm use v4.2.6
 ENV=development
 [ -e .env ] && source .env
 NODE_ENV=$ENV node webpack.local.js
