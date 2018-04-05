90144909d5a736200c6e00a43a71a8e97e63ff66
Chris McKenzie <kristopolous@yahoo.com>
2018-04-04 17:34:36 -0700

    Initializing empty lists in forms for the post/put

--- src/modules/bento/lib/helpers/form.js
+++ src/modules/bento/lib/helpers/form.js
@@ -3,18 +3,23 @@ var _ = require('lodash');
 
 module.exports = class Form {
 
-  addValue(key, value) {
-    // if we are using the classic PHP style naming convention
-    // for arrays then make sure we do this in an array 
+  initialize(key) {
     if(key.indexOf('[]') !== -1) {
       key = key.slice(0,-2);
       if(!_.isArray(this.data[key])) {
         this.data[key] = [];
       }
-      this.data[key].push(value);
-    } else { 
-      this.data[key] = value;
     }
+  }
+
+  addValue(key, value) {
+    // if we are using the classic PHP style naming convention
+    // for arrays then make sure we do this in an array 
+    if(key.indexOf('[]') !== -1) {
+      key = key.slice(0,-2);
+      return this.data[key].push(value);
+    } 
+    this.data[key] = value;
   }
 
   constructor(event) {
@@ -22,6 +27,9 @@ module.exports = class Form {
     for (let i = 0, len = event.target.length; i < len; i++) {
       let el      = event.target[i];
       let isCheck = [ 'radio', 'checkbox' ].indexOf(el.type) > -1;
+
+      this.initialize(el.name);
+
       if (el.name && !isCheck) {
         this.addValue(el.name, el.value);
       } else if (isCheck && el.checked) {
