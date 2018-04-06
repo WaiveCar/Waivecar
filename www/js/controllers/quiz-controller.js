a2e41b1450593039b9f522cda6547d5287792986
Chris McKenzie <kristopolous@yahoo.com>
2018-04-06 16:36:39 -0700

    Making sure the quiz is set up after the user context is established

--- www/js/controllers/quiz-controller.js
+++ www/js/controllers/quiz-controller.js
@@ -7,7 +7,74 @@ function QuizController($injector, $stateParams, $scope, $interval, $data){
   var $window = $injector.get('$window');
   var $state = $injector.get('$state');
 
+  function setupQuiz() {
+    if($data.me.hasTag('level')) {
+      $scope.questionList = [
+        {
+          prompt: 'Who is allowed in a WaiveCar?',
+          answerList: [
+            [ 0, 'Only level residents', 'Guests are allowed!' ],
+            [ 1, 'Anyone, but only the renter can drive', 'Correct. Only level residents can rent, and the renter must drive'],
+            [ 0, 'Anyone can come in and anyone can drive', 'Sorry. Driving is restricted to the renter.'],
+            [ 0, 'The cars are completely autonomous and drive themselves!', "Oh don't you wish! Maybe in a few years."]
+          ]
+        },
+        {
+          prompt: 'WaiveCars are free for the first 2 hours. When may there be charges?',
+          answerList: [
+            [ 0, 'Never. I can do whatever I please!', 'Unfortunately there\'s a bit of responsibilty.' ],
+            [ 0, 'If I get a ticket, am at fault for an accident or otherwise damage the car', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
+            [ 0, 'If I abandon the car at a really low charge or make it really messy', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
+            [ 1, 'Both B and C', 'Correct. Unfortunately if you get speeding tickets, that\'s still on you.' ]
+          ]
+        },
+        {
+          prompt: 'I can end my ride at',
+          answerList: [
+            [ 0, 'Any parking garage', 'Almost! We are specific on our garages.' ],
+            [ 0, 'The shoulder of the New Jersey Turnpike', "That's silly. I mean come on, really?" ],
+            [ 0, 'Any street parking space', 'The cars have to be convenient for other residents.' ],
+            [ 1, 'The 24 hour lot at 34 North 7th Street', 'Correct Again! All bookings must end here.' ]
+          ]
+        }
+      ];
+    } else {
+      $scope.questionList = [
+        {
+          prompt: 'When ending your ride make sure your spot is Legal and Free for at least',
+          answerList: [
+            [ 0, 'I am not responsible for where I park the car', 'That would be chaos!' ],
+            [ 0, '1 hour', 'It\'s a bit longer than 1 hour.'],
+            [ 1, '3 hours', 'That\'s right! As long as the car is good for the next 3 hours, you\'re good.'],
+            [ 0, '6 hours', 'Nope. Not that long!']
+          ]
+        },
+        {
+          prompt: 'WaiveCars are free for the first 2 hours. When may there be charges?',
+          answerList: [
+            [ 0, 'Never. I can do whatever I please!', 'Unfortunately there\'s a bit of responsibilty.' ],
+            [ 0, 'If I get a ticket, am at fault for an accident or otherwise damage the car', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
+            [ 0, 'If I abandon the car  at a really low charge or make it really messy', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
+            [ 1, 'Both B and C', 'Correct. Unfortunately if you get speeding tickets, that\'s still on you.' ]
+          ]
+        },
+        {
+          prompt: 'I can return my low (25mi or lower) charge car to',
+          answerList: [
+            [ 0, 'Whole Foods Charger', 'Woops. Whole Foods doesn\'t really like that.' ],
+            [ 0, 'Public Parking Garage Charger', 'Almost! We like to service and clean our low cars.' ],
+            [ 0, 'Any 3 Hour Spot', 'The car might not make it back to us if we allowed this.' ],
+            [ 1, 'WaiveCar HQ', 'Correct Again! We use the car being low as a good time to make it fresh and clean.' ]
+          ]
+        }
+      ];
+    }
+  }
+
   $scope.init = function(){
+    // we start with an empty list to make sure
+    // we don't get any weird errors.
+    $scope.questionList = [];
     $scope.isWizard = $stateParams.step;
     $scope.user = $auth.me;
     $scope.timer = 180;
@@ -17,6 +84,7 @@ function QuizController($injector, $stateParams, $scope, $interval, $data){
       .then(function(me) {
         $scope.user = me;
         $scope.user.hasTag = $data.me.hasTag;
+        setupQuiz();
       });
 
 
@@ -35,67 +103,6 @@ function QuizController($injector, $stateParams, $scope, $interval, $data){
 
   $scope.init();
 
-  if($data.me.hasTag('level')) {
-    $scope.questionList = [
-      {
-        prompt: 'Who is allowed in a WaiveCar?',
-        answerList: [
-          [ 0, 'Only level residents', 'Guests are allowed!' ],
-          [ 1, 'Anyone, but only the renter can drive', 'Correct. Only level residents can rent, and the renter must drive'],
-          [ 0, 'Anyone can come in and anyone can drive', 'Sorry. Driving is restricted to the renter.'],
-          [ 0, 'The cars are completely autonomous and drive themselves!', "Oh don't you wish! Maybe in a few years."]
-        ]
-      },
-      {
-        prompt: 'WaiveCars are free for the first 2 hours. When may there be charges?',
-        answerList: [
-          [ 0, 'Never. I can do whatever I please!', 'Unfortunately there\'s a bit of responsibilty.' ],
-          [ 0, 'If I get a ticket, am at fault for an accident or otherwise damage the car', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
-          [ 0, 'If I abandon the car at a really low charge or make it really messy', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
-          [ 1, 'Both B and C', 'Correct. Unfortunately if you get speeding tickets, that\'s still on you.' ]
-        ]
-      },
-      {
-        prompt: 'I can end my ride at',
-        answerList: [
-          [ 0, 'Any parking garage', 'Almost! We are specific on our garages.' ],
-          [ 0, 'The shoulder of the New Jersey Turnpike', "That's silly. I mean come on, really?" ],
-          [ 0, 'Any street parking space', 'The cars have to be convenient for other residents.' ],
-          [ 1, 'The 24 hour lot at 34 North 7th Street', 'Correct Again! All bookings must end here.' ]
-        ]
-      }
-    ];
-  } else {
-    $scope.questionList = [
-      {
-        prompt: 'When ending your ride make sure your spot is Legal and Free for at least',
-        answerList: [
-          [ 0, 'I am not responsible for where I park the car', 'That would be chaos!' ],
-          [ 0, '1 hour', 'It\'s a bit longer than 1 hour.'],
-          [ 1, '3 hours', 'That\'s right! As long as the car is good for the next 3 hours, you\'re good.'],
-          [ 0, '6 hours', 'Nope. Not that long!']
-        ]
-      },
-      {
-        prompt: 'WaiveCars are free for the first 2 hours. When may there be charges?',
-        answerList: [
-          [ 0, 'Never. I can do whatever I please!', 'Unfortunately there\'s a bit of responsibilty.' ],
-          [ 0, 'If I get a ticket, am at fault for an accident or otherwise damage the car', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
-          [ 0, 'If I abandon the car  at a really low charge or make it really messy', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
-          [ 1, 'Both B and C', 'Correct. Unfortunately if you get speeding tickets, that\'s still on you.' ]
-        ]
-      },
-      {
-        prompt: 'I can return my low (25mi or lower) charge car to',
-        answerList: [
-          [ 0, 'Whole Foods Charger', 'Woops. Whole Foods doesn\'t really like that.' ],
-          [ 0, 'Public Parking Garage Charger', 'Almost! We like to service and clean our low cars.' ],
-          [ 0, 'Any 3 Hour Spot', 'The car might not make it back to us if we allowed this.' ],
-          [ 1, 'WaiveCar HQ', 'Correct Again! We use the car being low as a good time to make it fresh and clean.' ]
-        ]
-      }
-    ];
-  }
   $scope.guessList = [];
   $scope.wrongAppend = ' Please review and try again.';
 
