268481cb89b0d6bf7493cf5ae2bb10d10256c3bb
Chris McKenzie <kristopolous@yahoo.com>
2018-04-18 21:11:35 -0700

    code cleanup

--- modules/waivecar/bootstrap/jobs.js
+++ modules/waivecar/bootstrap/jobs.js
@@ -2,19 +2,14 @@
 let config = Bento.config.waivecar;
 
 module.exports = function *() {
-  if (!Bento.isTesting()) {
-    yield require('./schedules/booking-auto-cancel');
-    yield require('./schedules/booking-auto-lock');
-    yield require('./schedules/booking-free-timer');
-    yield require('./schedules/booking-free-timer-expired');
-    yield require('./schedules/booking-forfeiture-first-warning');
-    yield require('./schedules/booking-forfeiture-second-warning');
-    yield require('./schedules/booking-forfeiture');
-    yield require('./schedules/active-booking');
-    yield require('./schedules/cars-sync');
-    yield require('./schedules/cars-status');
-    if (config.mock.cars) {
-      yield require('./schedules/cars-mock-locations');
-    }
-  }
+  yield require('./schedules/booking-auto-cancel');
+  yield require('./schedules/booking-auto-lock');
+  yield require('./schedules/booking-free-timer');
+  yield require('./schedules/booking-free-timer-expired');
+  yield require('./schedules/booking-forfeiture-first-warning');
+  yield require('./schedules/booking-forfeiture-second-warning');
+  yield require('./schedules/booking-forfeiture');
+  yield require('./schedules/active-booking');
+  yield require('./schedules/cars-sync');
+  yield require('./schedules/cars-status');
 };
