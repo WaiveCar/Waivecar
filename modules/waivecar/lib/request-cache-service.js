92bc8168c078dbfb1fa138953e38b49f2c024a59
Chris McKenzie <kristopolous@yahoo.com>
2018-05-16 17:43:18 -0700

    Only caching get requests

--- modules/waivecar/lib/request-cache-service.js
+++ modules/waivecar/lib/request-cache-service.js
@@ -8,32 +8,34 @@ let CACHE = 'request-cache';
 let MAXAGE = 1000 * 60 * 60 * 18;
 
 module.exports = function*(params, opts) {
-  let cache_point = `${CACHE}/${md5(JSON.stringify(params))}`;
-  opts = opts || {};
-  opts.maxAge = opts.maxAge || MAXAGE;
+  if (!params.method || params.method.toLowerCase() === 'get') {
+    let cache_point = `${CACHE}/${md5(JSON.stringify(params))}`;
+    opts = opts || {};
+    opts.maxAge = opts.maxAge || MAXAGE;
 
-  // this logic is written in a way to reduce the i/o. sync
-  // calls can block (in fact, they've been a problem).
-  let stat = false;
-  try {
-    stat = fs.statSync(cache_point);
-  } catch(ex) { }
+    // This logic is written in a way to reduce the i/o. sync
+    // calls can block (in fact, they've been a problem).
+    let stat = false;
+    try {
+      stat = fs.statSync(cache_point);
+    } catch(ex) { }
 
-  if(!stat && !fs.existsSync(CACHE)) {
-    fs.mkdirSync(CACHE);
+    if(!stat && !fs.existsSync(CACHE)) {
+      fs.mkdirSync(CACHE);
+    }
+
+    if(stat && !opts.force) {
+
+      let age = new Date() - stat.ctime;
+
+      // This is the only cache exit point.
+      if (age < opts.maxAge) {
+        return { body: fs.readFileSync(cache_point) };
+      }
+    } 
   }
 
-  if(stat && !opts.force) {
-
-    let age = new Date() - stat.ctime;
-
-    // this is the only cache exit point.
-    if (age < opts.maxAge) {
-      return { body: fs.readFileSync(cache_point) };
-    }
-  } 
-
-  // if we make it to here then we are getting another copy.
+  // If we make it to here then we are getting another copy.
   let reqResponse = yield request(params);
 
   fs.writeFile(cache_point, reqResponse.body);
