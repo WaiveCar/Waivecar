1f0e20131e29e695954a1dd106ad8e903a39479e
Chris McKenzie <kristopolous@yahoo.com>
2018-05-17 04:49:50 -0700

    Updating the magic

--- src/views/app/admin/cars/magic.jsx
+++ src/views/app/admin/cars/magic.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import { api, relay, dom }  from 'bento';
+import { api, auth, relay, dom }  from 'bento';
 
 module.exports = class Magic extends React.Component {
 
   constructor(...args) {
     super(...args);
     this.state = {
+      action: null,
       car : null
     };
     dom.setTitle('Magic');
   }
 
-  findCar(position) {
+  doAct(position) {
     console.log(position, arguments);
     let act = this.props.params && this.props.params.act || 'unlock';
-    api.put('/magic/' + act + '?latitude=' + position.coords.latitude + '&longitude=' + position.coords.longitude, {}, (err, res) => {
+    api.put(`/magic/${ act }?latitude=${ position.coords.latitude }&longitude=${ position.coords.longitude }`, {}, (err, res) => {
       if(res.car) {
-        this.setState({car: res.car.license });
+        this.setState({action: act, car: res.car });
       } else {
-        this.setState({car: 'failed' });
+        this.setState({car: { license: 'failed' } });
       }
+    });
+  }
+
+  lock() {
+    api.put(`/car/${ this.state.car.id }/lock}`, {}, (err, res) => {
+      this.setState({ action: lock });
+    });
+  }
+  unlock() {
+    api.put(`/car/${ this.state.car.id }/unlock}`, {}, (err, res) => {
+      this.setState({ action: unlock });
     });
   }
 
@@ -27,19 +39,30 @@ module.exports = class Magic extends React.Component {
     console.log("failure", what);
   }
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(this.findCar.bind(this), this.fail.bind(this), {
+  findMe() {
+    navigator.geolocation.getCurrentPosition(this.doAct.bind(this), this.fail.bind(this), {
       enableHighAccuracy: true,
       timeout: 5000,
       maximumAge: 0
     });
   }
+  componentDidMount() {
+    this.findMe();
+  }
 
   render() {
+    dom.setTitle(`${ this.state.action } Magic`);
+
     return (
-        <div>
-`         <h1> { this.state.car } </h1>
-        </div>
+      <div>
+        <h1> { this.state.action }{ this.state.car ? this.state.car.license : '' } </h1>
+        { this.state.action ?
+          <div>
+            <button className="btn" onClick={ this.lock() }>lock</button>
+            <button className="btn" onClick={ this.unlock() }>unlock</button>
+          </div> : ''
+        }
+      </div>
     );
   }
 }
