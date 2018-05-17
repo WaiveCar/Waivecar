35f1619fd14935fe12105a9d31ebe6eaecf33f2f
Chris McKenzie <kristopolous@yahoo.com>
2018-05-16 17:02:59 -0700

    Adding a caching layer for evgo

---
+++ modules/waivecar/lib/request-cache-service.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let request = require('co-request');
+let md5     = require('md5');
+let fs = require('fs');
+
+let CACHE = 'request-cache';
+let MAXAGE = 1000 * 60 * 60 * 18;
+
+module.exports = function*(params, opts) {
+  let cache_point = `${CACHE}/${md5(JSON.stringify(params))}`;
+  opts = opts || {};
+  opts.maxAge = opts.maxAge || MAXAGE;
+
+  // this logic is written in a way to reduce the i/o. sync
+  // calls can block (in fact, they've been a problem).
+  let stat = false;
+  try {
+    stat = fs.statSync(cache_point);
+  } catch(ex) { }
+
+  if(!stat && !fs.existsSync(CACHE)) {
+    fs.mkdirSync(CACHE);
+  }
+
+  if(stat && !opts.force) {
+
+    let age = new Date() - stat.ctime;
+
+    // this is the only cache exit point.
+    if (age < opts.maxAge) {
+      return { body: fs.readFileSync(cache_point) };
+    }
+  } 
+
+  // if we make it to here then we are getting another copy.
+  let reqResponse = yield request(params);
+
+  fs.writeFile(cache_point, reqResponse.body);
+
+  return { body: reqResponse.body };
+};
