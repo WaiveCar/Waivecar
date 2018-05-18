2cc387a133097356d3bd978526ad403bd55d8a6d
Chris McKenzie <kristopolous@yahoo.com>
2018-05-18 00:26:37 -0700

    Updating the magic page

--- src/views/app/admin/cars/magic.jsx
+++ src/views/app/admin/cars/magic.jsx
@@ -12,27 +12,34 @@ module.exports = class Magic extends React.Component {
     dom.setTitle('Magic');
   }
 
-  doAct(position) {
+  doTrick(position) {
     this.setState({action: 'processing...'});
-    let act = this.props.params && this.props.params.act || 'unlock';
+    let act = this.props.params && this.props.params.act || 'retrieve';
     api.put(`/magic/${ act }?latitude=${ position.coords.latitude }&longitude=${ position.coords.longitude }`, {}, (err, res) => {
       if(res.car) {
         this.setState({action: act, car: res.car });
+      } else if(res.candidates) {
+        this.setState({action: "Nearby Cars" });
+        this.setState({candidates: res.candidates.sort((a, b) => {
+            return a.distance - b.distance;
+          })
+        });
       } else {
-        this.setState({car: { license: 'failed' } });
+        this.setState({action: 'failed'});
       }
     });
   }
 
-  lock() {
-    api.put(`/car/${ this.state.car.id }/lock}`, {}, (err, res) => {
-      this.setState({ action: lock });
+  beBoring(act) {
+    api.put(`/cars/${ this.state.car.id }/${act}`, {}, (err, res) => {
+      this.setState({ action: verb });
     });
   }
-  unlock() {
-    api.put(`/car/${ this.state.car.id }/unlock}`, {}, (err, res) => {
-      this.setState({ action: unlock });
-    });
+
+  chooseCar(i) {
+    let act = this.props.params && this.props.params.act || 'retrieve';
+    this.setState({car: this.state.candidates[i]});
+    this.beBoring(act);
   }
 
   fail(what) {
@@ -40,7 +47,7 @@ module.exports = class Magic extends React.Component {
   }
 
   findMe() {
-    navigator.geolocation.getCurrentPosition(this.doAct.bind(this), this.fail.bind(this), {
+    navigator.geolocation.getCurrentPosition(this.doTrick.bind(this), this.fail.bind(this), {
       enableHighAccuracy: true,
       timeout: 5000,
       maximumAge: 0
@@ -56,9 +63,22 @@ module.exports = class Magic extends React.Component {
     return (
       <div>
         <h1> { this.state.action } { this.state.car ? this.state.car.license : '' } </h1>
-        { this.state.action ?
+        { this.state.candidates ?
           <div>
-            <button className="btn" onClick={ this.lock.bind(this) }>lock</button> <button className="btn" onClick={ this.unlock.bind(this) }>unlock</button>
+            { this.state.candidates.sort((a, b) => {
+                return a.distance - b.distance;
+              }).map((row, i) => {
+                return <button className='btn' onClick={ this.chooseCar.call(this, i) }>{ row.license }</button> 
+              })
+            }
+          </div> : ''
+        }
+        { this.state.car ?
+          <div>
+            { ['lock','unlock','lock-immobilizer','unlock-immobilizer','available','unavailable'].map((act) => {
+                return <button className="btn" onClick={ this.act.call(this, act) }>{ act }</button>
+              })
+            }
           </div> : ''
         }
       </div>
