a525bcba1ffee94543f0c217ed6a7891ae54c019
Chris McKenzie <kristopolous@yahoo.com>
2018-06-07 16:14:38 -0700

    Fixing the magic page

--- src/views/app/admin/cars/magic.jsx
+++ src/views/app/admin/cars/magic.jsx
@@ -8,26 +8,34 @@ module.exports = class Magic extends React.Component {
   constructor(...args) {
     super(...args);
     this.state = {
+      actionText: null,
       action: null,
       car : null
     };
     dom.setTitle('Magic');
   }
 
+  componentDidMount() {
+    if(this.props && this.props.params) {
+      this.setState({action: this.props.params.act });
+    }
+    this.findMe();
+  }
+
   doTrick(position) {
-    this.setState({action: 'Looking around...'});
-    let act = this.props.params && this.props.params.act || 'retrieve';
+    this.setState({actionText: 'Looking around...'});
+    let act = this.state.action || 'retrieve';
     api.put(`/magic/${ act }?latitude=${ position.coords.latitude }&longitude=${ position.coords.longitude }`, {}, (err, res) => {
       if(res.car) {
-        this.setState({action: act, car: res.car[0] });
+        this.setState({actionText: act, car: res.car[0] });
       } else if(res.candidates) {
-        this.setState({action: "Nearby Cars (" + act + ")" });
+        this.setState({actionText: "Nearby (" + act + ")" });
         this.setState({candidates: res.candidates.sort((a, b) => {
             return a.distance - b.distance;
           })
         });
       } else {
-        this.setState({action: 'failed'});
+        this.setState({actionText: 'failed'});
       }
     });
   }
@@ -37,16 +45,16 @@ module.exports = class Magic extends React.Component {
       car = this.state.car.id;
     }
 
-    this.setState({action: 'Working...'});
+    this.setState({actionText: 'Working...'});
     api.put(`/cars/${ car }/${ act }`, {}, (err, res) => {
-      this.setState({ action: act });
+      this.setState({ action: act, actionText: act });
     });
   }
 
   chooseaCar(i) {
-    let act = this.props.params && this.props.params.act || 'retrieve';
+    let act = this.state.action || 'retrieve';
     let car = this.state.candidates[i];
-    this.setState({car: car, candidates: undefined});
+    this.setState({car: car});
     this.beBoring(act, car.id);
   }
 
@@ -61,12 +69,10 @@ module.exports = class Magic extends React.Component {
       maximumAge: 0
     });
   }
-  componentDidMount() {
-    this.findMe();
-  }
 
   carInfo(car) {
       let htmlList = [];
+      let klass = '';
       let checkList = [
         ['isImmobilized', 'stop'],
         ['userId', 'user'],
@@ -80,7 +86,11 @@ module.exports = class Magic extends React.Component {
         htmlList.push( <i className={ className }></i> );
       })
 
-      return <span>{ htmlList } { car.license } <em>{ car.charge }%</em></span>
+      if(!car.isLocked && !car.isImmobilized && !car.userId) {
+        klass = 'warning';
+      }
+
+      return <span className={ klass }>{ htmlList } { car.license } <em>{ car.charge }%</em></span>
   }
 
   showCandidates() {
@@ -93,6 +103,7 @@ module.exports = class Magic extends React.Component {
       });
     return <div> { rows } </div>
   }
+
   showControls() {
     var rows = ['rentable', 'retrieve', 'lock','unlock','lock-immobilizer','unlock-immobilizer','available','unavailable'].map((verb) => {
       return <button className='btn' onClick={ this.beBoring.bind(this, verb) }>{ verb }</button>
@@ -100,13 +111,37 @@ module.exports = class Magic extends React.Component {
     return <div> { rows } </div>
   }
 
+  setAction(what) {
+    this.setState({
+      actionText: "Nearby (" + what + ")",
+      action: what
+    });
+  }
+
+  showCarList() {
+    this.setState({car: undefined});
+  }
+
+  renderCarHeader() {
+    return <span>
+        { this.carInfo(this.state.car) }
+        <small><a onClick={ this.showCarList.bind(this) }>car list</a></small>
+      </span>
+  }
+
+  renderLinks() {
+    return <small>
+      <a onClick={ this.setAction.bind(this, 'unlock') }>unlock</a> <a onClick={ this.setAction.bind(this, 'lock') }>lock</a> <a onClick={ this.setAction.bind(this, 'rentable') }>rentable</a> <a onClick={ this.setAction.bind(this, 'retrieve') }>retrieve</a>
+    </small>
+  }
+
   render() {
     dom.setTitle(`${ this.state.action } Magic`);
 
     return (
       <div className="magic">
-        <h1> { this.state.action } { this.state.car ? this.carInfo(this.state.car) : '' } </h1>
-        { this.state.candidates ? this.showCandidates() : '' }
+        <h1> { this.state.actionText } { this.state.car ? this.renderCarHeader() : this.renderLinks() } </h1>
+        { (this.state.candidates && !this.state.car) ? this.showCandidates() : '' }
         { this.state.car ? this.showControls() : '' }
       </div>
     );
