1c3fbfaf98ec0fb4a76833ee6d47686b419d1936
Christopher McKenzie <kristopolous@yahoo.com>
2018-06-08 15:52:15 -0700

    fixing the ios descriptions

--- scripts/ios-build.sh
+++ scripts/ios-build.sh
@@ -2,7 +2,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/common.sh
 
-nvmcheck
+#cordova prepare ios
+ionic build ios --verbose --release
 
-ionic build ios
-$DIR/ios-replace.sh
+for word in microphone contact reminders calendar; do
+  grep -viE "${word}s?(Usage| access)" $DIR/../platforms/ios/WaiveCar/WaiveCar-Info.plist > /tmp/removed
+  mv /tmp/removed $DIR/../platforms/ios/WaiveCar/WaiveCar-Info.plist
+done
+
+for word in AlwaysUsage "even when the screen is off"; do
+  grep -vE "$word" $DIR/../platforms/ios/WaiveCar/WaiveCar-Info.plist > /tmp/removed
+  mv /tmp/removed $DIR/../platforms/ios/WaiveCar/WaiveCar-Info.plist
+done
+
+#$DIR/ios-replace.sh
