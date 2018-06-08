0d1d16109b6fcf3657da5a878594820fb1210e1e
David Leigh <daleighan@gmail.com>
2018-06-07 18:02:48 -0700

    A request can now be made to issue refunds of charges

--- src/views/app/components/user/charges/charge-list.jsx
+++ src/views/app/components/user/charges/charge-list.jsx
@@ -68,18 +68,26 @@ class ChargeList extends Component {
   }
 
   refund(id, amount, description) {
+    console.log(id, amount, description);
     let refundAmount = prompt('Refunding up to $' + (amount / 100).toFixed(2) + ' for:\n  ' + description + '\nTo issue a partial refund, ent  er the amount below. For a full refund, leave the field blank');
-    console.log(refundAmount, Number(refundAmount));
-    if (Number(refundAmount) === 0) {
-      // refund full amount
-      console.log('refund the full amount');
-    } else if (refundAmount > 0) {
-      // refund specific amount
-      console.log(`refund of ${ amount } dollars`);
+    if (Number(refundAmount) >= 0) {
+      // Issues a refund if a 
+      api.post(`/shop/refund/${id}`, {
+        'amount': 100,
+	      'userId': 16126
+      }, (err, response) => {
+        if (err) {
+          return console.log(err);
+        }
+        return console.log(response);
+      });
+      // Refund the full amount when no amount is entered
+      console.log(`refund of ${amount > 0 ? `$${amount} refunded` : 'full amount refunded'}`);
     } else if (refundAmount === null) {
+      // This occurs when the cancel button is pressed
       return;
     } else {
-      // invalid input
+      // For invalid inputs
       console.log('invalid input');
       this.refund(id, amount, description);
     }
