ca22fed9e9225a103a04efd40f17b409876d8805
Chris McKenzie <kristopolous@yahoo.com>
2018-06-14 19:37:14 -0700

    updating the delete tool

--- tools/delete-user.sh
+++ tools/delete-user.sh
@@ -2,8 +2,14 @@
 user=$1;
 cat > /tmp/delete-user.sql << ENDL
  delete from logs where user_id=$user or actor_id=$user or booking_id in (select id from bookings where user_id=$user);
+ delete from parking_details where booking_detail_id in (select id from booking_details where booking_id in (select id from bookings where user_id=$user));
  delete from booking_details where booking_id in (select id from bookings where user_id=$user);
+ delete from booking_payments where booking_id in (select id from bookings where user_id=$user);
  delete from booking_locations where booking_id in (select id from bookings where user_id=$user);
+ delete from report_files where report_id in (select id from reports where booking_id in (select id from bookings where user_id=$user));
+ delete from reports where booking_id in (select id from bookings where user_id=$user);
+ delete from shop_order_items where order_id in (select id from shop_orders where user_id=$user);
+ delete from log_events where user_id=$user;
  delete from bookings where user_id=$user;
  delete from group_users where user_id=$user;
  delete from licenses where user_id=$user;
@@ -14,4 +20,4 @@ cat > /tmp/delete-user.sql << ENDL
  delete from waitlist where user_id=$user;
  delete from users where id=$user;
 ENDL
-echo 'mysql -uroot waivecar_production < /tmp/delete-user.sql'
+mysql -uroot waivecar_development < /tmp/delete-user.sql
