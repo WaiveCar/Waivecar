7458ec5eda3b99f85b6ef59c43891f2867596bf3
Chris McKenzie <kristopolous@yahoo.com>
2018-06-20 19:47:55 -0700

    Generating the splash for android as well

--- resources/generate-splash.sh
+++ resources/generate-splash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in ios/splash/*; do
+for i in ios/splash/* android/splash/*; do
   width=`identify -format %w $i`
   size=`identify -format %wx%h $i`
   dpi=$(( width / 3 ))
@@ -7,7 +7,7 @@ for i in ios/splash/*; do
   convert \
     -background none -density $dpi circle-logo.svg \
     -background black -gravity center -extent $size -colors 18 /tmp/$fname
-  pngcrush -brute /tmp/$fname $i
+  pngcrush -l 9 /tmp/$fname $i
 done
 
 
