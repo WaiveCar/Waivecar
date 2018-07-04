411bdb1959b8caa6904a2294aa9f77b46efd604f
Chris McKenzie <kristopolous@yahoo.com>
2018-07-04 16:49:30 -0700

    trying to have different thresholds for tow truck detection

--- modules/waivecar/bootstrap/schedules/active-booking.js
+++ modules/waivecar/bootstrap/schedules/active-booking.js
@@ -170,9 +170,9 @@ var checkBooking = co.wrap(function *(booking) {
   });
   yield newLocation.save();
   
-  let hasMoved = GeocodingService.hasMoved(car, device);
+  let hasMoved = GeocodingService.hasMoved(car, device, 600);
   // If the car has moved, but the ignition is off, that means that the vehicle may currently be being towed and a notification is sent tto slack
-  if (hasMoved && !device.isIgnitionOn && !car.isIgnitionOn && car.totalMileage === device.totalMileage) {
+  if (hasMoved && !device.isIgnitionOn && !car.isIgnitionOn && car.totalMileage === device.totalMileage && hasMoved < 10000) {
     console.log(car, device, car.totalMileage, device.totalMileage, device.isIgnitionOn, car.isIgnitionOn, hasMoved);
     yield notify.notifyAdmins(`:flying_saucer: ${ car.license } is moving without the ignition on or odometer incrementing. It may be on a tow truck.`, [ 'slack' ], { channel : '#rental-alerts' });
   }
