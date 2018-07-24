afb9a8b325c022e00d46a577fabf134498bfb6fc
David Leigh <daleighan@gmail.com>
2018-07-23 18:40:27 -0700

    The updates to the parking space are now emitted to the socket

--- modules/waivecar/bootstrap/schedules/parking-auto-cancel.js
+++ modules/waivecar/bootstrap/schedules/parking-auto-cancel.js
@@ -2,13 +2,24 @@
 
 let scheduler = Bento.provider('queue').scheduler;
 let UserParking = Bento.model('UserParking');
+let relay = Bento.Relay;
 
 scheduler.process('parking-auto-cancel', function*(job) {
   let space = yield UserParking.findById(job.data.spaceId);
+  let currentUserId = space.reservedById;
   yield space.update({
     reserved: false,
     reservedById: null,
-    reservedAt: null
+    reservedAt: null,
   });
   console.log('updated: ', space);
+  relay.user(currentUserId, 'userParking', {
+    type: 'update',
+    data: space.toJSON(),
+  });
+  relay.admin('userParking', {
+    type: 'update',
+    data: space.toJSON(),
+  });
+    console.log('events emitted');
 });
--- modules/waivecar/lib/parking-service.js
+++ modules/waivecar/lib/parking-service.js
@@ -51,7 +51,6 @@ module.exports = {
     });
     // This can be changed to make the process end at a different time
     let timerObj = {value: 5, type: 'seconds'};
-    console.log('spaceId: ', space.id);
     queue.scheduler.add('parking-auto-cancel', {
       uid: `parking-${parkingId}`,
       timer: timerObj,
