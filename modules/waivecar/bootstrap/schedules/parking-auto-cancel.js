c737036859c105e0924972c5f6faf2ead962ba4a
David Leigh <daleighan@gmail.com>
2018-07-26 12:09:44 -0700

    Corrected the wording

--- modules/waivecar/bootstrap/schedules/parking-auto-cancel.js
+++ modules/waivecar/bootstrap/schedules/parking-auto-cancel.js
@@ -6,13 +6,20 @@ let ParkingService = require('../../lib/parking-service');
 let notify = require('../../lib/notification-service');
 
 scheduler.process('parking-auto-cancel', function*(job) {
-  yield ParkingService.cancel(job.data.spaceId, job.data.user.id, job.data.reservation.id);
+  yield ParkingService.cancel(
+    job.data.spaceId,
+    job.data.user.id,
+    job.data.reservation.id,
+  );
   yield notify.notifyAdmins(
     `:rage: ${job.data.user.firstName} ${
       job.data.user.lastName
-    } has reserved parking spot #${job.data.spaceId}`,
+    } has lost their reservation for parking spot #${job.data.spaceId}`,
     ['slack'],
     {channel: '#reservations'},
   );
-  yield notify.sendTextMessage(job.data.user.id, `Your reservation for space #${job.data.spaceId} has expired`);
+  yield notify.sendTextMessage(
+    job.data.user.id,
+    `Your reservation for space #${job.data.spaceId} has expired`,
+  );
 });
