3364b017d169eddf8c77ce2811245df038d169db
David Leigh <daleighan@gmail.com>
2018-07-30 11:50:12 -0700

    The user parking component now fetches a user's spaces

--- src/views/app/components/user/user-parking/user-parking.jsx
+++ src/views/app/components/user/user-parking/user-parking.jsx
@@ -11,19 +11,30 @@ export default class UserParking extends Component {
   }
 
   componentDidMount() {
-    console.log('UserParking props: ', this.props);
-    console.log('State: ', this.state);
+    let {userId} = this.props;
+    api.get(`/parking/users/${userId}`, (err, spaces) => {
+      if (err) {
+        console.log('error: ', err);
+      }
+      console.log('spaces: ', spaces);
+      this.setState({spaces}, () => console.log('state: ', this.state));
+    });
   }
 
   render = () => {
-    let {spaces} = this.props;
+    let {spaces} = this.state;
     return (
       <div className="box">
         <h3>
           <span>Parking Spaces</span>
           <small>Manage parking spaces</small>
         </h3>
-        <div className="box-content">Some text here</div>
+        <div className="box-content">
+          <h4>Your parking spaces</h4>
+          <div>
+            {spaces.map(space => <div>A space</div>)}
+          </div>
+        </div>
       </div>
     );
   };
