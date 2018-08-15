0775c6ea9a559d35780fe0e56fc154f4c4ec4b17
Chris McKenzie <kristopolous@yahoo.com>
2018-08-15 11:51:25 -0700

    updating the faq

--- src/views/site/faqlevel.jsx
+++ src/views/site/faqlevel.jsx
@@ -11,23 +11,23 @@ module.exports = class Faqlevel extends Component {
           <p>Currently in New York, we are an amenity exclusively available for residents of level.</p>
           <h2 id="signing-up">Signing up</h2>
           <h3 id="what-do-i-need-to-sign-up">What do I need to sign up?</h3>
-          <p>All you need to sign up with WaiveCar is to be a resident of level, a smart phone, credit/debit card, a valid driver's license, and a good driving record. Simply download the app from your app store!</p>
-          <h4 id="whats-a-good-driving-record">What's a good driving record?</h4>
+          <p>All you need to sign up with WaiveCar is to be a resident of level, a smart phone, credit/debit card, a valid driver’s license, and a good driving record. Simply download the app from your app store!</p>
+          <h4 id="whats-a-good-driving-record">What’s a good driving record?</h4>
           <p>To us, a good driving record means one without any:</p>
           <ul>
           <li>DUIs</li>
           <li>Moving violations</li>
           <li>Driving with a suspended license</li>
-          <li>Any similar negligent or reckless acts</li>
+          <li>Any similar negligent or reckless acts  * Accidents</li>
           </ul>
           <h3 id="does-it-cost-any-money-to-sign-up">Does it cost any money to sign up?</h3>
-          <p>Nope! We promise there REALLY are no catches. We're that magical.</p>
-          <h3 id="why-do-i-need-a-creditdebit-card-to-sign-up-if-its-free">Why do I need a credit/debit card to sign up if it's free?</h3>
-          <p>We charge a temporary $1 hold on your card when you book a car that will be refunded as soon as your ride is over just to make sure there are funds in case you go over the 2 free hours!</p>
+          <p>Nope! We promise there REALLY are no catches. We’re that magical.</p>
+          <h3 id="why-do-i-need-a-creditdebit-card-to-sign-up-if-its-free">Why do I need a credit/debit card to sign up if it’s free?</h3>
+          <p>We charge a temporary $20 hold on your card when you book a car that is immediately refunded just to make sure there are funds in case you go over the 2 free hours!</p>
           <h3 id="how-old-do-i-need-to-be-to-drive-waivecar">How old do I need to be to drive WaiveCar?</h3>
           <p>You need to be 21 to drive with WaiveCar.</p>
           <h3 id="can-i-use-my-ipadtablet-to-use-waivecar">Can I use my iPad/tablet to use WaiveCar?</h3>
-          <p>No. Unfortunately we require a smart phone so that a phone number can be attached to the account as well as GPS services.</p>
+          <p>No. Unfortunately we require a smart phone so that a phone number can be attached to the account as well as GPS services.</p>
           <h3 id="is-insurance-included">Is insurance included?</h3>
           <p>Yep! Insurance with a $1,000 deductible is included in the signup! The information is in the glove box!</p>
           <h3 id="can-i-use-an-out-of-state-drivers-license-to-drive-with-waivecar">Can I use an out of state drivers license to drive with WaiveCar?</h3>
@@ -37,52 +37,52 @@ module.exports = class Faqlevel extends Component {
           <h3 id="can-i-use-a-temporary-drivers-license">Can I use a temporary drivers license?</h3>
           <p>Unfortunately you will need a permanent license to drive with WaiveCar.</p>
           <h3 id="how-long-does-the-signup-take">How long does the signup take?</h3>
-          <p>It's very quick! Should only take about 10 minutes for the signup and the background check to complete. If the background check seems to be taking longer, please give us a call at 855-WAIVE55</p>
+          <p>It’s very quick! Should only take about 10 minutes for the signup and the background check to complete. If the background check seems to be taking longer, please give us a call at 855-WAIVE55</p>
           <h2 id="booking-cars">Booking Cars</h2>
-          <h3 id="what-are-waivecars-hours">What are WaiveCar's hours?</h3>
+          <h3 id="what-are-waivecars-hours">What are WaiveCar’s hours?</h3>
           <p>Our cars are available 24/7 and the customer service line is open from 9am-midnight, California time. If you need assistance at other hours, simply send us an email at support@waive.car and we will reach you at our earliest convenience.</p>
           <h3 id="how-do-i-book-a-waivecar">How do I book a WaiveCar?</h3>
-          <p>Log onto the app to see if any cars are available. Tap on a car and hit &quot;Book it&quot;! You then have 15 minutes to get to the car before the reservation times out.</p>
+          <p>Log onto the app to see if any cars are available. Tap on a car and hit “Book it”! You then have 15 minutes to get to the car before the reservation times out.</p>
           <h3 id="where-can-i-get-a-waivecar">Where can I get a WaiveCar?</h3>
-          <p>WaiveCars are an exclusive amenity of level residents in Brooklyn and are available at 34 N 7th Street. We also have a large fleet available on the West Side of Los Angeles so come on by if you're ever in sunny LA.</p>
+          <p>WaiveCars are an exclusive amenity of level residents in Brooklyn and are available at 34 N 7th Street. We also have a large fleet available on the West Side of Los Angeles so come on by if you’re ever in sunny LA.</p>
           <h3 id="how-long-do-i-have-to-get-to-a-waivecar">How long do I have to get to a WaiveCar?</h3>
           <p>You have 15 minutes to get to a WaiveCar before the reservation times out.</p>
-          <p>Once you get there, please make sure you drive the WaiveCar! We may have to forfeit your booking if the WaiveCar isn't being used. But don't worry, we send out a message beforehand and you'll have plenty of time to contact us if we've made a mistake.</p>
+          <p>Once you get there, please make sure you drive the WaiveCar! We may have to forfeit your booking if the WaiveCar isn’t being used. But don’t worry, we send out a message beforehand and you’ll have plenty of time to contact us if we’ve made a mistake.</p>
           <h3 id="can-i-reserve-a-car-in-advance">Can I reserve a car in advance?</h3>
-          <p>Unlike a rental company, you CANNOT reserve a car in advance. WaiveCar believes in the convenience of giving you a free car NOW! Get it while it's hot!</p>
+          <p>Unlike a rental company, you CANNOT reserve a car in advance. WaiveCar believes in the convenience of giving you a free car NOW! Get it while it’s hot!</p>
           <h3 id="can-i-book-multiple-cars-a-day">Can I book multiple cars a day?</h3>
           <p>Yep! And even better, you will get the free 2 hours for EVERY ride!</p>
           <h3 id="does-the-free-2-hours-start-when-i-reserve-a-car-or-when-i-start-the-ride">Does the free 2 hours start when I reserve a car or when I start the ride?</h3>
           <p>When you start the ride! Enjoy your leisurely walk!</p>
-          <h3 id="is-the-free-2-hours-only-when-im-driving-the-car">Is the free 2 hours only when I'm driving the car?</h3>
-          <p>Nope! The free 2 hours counts for having the car while it's driving or parked.</p>
-          <h3 id="what-happens-if-i-dont-make-it-to-the-car-in-15-minutes">What happens if I don't make it to the car in 15 minutes?</h3>
-          <p>Then your reservation will expire! So hurry up and try to rebook the same car or another WaiveCar before it's gone! We're a hot commodity!</p>
-          <h3 id="whats-with-that-1-charge-when-i-book-a-waivecar">What's with that $1 charge when I book a WaiveCar?</h3>
-          <p>No worries! It's just a $1 hold on your card we will refund right back to you when your ride is complete!</p>
+          <h3 id="is-the-free-2-hours-only-when-im-driving-the-car">Is the free 2 hours only when I’m driving the car?</h3>
+          <p>Nope! The free 2 hours counts for having the car while it’s driving or parked.</p>
+          <h3 id="what-happens-if-i-dont-make-it-to-the-car-in-15-minutes">What happens if I don’t make it to the car in 15 minutes?</h3>
+          <p>Then your reservation will expire! So hurry up and try to rebook the same car or another WaiveCar before it’s gone! We’re a hot commodity!</p>
+          <h3 id="whats-with-that-1-charge-when-i-book-a-waivecar">What’s with that $1 charge when I book a WaiveCar?</h3>
+          <p>No worries! It’s just a $1 hold on your card we will refund right back to you when your ride is complete!</p>
           <h3 id="how-many-waivecars-are-there">How many WaiveCars are there?</h3>
           <p>We currently have 3 available for level residents.</p>
           <h2 id="driving-cars">Driving Cars</h2>
           <h3 id="how-far-can-i-drive-with-a-waivecar">How far can I drive with a WaiveCar?</h3>
-          <p>The Chevy Sparks have between a 50 and 70 mile range depending on your driving style. You are at liberty to charge the car if you'd like for longer journeys. Along with being free the first 2 hours, a single WaiveCar can be rented for up to 12 hours at a time.</p>
+          <p>The Chevy Sparks have between a 50 and 70 mile range depending on your driving style. You are at liberty to charge the car if you’d like for longer journeys. Along with being free the first 2 hours, a single WaiveCar can be rented for up to 12 hours at a time.</p>
           <h3 id="where-are-the-keys">Where are the keys?</h3>
-          <p>You don't need keys to start the car, however if you need to lock the car DURING your rental, the keys are on a sensor in the glovebox! Just make sure you replace them when you're done.</p>
-          <h3 id="what-should-i-do-if-im-running-low-on-charge">What should I do if I'm running low on charge?</h3>
-          <p>Check the app for the closest fast charger, or download the app &quot;Plugshare&quot;. Give us a call at 855-WAIVE55 if there is any confusion and we can direct you to one!</p>
+          <p>You don’t need keys to start the car, however if you need to lock the car DURING your rental, the keys are on a sensor in the glovebox! Just make sure you replace them when you’re done.</p>
+          <h3 id="what-should-i-do-if-im-running-low-on-charge">What should I do if I’m running low on charge?</h3>
+          <p>Check the app for the closest fast charger, or download the app “Plugshare”. Give us a call at 855-WAIVE55 if there is any confusion and we can direct you to one!</p>
           <h3 id="do-you-guys-cover-the-charging-station-costs">Do you guys cover the charging station costs?</h3>
-          <p>Nope! Just like other services, we don't compensate to refuel/recharge, however just bring it back to the HQ where they will charge for FREE and see if you can switch out for a higher charged car!</p>
+          <p>Nope! Just like other services, we don’t compensate to refuel/recharge, however just bring it back to the HQ where they will charge for FREE and see if you can switch out for a higher charged car!</p>
           <h3 id="how-much-does-it-usually-cost-to-charge-an-electric-car">How much does it usually cost to charge an electric car?</h3>
-          <p>Fast chargers typically cost around $10 and take about 15 minutes. Level 2 chargers take about 6 hours to fully charge but can be free depending on the station. Call us if there's ANY confusion!</p>
+          <p>Fast chargers typically cost around $10 and take about 15 minutes. Level 2 chargers take about 6 hours to fully charge but can be free depending on the station. Call us if there’s ANY confusion!</p>
           <h3 id="can-someone-else-drive-using-my-account">Can someone else drive using my account?</h3>
-          <p>This one's a big N.O. The ONLY person allowed to drive a WaiveCar is the person on the account who has booked the car. Big consequences if we find out otherwise!</p>
-          <h3 id="what-happens-if-theres-a-parking-ticket">What happens if there's a parking ticket?</h3>
-          <p>If you get a ticket, you have 10 days to pay it. If you don't pay it within that time, we will charge your account for the amount due. If you see a ticket from a previous user, simply put it in the glove box.</p>
+          <p>This one’s a big N.O. The ONLY person allowed to drive a WaiveCar is the person on the account who has booked the car. Big consequences if we find out otherwise!</p>
+          <h3 id="what-happens-if-theres-a-parking-ticket">What happens if there’s a parking ticket?</h3>
+          <p>If you get a ticket, you have 10 days to pay it. If you don’t pay it within that time, we will charge your account for the amount due. If you see a ticket from a previous user, simply put it in the glove box.</p>
           <h2 id="ending-your-ride">Ending Your Ride</h2>
-          <h3 id="where-can-i-park-when-im-done">Where can I park when I'm done?</h3>
-          <p>All WaiveCars must be returned to the 24 hour parking lot at 34 N 7th Street. You won't need to pay anything at the lot, so don't worry - the attendants will help you.</p>
+          <h3 id="where-can-i-park-when-im-done">Where can I park when I’m done?</h3>
+          <p>All WaiveCars must be returned to the 24 hour parking lot at 34 N 7th Street. You won’t need to pay anything at the lot, so don’t worry - the attendants will help you.</p>
           <h3 id="how-do-i-end-my-ride">How do I end my ride?</h3>
-          <p>First make sure that you have returned the vehicle to the parking lot. Turn off the car, put the key back on the sensor in the glovebox, grab your belongings, and exit the car. Select &quot;End Ride&quot; from the app and input the appropriate information. If you haven't received a confirmation text please call us at 855-WAIVE55!</p>
-          <h3 id="do-i-need-to-clean-the-car-when-im-done">Do I need to clean the car when I'm done?</h3>
+          <p>First make sure that you have returned the vehicle to the parking lot. Turn off the car, put the key back on the sensor in the glovebox, grab your belongings, and exit the car. Select “End Ride” from the app and input the appropriate information. If you haven’t received a confirmation text please call us at 855-WAIVE55!</p>
+          <h3 id="do-i-need-to-clean-the-car-when-im-done">Do I need to clean the car when I’m done?</h3>
           <p>Yes! No need for an extensive cleaning, but make sure all your belongings are out of the car and clean up any trash you may leave. If you leave a mess, there could be a cleaning fee!</p>
           <h3 id="do-i-need-to-charge-the-car-before-i-end-my-ride">Do I need to charge the car before I end my ride?</h3>
           <p>Nope! Follow the rules above. If the car is under 25 miles charged, simply return it at the HQ!</p>
@@ -103,7 +103,7 @@ module.exports = class Faqlevel extends Component {
           <li><strong>Pet not in carrier/odor</strong> $50</li>
           <li><strong>Damage to the interior</strong> Our costs to repair</li>
           </ul>
-          <p>There's a few unfortunate events that can happen where we have pass the costs on to you:</p>
+          <p>There’s a few unfortunate events that can happen where we have pass the costs on to you:</p>
           <ul>
           <li><strong>Vehicle Recovery</strong> Miscellaneous Recovery Costs</li>
           <li><strong>Parking Ticket</strong> Ticket Price + $20.00 Handling fee (unless user pays themselves)</li>
--- src/views/site/job.jsx
+++ src/views/site/job.jsx
@@ -15,12 +15,12 @@ module.exports = class Job extends Component {
           <li>hacker news/reddit account name</li>
           <li>portfolio or personal website</li>
           </ul>
-          <p>Please feel free to apply if you have a lot of experience in programming and startups regardless of whether you've done the particular technologies we work with: javascript (react, angular, es6 harmony etc) and mobile (android, swift, etc).</p>
+          <p>Please feel free to apply if you have a lot of experience in programming and startups regardless of whether you’ve done the particular technologies we work with: javascript (react, angular, es6 harmony etc) and mobile (android, swift, etc).</p>
           <p>You should be able to be able to get to Santa Monica in a reasonable amount of time - although we are comfortable with offsite work.</p>
           <p>The most important thing for us is your process, character, and intuition with regard to software development.</p>
-          <p>If you could include a cover letter talking about how you'd address this, that'd be great:</p>
+          <p>If you could include a cover letter talking about how you’d address this, that’d be great:</p>
           <blockquote>
-          <p>Let's say a non-technical person reports a problem and proposes a solution, what do you see as your job and list of priorities?</p>
+          <p>Let’s say a non-technical person reports a problem and proposes a solution, what do you see as your job and list of priorities?</p>
           </blockquote>
           <p>Apply to chris@(domain this is on).</p>
           </div>
