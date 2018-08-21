267c4880cfa399d2621777f3f68ac33bd48e8278
David Leigh <daleighan@gmail.com>
2018-08-21 12:02:59 -0700

    Fixed syntax errors

--- config/license/default.js
+++ config/license/default.js
@@ -17,7 +17,7 @@ module.exports = {
       uri  : 'https://api.onfido.com/v2/',
       key  : 'test_jNkxF8-E4hfeUiHgNx_OZGtdl4F2ntpS',
       hook : null
-    }
+    },
     checkr : {
       key : '98413a44829d07166004a0bab8b7535a12f727e6',
       token : '304e6e802f2fb602ae63778bcbfc0a0b0078d05'
--- config/license/development.js
+++ config/license/development.js
@@ -15,7 +15,7 @@ module.exports = {
   license : {
     onfido : {
       hook : 'Yb4GSgSCxVPixBDw3VXaPH6GoPg_oPZW'
-    }
+    },
     checkr : {
       key : '98413a44829d07166004a0bab8b7535a12f727e6',
       token : '304e6e802f2fb602ae63778bcbfc0a0b0078d05'
