e70aecdec9a684c54b4b1213ccf722916fc2c05c
Chris McKenzie <kristopolous@yahoo.com>
2018-08-31 15:22:31 -0700

    Doubling the tow truck trigger

--- modules/waivecar/bootstrap/schedules/active-booking.js
+++ modules/waivecar/bootstrap/schedules/active-booking.js
@@ -178,7 +178,7 @@ var checkBooking = co.wrap(function *(booking) {
   
   let hasMoved = GeocodingService.hasMoved(car, device, 600);
   // If the car has moved, but the ignition is off, that means that the vehicle may currently be being towed and a notification is sent tto slack
-  if (hasMoved && !device.isIgnitionOn && !car.isIgnitionOn && car.totalMileage === device.totalMileage && hasMoved < 10000) {
+  if (hasMoved && !device.isIgnitionOn && !car.isIgnitionOn && car.totalMileage === device.totalMileage && hasMoved < 20000) {
     console.log(car, device, car.totalMileage, device.totalMileage, device.isIgnitionOn, car.isIgnitionOn, hasMoved);
     yield notify.notifyAdmins(`:flying_saucer: ${ car.license } is moving without the ignition on or odometer incrementing. It may be on a tow truck.`, [ 'slack' ], { channel : '#rental-alerts' });
   }
