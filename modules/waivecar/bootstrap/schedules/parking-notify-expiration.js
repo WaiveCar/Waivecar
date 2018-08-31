bc3c24576307bbd1698b7efff4c823bd85800d38
Chris McKenzie <kristopolous@yahoo.com>
2018-08-30 19:38:24 -0700

    moving parking notification to a different channel

--- modules/waivecar/bootstrap/schedules/parking-notify-expiration.js
+++ modules/waivecar/bootstrap/schedules/parking-notify-expiration.js
@@ -6,10 +6,15 @@ let Ticket = Bento.model('Ticket');
 
 scheduler.process('parking-notify-expiration', function*(job) {
   let {address, zone, notificationTime, car, carId} = job.data;
+  if(address) {
+    address = "at " + address;
+  } else {
+    address = '';
+  }
   yield notify.slack(
     {
-      text: `Parking space expiring: ${car} at ${address} in ${zone} in ${notificationTime} minutes`,
+      text: `:memo: Parking space expiring: ${car.link()} ${address} in ${zone} in ${notificationTime} minutes`,
     },
-    {channel: '#fleet'},
+    {channel: '#rental-alerts'},
   );
 });
