030ae4cb85b69790509feea1a39ea3ac534beeee
Chris McKenzie <kristopolous@yahoo.com>
2018-09-04 12:06:13 -0700

    updating the uri for checkr production

--- config/license/production.js
+++ config/license/production.js
@@ -18,7 +18,7 @@ module.exports = {
     checkr : {
       key : 'e85d14c01d7f9f8970181b447b56f61e8d65d377',
       token : 'b98ba65960820e9869bc8972191767c1de72f6d8',
-      uri : 'https://98413a44829d07166004a0bab8b7535a12f727e6:@api.checkr.com/v1'
+      uri : 'https://e85d14c01d7f9f8970181b447b56f61e8d65d377:@api.checkr.com/v1'
     }
   }
 
