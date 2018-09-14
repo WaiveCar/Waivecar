5215eefc3ac6cea9939d1a9b55351b60e62eec4d
David Leigh <daleighan@gmail.com>
2018-09-14 14:57:39 -0700

    There is now a query for the last 10 bookings by each user that has had a completed ride in the past 24 hours

--- modules/waivecar/bootstrap/schedules/check-user-levels.js
+++ modules/waivecar/bootstrap/schedules/check-user-levels.js
@@ -17,7 +17,21 @@ scheduler.process('check-user-levels', function*(job) {
       status: 'completed',
     },
   });
-  console.log('new bookings: ', newBookings);
+  let usersToProcess = new Set();
+  newBookings.forEach(booking => usersToProcess.add(booking.userId));
+  let lastTenBookings = {};
+  for (let id of usersToProcess) {
+    let temp = yield Booking.find({
+      where: {
+        userId: id,
+        status: 'completed',
+      },
+      order: [['id', 'DESC']],
+      limit: 10,
+    });
+    lastTenBookings[id] = temp;
+  }
+  console.log('last 10: ', lastTenBookings);
 });
 
 module.exports = function*() {
