0cc501af0e8d17c9d3ec94b164bb50473fbe490a
David Leigh <daleighan@gmail.com>
2018-09-14 17:44:01 -0700

    The query for a user's recent booking now works correctly

--- modules/waivecar/bootstrap/schedules/check-user-levels.js
+++ modules/waivecar/bootstrap/schedules/check-user-levels.js
@@ -19,6 +19,9 @@ scheduler.process('check-user-levels', function*(job) {
   });
   let usersToProcess = new Set();
   newBookings.forEach(booking => usersToProcess.add(booking.userId));
+
+  console.log('list of users to check: ', usersToProcess);
+
   let recentBookings = {};
   for (let id of usersToProcess) {
     recentBookings[id] = yield Booking.find({
@@ -28,16 +31,25 @@ scheduler.process('check-user-levels', function*(job) {
       },
       order: [['id', 'DESC']],
       limit: 20,
-      include: [{
-        model: 'BookingDetails',
-        as: 'details',
-      }],
+      include: [
+        {
+          model: 'BookingDetails',
+          as: 'details',
+        },
+      ],
     });
   }
+  yield calculateLevel();
+  /*
   for (let user in recentBookings) {
     recentBookings[user].forEach(booking => console.log(booking.details));
   }
+  */
 });
+
+let calculateLevel = function*(bookings) {
+  console.log('calcuate level with this fucntion');
+};
 
 module.exports = function*() {
   scheduler.add('check-user-levels', {
