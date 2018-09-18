22bb42b824cbafdc5a7ba97012bf547c0b36f8d5
Chris McKenzie <kristopolous@yahoo.com>
2018-09-18 15:01:35 -0700

    Fixing a syntax error

--- modules/waivecar/lib/booking-service.js
+++ modules/waivecar/lib/booking-service.js
@@ -162,10 +162,10 @@ module.exports = class BookingService extends Service {
                 body,
               }
             });
-          }
-        } catch(err) {
-          log.warn('email error: ', err);
-        };
+          } catch(err) {
+            log.warn('email error: ', err);
+          } 
+        }
       } catch (err) {
         // Failing to secure the authorization hold should be recorded as an
         // iniquity. See https://github.com/WaiveCar/Waivecar/issues/861 for
