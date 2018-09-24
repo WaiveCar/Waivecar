dbb5a8cdbff98f4efefd5af8d9a3d3e4bba03421
David Leigh <daleighan@gmail.com>
2018-09-24 15:13:53 -0700

    The process should now relay the updates to the app while not modifying the database

--- modules/waivecar/bootstrap/schedules/make-cars-unavailable.js
+++ modules/waivecar/bootstrap/schedules/make-cars-unavailable.js
@@ -3,9 +3,19 @@
 let scheduler = Bento.provider('queue').scheduler;
 let Car = Bento.model('Car');
 var moment = require('moment-timezone');
+let relay = Bento.Relay;
 
 scheduler.process('make-cars-unavailable', function*(job) {
-  console.log('cars to be made unavailable');
+  let carsToProcess = yield Car.find({where: {isAvailable: true}});
+  carsToProcess.forEach(car => {
+    car.isAvailable = false;
+    car.relay('update');
+  });
+
+  let timerObj = {value: 24, type: 'hours'};
+  scheduler.add('make-cars-unavailable', {
+    timer: timerObj,
+  });
 });
 
 module.exports = function*() {
