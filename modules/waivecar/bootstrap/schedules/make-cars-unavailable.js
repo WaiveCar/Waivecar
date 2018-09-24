39e32b880fb3c7ee9d0ecc3011943644f1230b3e
David Leigh <daleighan@gmail.com>
2018-09-24 15:18:13 -0700

    Added some comments

--- modules/waivecar/bootstrap/schedules/make-cars-unavailable.js
+++ modules/waivecar/bootstrap/schedules/make-cars-unavailable.js
@@ -8,6 +8,8 @@ let relay = Bento.Relay;
 scheduler.process('make-cars-unavailable', function*(job) {
   let carsToProcess = yield Car.find({where: {isAvailable: true}});
   carsToProcess.forEach(car => {
+    // This sets the car as unavailable so that the unavailable car is relayed. This is so that
+    // they disappear from users' maps without actually becoming marked unavailable in the database.
     car.isAvailable = false;
     car.relay('update');
   });
@@ -19,11 +21,14 @@ scheduler.process('make-cars-unavailable', function*(job) {
 });
 
 module.exports = function*() {
+  // This gets the hour of the current time
   let currentHour = Number(
     moment()
       .tz('America/Los_Angeles')
       .format('H'),
   );
+  // This is the time that cars are to be made unavailable. If it is after 1:00, a day will be added
+  // to the time to update the cars 
   let timeToCheck =
     currentHour < 1
       ? moment()
@@ -37,7 +42,7 @@ module.exports = function*() {
           .hours(1)
           .minutes(0)
           .seconds(0);
-
+  // This gets the seconds until the cars are to be marked unavailable
   let secondsUntilTime = Math.abs(moment().diff(timeToCheck, 'seconds'));
 
   let timerObj = {value: secondsUntilTime, type: 'seconds'};
