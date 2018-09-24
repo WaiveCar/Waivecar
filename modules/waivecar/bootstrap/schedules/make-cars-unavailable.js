6612075b989d7f1dd0f73a1a405156b0612b5855
David Leigh <daleighan@gmail.com>
2018-09-24 14:09:38 -0700

    The number of seconds until the make cars unavailable process runs should be calculated correctly

--- modules/waivecar/bootstrap/schedules/make-cars-unavailable.js
+++ modules/waivecar/bootstrap/schedules/make-cars-unavailable.js
@@ -2,20 +2,36 @@
 
 let scheduler = Bento.provider('queue').scheduler;
 let Car = Bento.model('Car');
-let moment = require('moment');
-
+var moment = require('moment-timezone');
 
 scheduler.process('make-cars-unavailable', function*(job) {
   console.log('cars to be made unavailable');
 });
 
-module.exports = function *() {
-  let secondsUntilTime = moment().diff(moment().hour(1).minute(0).second(0), 'seconds');
-  console.log('seconds until: ', secondsUntilTime);
+module.exports = function*() {
+  let currentHour = Number(
+    moment()
+      .tz('America/Los_Angeles')
+      .format('H'),
+  );
+  let timeToCheck =
+    currentHour < 1
+      ? moment()
+          .tz('America/Los_Angeles')
+          .hours(1)
+          .minutes(0)
+          .seconds(0)
+      : moment()
+          .tz('America/Los_Angeles')
+          .add(1, 'days')
+          .hours(1)
+          .minutes(0)
+          .seconds(0);
+
+  let secondsUntilTime = Math.abs(moment().diff(timeToCheck, 'seconds'));
+
   let timerObj = {value: secondsUntilTime, type: 'seconds'};
   scheduler.add('make-cars-unavailable', {
-    init   : true,
-    repeat : false,
-    timer  : timerObj,
+    timer: timerObj,
   });
 };
