fa7505e26aa14630bb9cd4e985469decb9d7dcd1
David Leigh <daleighan@gmail.com>
2018-09-25 13:51:43 -0700

    The object for passing cars to be checked is now passed when the process is repeated

--- modules/waivecar/bootstrap/schedules/have-cars-moved.js
+++ modules/waivecar/bootstrap/schedules/have-cars-moved.js
@@ -5,11 +5,16 @@ let bookingService = require('../../lib/booking-service.js');
 
 scheduler.process('have-cars-moved', function *(job) {
   console.log('have cars moved process');
+  console.log('job.data: ', job.data);
+  
+  let carsForNextCheck = !job.data.carsForNextCheck ? {} : job.data.carsForNextCheck;
+
   let cars = yield Car.find({
     where: {
       inRepair: false,
     }
   });
+
   for (let car of cars) {
     if (yield bookingService.isAtHub(car)) {
       console.log('car is at hub');
@@ -23,7 +28,6 @@ module.exports = function*() {
   let timerObj = {value: 15, type: 'seconds'};
   scheduler.add('have-cars-moved', {
     init: true,
-    repeat: true,
     timer: timerObj, 
     data: {},
   });
