143de4a009b2e21bbc8c67ce4864cccb566933dc
David Leigh <daleighan@gmail.com>
2018-09-25 13:42:06 -0700

    Added a query for all cars and added a check for if the cars are at a hub

--- modules/waivecar/bootstrap/schedules/have-cars-moved.js
+++ modules/waivecar/bootstrap/schedules/have-cars-moved.js
@@ -1,9 +1,22 @@
 'use strict';
 let scheduler = Bento.provider('queue').scheduler;
 let Car = Bento.model('Car');
+let bookingService = require('../../lib/booking-service.js');
 
 scheduler.process('have-cars-moved', function *(job) {
   console.log('have cars moved process');
+  let cars = yield Car.find({
+    where: {
+      inRepair: false,
+    }
+  });
+  for (let car of cars) {
+    if (yield bookingService.isAtHub(car)) {
+      console.log('car is at hub');
+    } else {
+      console.log('car not at hub');
+    }
+  }
 });
 
 module.exports = function*() {
@@ -12,5 +25,6 @@ module.exports = function*() {
     init: true,
     repeat: true,
     timer: timerObj, 
+    data: {},
   });
 };
