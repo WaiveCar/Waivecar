aa5a0e0890f40a40d0ac1a78afb77b6c371da927
David Leigh <daleighan@gmail.com>
2018-09-25 15:01:03 -0700

    The process now runs correctly at the given interval

--- modules/waivecar/bootstrap/schedules/have-cars-moved.js
+++ modules/waivecar/bootstrap/schedules/have-cars-moved.js
@@ -2,33 +2,54 @@
 let scheduler = Bento.provider('queue').scheduler;
 let Car = Bento.model('Car');
 let bookingService = require('../../lib/booking-service.js');
+let timerSettings = {
+  value: 15,
+  type: 'seconds',
+  incrementTime: 3,
+  incrementUnit: 'hours',
+};
 
-scheduler.process('have-cars-moved', function *(job) {
-  console.log('have cars moved process');
-  console.log('job.data: ', job.data);
-  
-  let carsForNextCheck = !job.data.carsForNextCheck ? {} : job.data.carsForNextCheck;
-
+scheduler.process('have-cars-moved', function*(job) {
+  console.log('running process');
+  let carsForNextCheck = !job.data.carsForNextCheck
+    ? {}
+    : job.data.carsForNextCheck;
   let cars = yield Car.find({
     where: {
       inRepair: false,
-    }
+      isWaivework: false,
+    },
   });
 
   for (let car of cars) {
-    if (yield bookingService.isAtHub(car)) {
-      console.log('car is at hub');
-    } else {
-      console.log('car not at hub');
+    if (!(yield bookingService.isAtHub(car))) {
+      if (!carsForNextCheck[car.license]) {
+        let toStore = {
+          latitude: car.latitude,
+          longitude: car.longitude,
+          hoursSitting: 0,
+        };
+        carsForNextCheck[car.license] = toStore;
+      } else if (
+        carsForNextCheck[car.license].latitude === car.latitude &&
+        carsForNextCheck[car.license].longitude === car.longitude
+      ) {
+        //carsForNextCheck[car.license].hoursSitting += incrementTime;
+        //notify slack
+      }
     }
   }
+  scheduler.add('have-cars-moved', {
+    timer: timerSettings,
+    data: {carsForNextCheck},
+  });
 });
 
 module.exports = function*() {
-  let timerObj = {value: 15, type: 'seconds'};
+  //scheduler.cancel('have-cars-moved');
   scheduler.add('have-cars-moved', {
     init: true,
-    timer: timerObj, 
+    timer: timerSettings,
     data: {},
   });
 };
