2bb15f66774aa1c86702883070c419dd2de515e9
Chris McKenzie <kristopolous@yahoo.com>
2018-09-26 15:57:24 -0700

    Updating the evgo creds

--- config/charging/default.js
+++ config/charging/default.js
@@ -1,7 +1,6 @@
 module.exports = {
   evgo : {
-    token: 'dsakjrh3447sdfgs32985sd',
+    token: '7e64ef7b-20cb-447c-92e0-253605c4edf7',
     cpoUrl: 'https://op.evgo.com/externalIncoming/ocpi/cpo/2.1.1/'
   }
-
 };
