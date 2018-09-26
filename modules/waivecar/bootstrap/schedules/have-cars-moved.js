7e77a79767b2db7fda0cc1c49476f1424f12283d
David Leigh <daleighan@gmail.com>
2018-09-25 18:20:38 -0700

    Wrote the full message to be sent to slack and added some comments

--- modules/waivecar/bootstrap/schedules/have-cars-moved.js
+++ modules/waivecar/bootstrap/schedules/have-cars-moved.js
@@ -11,18 +11,24 @@ let timerSettings = {
 };
 
 scheduler.process('have-cars-moved', function*(job) {
+  // If there are no cars already being passed to this process, a new object is used
   let carsForNextCheck = !job.data.carsForNextCheck
     ? {}
     : job.data.carsForNextCheck;
+
   let cars = yield Car.find({
     where: {
       inRepair: false,
       isWaivework: false,
     },
   });
+
   for (let car of cars) {
+    // Cars are added to/updated in the object if they are not at home
     if (!(yield bookingService.isAtHub(car))) {
       if (!carsForNextCheck[car.license]) {
+        // If the car is not already in the object of cars to check and it is not at a hub,
+        // it is added to the object
         carsForNextCheck[car.license] = {
           latitude: car.latitude,
           longitude: car.longitude,
@@ -33,13 +39,23 @@ scheduler.process('have-cars-moved', function*(job) {
         carsForNextCheck[car.license].latitude === car.latitude &&
         carsForNextCheck[car.license].longitude === car.longitude
       ) {
+        // If the car is already in the object and has not moved, its time is incremented and 
+        // a slack notification is sent out
         carsForNextCheck[car.license].timeSitting += timerSettings.value;
         yield notify.notifyAdmins(
-          `:fishing_pole_and_fish: a message`,
+          `:fishing_pole_and_fish: ${car.license} has been sitting for ${
+            carsForNextCheck[car.license].timeSitting
+          } ${timerSettings.type} at ${carsForNextCheck[car.license].address}`,
           ['slack'],
           {channel: '#rental-alerts'},
         );
       } else {
+        // If the car has moved, it is deleted from the object
+        delete carsForNextCheck[car.license];
+      }
+    } else {
+      // The car is removed from the object if it is at home base
+      if (carsForNextCheck[car.license]) {
         delete carsForNextCheck[car.license];
       }
     }
