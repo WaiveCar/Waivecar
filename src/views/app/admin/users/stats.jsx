7cc1d3d148511a08b9b32b8fc28bc086ad95f70a
David Leigh <daleighan@gmail.com>
2018-09-28 18:15:34 -0700

    There are now some stats for each window of time shown

--- src/views/app/admin/users/stats.jsx
+++ src/views/app/admin/users/stats.jsx
@@ -7,17 +7,23 @@ class Stats extends Component {
     this.state = {
       stats: null,
       timeFrameSelected: 'allTime',
+      currentBookings: [],
+      currentOrders: [],
     };
   }
 
   componentDidMount() {
     api.get(`/users/${this.props.id}/stats`, (err, stats) => {
-      this.setState({stats});
+      this.setState({
+        stats,
+        currentBookings: stats.allTime.bookings,
+        currentOrders: stats.allTime.orders,
+      });
     });
   }
 
-  render = () => {
-    let {stats} = this.state;
+  render() {
+    let {stats, timeFrameSelected, currentBookings, currentOrders} = this.state;
     return (
       <div>
         {stats && (
@@ -41,30 +47,71 @@ class Stats extends Component {
               </div>
               <button
                 className="btn btn-sm btn-primary"
-                onClick={() => this.setState({timeFrameSelected: 'day'})}>
+                onClick={() =>
+                  this.setState({
+                    timeFrameSelected: 'allTime',
+                    currentBookings: stats.allTime.bookings,
+                    currentOrders: stats.allTime.orders,
+                  })
+                }>
+                All Time
+              </button>
+              <button
+                className="btn btn-sm btn-primary"
+                onClick={() =>
+                  this.setState({
+                    timeFrameSelected: 'day',
+                    currentBookings: stats.day.bookings,
+                    currentOrders: stats.day.orders,
+                  })
+                }>
                 24 Hours
               </button>
               <button
                 className="btn btn-sm btn-primary"
-                onClick={() => this.setState({timeFrameSelected: 'week'})}>
+                onClick={() =>
+                  this.setState({
+                    timeFrameSelected: 'week',
+                    currentBookings: stats.week.bookings,
+                    currentOrders: stats.week.orders,
+                  })
+                }>
                 7 Days
               </button>
               <button
                 className="btn btn-sm btn-primary"
-                onClick={() => this.setState({timeFrameSelected: 'month'})}>
+                onClick={() =>
+                  this.setState({
+                    timeFrameSelected: 'month',
+                    currentBookings: stats.month.bookings,
+                    currentOrders: stats.month.orders,
+                  })
+                }>
                 30 Days
               </button>
-              <button
-                className="btn btn-sm btn-primary"
-                onClick={() => this.setState({timeFrameSelected: 'allTime'})}>
-                All Time
-              </button>
+              <div>
+                <label
+                  className="form-control-label"
+                  style={{color: '#666', fontWeight: 300}}>
+                  Total Rides During This Period: {currentBookings.length}
+                </label>
+                <label
+                  className="form-control-label"
+                  style={{color: '#666', fontWeight: 300}}>
+                  Total Spent During this period: ${(
+                    currentOrders.reduce(
+                      (acc, order) => acc + order.amount,
+                      0,
+                    ) / 100
+                  ).toFixed(2)}
+                </label>
+              </div>
             </div>
           </div>
         )}
       </div>
     );
-  };
+  }
 }
 
 export default Stats;
