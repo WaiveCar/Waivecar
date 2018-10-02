6a2336a02231dd7726e8410ddf54f1f2e70f4a45
Chris McKenzie <kristopolous@yahoo.com>
2018-10-01 21:19:38 -0700

    more syntax fixes

--- modules/waivecar/lib/waitlist-service.js
+++ modules/waivecar/lib/waitlist-service.js
@@ -306,11 +306,11 @@ module.exports = {
 
     params.intro = "Thanks for your patience. It's paid off because you are next in line and we've created your account.";
 
-    console.log(idList[ix]);
+    console.log(idList);
     for(var ix = 0; ix < idList.length; ix++) {
-      let userRecord = yield User.findOne({ 
+      let userRecord = yield User.findOne({where: { 
         id: idList[ix]
-      });
+      }});
       console.log(idList[ix], userRecord);
 
       if (userRecord) {
