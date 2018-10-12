e4b9f0e14d40277e7bb27d70fcaacc070dc1d24f
David Leigh <daleighan@gmail.com>
2018-10-12 13:55:19 -0700

    Changed some formatting of the text message sent out when a reservation is created

--- modules/waivecar/lib/booking-service.js
+++ modules/waivecar/lib/booking-service.js
@@ -300,7 +300,7 @@ module.exports = class BookingService extends Service {
     // If the car is currently WaiveParked, the notes from the spot need to be attached to the message.
     let currentParking = yield UserParking.findOne({ where: { carId: car.id } });
 
-    let msg = `${car.license} is yours! ${currentParking && `It is WaiveParked with the notes ${currentParking.notes}. `}${inject} ${timeToCar} minutes to get to it. Thanks!`;
+    let msg = `${car.license} is yours! ${currentParking ? `It is WaiveParked with the notes: "${currentParking.notes}". ` : ''}${inject} ${timeToCar} minutes to get to it. Thanks!`;
     if (isLevel) {
       // https://lb.waivecar.com/users/14827
       yield notify.sendTextMessage(14827, `${ driver.name() } reserved ${ car.license }.`);
