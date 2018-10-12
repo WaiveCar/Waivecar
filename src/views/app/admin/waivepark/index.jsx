2e7607ea1c463f9d53b20f82630c52bdb21b683a
David Leigh <daleighan@gmail.com>
2018-10-12 16:49:10 -0700

    Added some structure and the necessary api call to get all spaces

--- src/views/app/admin/waivepark/index.jsx
+++ src/views/app/admin/waivepark/index.jsx
@@ -1,13 +1,35 @@
 import React, {Component} from 'react';
+import {api} from 'bento';
+import {snackbar} from 'bento-web';
 
 class WaivePark extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      spaces: [],
+    };
+  }
+
+  componentDidMount() {
+    api.get('/parking', (err, response) => {
+      if (err) {
+        snackbar.notify({
+          type    : 'danger',
+          message : 'Error fetching spaces.'
+        });
+      }
+      console.log('response: ', response);
+    });
   }
 
   render = () => (
-    <div>
-      WaivePark
+    <div className="container">
+      <div className="box full">
+        <h3>WaivePark Spaces</h3>
+        <div className="box-content">
+          Box Content
+        </div>
+      </div>
     </div>
   );
 }
