e2e170fa9de5b67777daf975312e8fe0e6c06b69
David Leigh <daleighan@gmail.com>
2018-10-12 19:20:24 -0700

    Added some styles and a link to the user

--- src/views/app/admin/waivepark/show.jsx
+++ src/views/app/admin/waivepark/show.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ParkingActions from '../../components/user/user-parking/parking-actions.jsx';
 import Space from '../../components/user/user-parking/space.jsx';
+import { Link } from 'react-router';
 import {api, auth} from 'bento';
 import {snackbar} from 'bento-web';
 
@@ -16,7 +17,7 @@ export default class ParkingShow extends ParkingActions {
     this.getSpaces(this.props.params.id);
   }
 
-  getSpaces = (id) => {
+  getSpaces = id => {
     api.get(`/parking/${id}`, (err, space) => {
       if (err) {
         return snackbar.notify({
@@ -24,27 +25,38 @@ export default class ParkingShow extends ParkingActions {
           message: `Error: ${err.message}`,
         });
       }
+      console.log('space: ', space);
       this.setState({space});
     });
-  }
+  };
 
   render = () => {
     let {getSpaces, toggleSpace, deleteSpace, updateSpace, removeCar} = this;
     let {space} = this.state;
     return (
       <div>
-        Parking Show
         {space && (
-          <Space
-            space={space}
-            toggleSpace={toggleSpace}
-            deleteSpace={deleteSpace}
-            updateSpace={updateSpace}
-            removeCar={removeCar}
-            space={space}
-            admin={auth.user().hasAccess('admin')}
-            show={true}
-          />
+          <div className="box parking-box">
+            <h3>
+              Manage <Link to={`/users/${space.owner.id}`}>{`${space.owner.firstName} ${space.owner.lastName}`}</Link>'s
+              WaiveSpot
+            </h3>
+            <div className="box-content">
+              Parking Show
+              {space && (
+                <Space
+                  space={space}
+                  toggleSpace={toggleSpace}
+                  deleteSpace={deleteSpace}
+                  updateSpace={updateSpace}
+                  removeCar={removeCar}
+                  space={space}
+                  admin={auth.user().hasAccess('admin')}
+                  show={true}
+                />
+              )}
+            </div>
+          </div>
         )}
       </div>
     );
