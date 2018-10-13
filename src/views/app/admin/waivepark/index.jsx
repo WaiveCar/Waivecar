e6eb30d585110184864f9c696126175243d7207e
David Leigh <daleighan@gmail.com>
2018-10-12 17:06:07 -0700

    Spaces are now shown on the page

--- src/views/app/admin/waivepark/index.jsx
+++ src/views/app/admin/waivepark/index.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {api} from 'bento';
 import {snackbar} from 'bento-web';
+import Space from '../../components/user/user-parking/space.jsx';
 
 class WaivePark extends Component {
   constructor(props) {
@@ -14,24 +15,27 @@ class WaivePark extends Component {
     api.get('/parking', (err, response) => {
       if (err) {
         snackbar.notify({
-          type    : 'danger',
-          message : 'Error fetching spaces.'
+          type: 'danger',
+          message: 'Error fetching spaces.',
         });
       }
-      console.log('response: ', response);
+      this.setState({spaces: response});
     });
   }
 
-  render = () => (
-    <div className="container">
-      <div className="box full">
-        <h3>WaivePark Spaces</h3>
-        <div className="box-content">
-          Box Content
+  render = () => {
+    let {spaces} = this.state;
+    return (
+      <div className="container">
+        <div className="box full">
+          <h3>WaivePark Spaces</h3>
+          <div className="box-content">
+            {spaces && spaces.map((space, i) => <Space key={i} space={space} />)}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
 }
 
 export default WaivePark;
