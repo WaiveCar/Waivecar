e270c5d51f8f50a2e41594a7635efec24eb0b80b
David Leigh <daleighan@gmail.com>
2018-10-12 18:20:37 -0700

    Added a map

--- src/views/app/admin/waivepark/index.jsx
+++ src/views/app/admin/waivepark/index.jsx
@@ -3,6 +3,7 @@ import {api, auth} from 'bento';
 import {snackbar} from 'bento-web';
 import ParkingActions from '../../components/user/user-parking/parking-actions.jsx';
 import Space from '../../components/user/user-parking/space.jsx';
+import {GMap} from 'bento-web';
 
 export default class WaivePark extends ParkingActions {
   componentDidMount() {
@@ -24,18 +25,34 @@ export default class WaivePark extends ParkingActions {
   render = () => {
     let {spaces} = this.state;
     let {getSpaces, toggleSpace, deleteSpace, updateSpace, removeCar} = this;
+    let markers = spaces.map(space => space.location);
     return (
       <div className="container">
-        <div className="box">
+        <div className="box full">
           <h3>WaivePark Spaces</h3>
           <div className="box-content">
-              <div className="btn-group" role="group" style={{marginBottom: '30px'}}>
-                <button
-                  className="btn btn-primary-outline btn-wave"
-                  onClick={() => getSpaces()}>
-                  Refresh Status
-                </button>
+            {spaces.length && (
+              <div className="row">
+                <div className="col-xs-12">
+                  <div className="map-dynamic">
+                    <GMap
+                      markerIcon={'/images/map/active-waivecar.svg'}
+                      markers={markers}
+                    />
+                  </div>
+                </div>
               </div>
+            )}
+            <div
+              className="btn-group"
+              role="group"
+              style={{marginBottom: '30px'}}>
+              <button
+                className="btn btn-primary-outline btn-wave"
+                onClick={() => getSpaces()}>
+                Refresh Status
+              </button>
+            </div>
             {spaces &&
               spaces.map((space, i) => (
                 <Space
@@ -57,4 +74,3 @@ export default class WaivePark extends ParkingActions {
     );
   };
 }
-
