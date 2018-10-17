e952accd633f2e393baa7eeba6ad8d6e9214fbee
David Leigh <daleighan@gmail.com>
2018-10-16 19:42:03 -0700

    The damage photos should now render the rows of photos

--- src/views/app/admin/cars/show.jsx
+++ src/views/app/admin/cars/show.jsx
@@ -611,8 +611,15 @@ class CarsShowView extends React.Component {
       let angles = bookingList.filter(item => item.type !== 'other');
       rowsToRender = [angles.slice(0, 4), angles.slice(4), other];
     } else {
-      rowsToRender.push(bookingList.filter(item => item.type === damageFilter));
+      console.log('bookingList: ', bookingList);
+      if (damageFilter) {
+        rowsToRender = [[bookingList]]
+      } else {
+        rowsToRender.push(bookingList.filter(item => item.type === damageFilter));
+      }
     }
+    console.log('rowsToRender: ', rowsToRender);
+    console.log('API_URI', API_URI);
     return (
       <div>
         {(rowsToRender[0] && rowsToRender[0].length > 0) &&
@@ -623,16 +630,17 @@ class CarsShowView extends React.Component {
             </h4>
             <div>
               {rowsToRender.map((row, i) => {
+                console.log('row: ', row);
                 return (
                   <div key={i} className="dmg-row">
-                    {row.map((image, j) =>  
-                      { image && image.file && 
+                    {row.map((image, j) =>  { 
+                      return (image && image.file && 
                         <div>
                           <a className="damage-image-holder" href={`${API_URI}/file/${image.file.id}` } target="_blank" key={j}>
                             <div>{image.type}</div>
                             <img className="damage-image" src={`${API_URI}/file/${image.file.id}`} />
                           </a>
-                        </div>
+                        </div>)
                       }
                     )}
                   </div>
