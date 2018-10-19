3e857fd14afba82c2d6df747e31610f2156cce11
Chris McKenzie <kristopolous@yahoo.com>
2018-10-18 23:36:46 -0700

    trying to fix the end XX

--- modules/waivecar/lib/contact-service.js
+++ modules/waivecar/lib/contact-service.js
@@ -69,12 +69,6 @@ module.exports = {
       return true;
     }
 
-    // this covers phrases like "end please or end waive
-    if(command.match(/^end(\s\w+|)$/i)) {
-      let message = `${ who } (${ phone }) send "${ params.query.Body }" and the computer finished the ride automatically`;
-      yield notify.slack({ text : message }, { channel : '#app_support' });
-      command = 'finish';
-    }
 
     // alias commands are blank.
     let documentation = {
@@ -110,6 +104,13 @@ module.exports = {
           return true;
         }
       }
+    }
+
+    // this covers phrases like "end please or end waive
+    if(command.match(/^end(\s\w+|)$/i) && user) {
+      let message = `${ user.link() } (${ phone }) send "${ params.query.Body }" and the computer finished the ride automatically`;
+      yield notify.slack({ text : message }, { channel : '#app_support' });
+      command = 'finish';
     }
 
     // now we can do the simple ones.
