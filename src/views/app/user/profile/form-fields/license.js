49a20bd13aa25bd4efdfb556c089755dcf30465d
David Leigh <daleighan@gmail.com>
2018-10-29 13:55:51 -0700

    Added new fields for additional license information

--- src/views/app/user/profile/form-fields/license.js
+++ src/views/app/user/profile/form-fields/license.js
@@ -7,13 +7,23 @@ let layout = fields.mergeFromLayout('licenses', [
     { name : 'lastName',   className : 'col-md-4 bento-form-input' }
   ],
   [
+    { name : 'street1', className : 'col-md-12 bento-form-input' },
+  ],
+  [
+    { name : 'street2', className : 'col-md-12 bento-form-input' },
+  ],
+  [
+    { name : 'city', className : 'col-md-6 bento-form-input' },
+    { name : 'state',  className : 'col-md-6 bento-form-select' }
+  ],
+  [
+    { name : 'zip', className : 'col-md-6 bento-form-input' },
+    { name : 'number', className : 'col-md-6 bento-form-input' },
+  ],
+  [
     { name : 'birthDate', className : 'col-md-6 bento-form-input' },
     { name : 'expirationDate', className : 'col-md-6 bento-form-input' }
   ],
-  [
-    { name : 'number', className : 'col-md-6 bento-form-input' },
-    { name : 'state',  className : 'col-md-6 bento-form-select' }
-  ]
 ]);
 
 module.exports = layout;
