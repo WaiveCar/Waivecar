4cc3d12fb5b0f24e1126a3cd96df9f034027da71
Chris McKenzie <kristopolous@yahoo.com>
2018-11-09 18:44:38 -0800

    Updating tikd api key

--- config/tikd/default.js
+++ config/tikd/default.js
@@ -1,6 +1,6 @@
 module.exports = {
   tikd : {
-    key: 'uDifxh7F4i5JyDJX0T5Bw9SdivDz0iiQ51l1l0HQ',
+    key: '1P4FkQSpBg4cjZSr5PGcCPvmrClQemA7BtwJQyrh',
     ep: 'https://rjbsapa8a9.execute-api.us-east-1.amazonaws.com/dev/'
   }
 };
