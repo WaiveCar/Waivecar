c5c0735fb94c26ccbcdd6fb2a453bc273a926c09
Chris McKenzie <kristopolous@yahoo.com>
2018-11-14 08:26:16 -0800

    more debugging for location

--- modules/waivecar/controllers/locations-controller.js
+++ modules/waivecar/controllers/locations-controller.js
@@ -43,7 +43,7 @@ Bento.Register.ResourceController('Location', 'LocationsController', function(co
     console.log(user);
     if(user) {
       matchSet = [6];
-      if !(yield user.isTagged('la')) {
+      if (!(yield user.isTagged('la'))) {
         matchSet = yield user.getTagList(false, 'id');
         console.log(matchSet);
       }
