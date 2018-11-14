803b3b56c8e4cb5691c23a7f75870c31e2da026e
Chris McKenzie <kristopolous@yahoo.com>
2018-11-14 08:39:20 -0800

    more debugging

--- modules/waivecar/controllers/locations-controller.js
+++ modules/waivecar/controllers/locations-controller.js
@@ -40,6 +40,8 @@ Bento.Register.ResourceController('Location', 'LocationsController', function(co
       ]};
     }
 
+    let isLevel = user && (yield user.isTagged('level'));
+    /*
     if(user) {
       matchSet = [6];
       let tlist = yield user.getTagList(false, 'id');
@@ -54,11 +56,15 @@ Bento.Register.ResourceController('Location', 'LocationsController', function(co
       console.log(query.include[1].where);
     }
     console.log(query);
+    */
 
     // Here is how we filtre out user parking outside of a zone. 
     // We use the obscure fact that Array.map, unluke the other 
     // Array functions can take a generator.
     let locations =  yield (yield Location.find(query)).map(function*(row) {
+      if(row.id === 1246 && !isLevel) {
+        return false;
+      }
       if(row.shape) {
         row.shape = JSON.parse(row.shape);
       }
