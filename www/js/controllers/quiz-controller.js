eb24cd6ce04bb6f7aed2f9005e9f6ffffd671983
Chris McKenzie <kristopolous@yahoo.com>
2018-11-16 04:36:57 -0800

    updating the quiz and faq

--- www/js/controllers/quiz-controller.js
+++ www/js/controllers/quiz-controller.js
@@ -20,7 +20,7 @@ function QuizController($injector, $stateParams, $scope, $interval, $data){
           ]
         },
         {
-          prompt: 'WaiveCars are free for the first 2 hours. When may there be charges?',
+          prompt: 'WaiveCars are free for the first 3 hours. When may there be charges?',
           answerList: [
             [ 0, 'Never. I can do whatever I please!', 'Unfortunately there\'s a bit of responsibilty.' ],
             [ 0, 'If I get a ticket, am at fault for an accident or otherwise damage the car', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
@@ -35,6 +35,36 @@ function QuizController($injector, $stateParams, $scope, $interval, $data){
             [ 0, 'The shoulder of the New Jersey Turnpike', "That's silly. I mean come on, really?" ],
             [ 0, 'Any street parking space', 'The cars have to be convenient for other residents.' ],
             [ 1, 'The 24 hour lot at 34 North 7th Street', 'Correct Again! All bookings must end here.' ]
+          ]
+        }
+      ];
+    } else if($data.me.hasTag('csula')) {
+      $scope.questionList = [
+        {
+          prompt: 'How do you fuel a WaiveCar?',
+          answerList: [
+            [ 0, '87 Octane at a gas station', "That's a BIG NO! These cars take a special kind of fuel." ],
+            [ 1, 'On campus for free between 8AM-5PM at the Hydrogen station, off campus at my expense', 'Correct. Hydrogen fuel off campus is expensive'],
+            [ 0, 'They are electric and you plug them in to a wall', 'No. Their are many fine electric cars, but these are Hydrogen Fuel Cells.'],
+            [ 0, 'The cars run forever on a perpetual motion machine', "What Sorcery! No, sorry."]
+          ]
+        },
+        {
+          prompt: 'WaiveCars are free for the first 2 hours. When may there be charges?',
+          answerList: [
+            [ 0, 'Never. I can do whatever I please!', 'Unfortunately there\'s a bit of responsibilty.' ],
+            [ 0, 'If I get a ticket, am at fault for an accident or otherwise damage the car', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
+            [ 0, 'If I abandon the car at a really low fuel level or make it really messy', 'Yes, there\'s a few more to add to this list. (hint hint).' ],
+            [ 1, 'Both B and C', 'Correct. Unfortunately if you get speeding tickets, that\'s still on you.' ]
+          ]
+        },
+        {
+          prompt: 'I can end my ride at',
+          answerList: [
+            [ 0, 'Any place on campus', 'Almost! You get special parking spaces.' ],
+            [ 0, 'The bottom of the LA river', "That's silly. I mean come on, really?" ],
+            [ 0, 'Any street parking space', 'The cars have to be convenient for others.' ],
+            [ 1, 'The marked spaces at the lot I picked it up at', 'Correct Again! All bookings must end here.' ]
           ]
         }
       ];
