79b48f6d0e01d012a38104b8c97ef8c2a4b2cb22
Chris McKenzie <kristopolous@yahoo.com>
2018-11-23 15:58:43 -0800

    Updating run script to try to trace deprecation warnings

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #DEBUG=api:* NODE_ENV=development nodemon -L --debug run.js |& tee -a /tmp/log.log
 ENV=development
 [ -e .env ] && source .env
-DEBUG=api:* NODE_ENV=$ENV node run.js |& tee -a /tmp/log.log
+DEBUG=api:* NODE_ENV=$ENV node --trace-warnings run.js |& tee -a /tmp/log.log
