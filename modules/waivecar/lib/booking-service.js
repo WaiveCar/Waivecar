b5d4884b9d1d4df0dc5c5a03bf2888a43bba5fa1
Chris McKenzie <kristopolous@yahoo.com>
2018-12-03 15:58:06 -0800

    shorter messaging

--- modules/waivecar/lib/booking-service.js
+++ modules/waivecar/lib/booking-service.js
@@ -1487,7 +1487,7 @@ module.exports = class BookingService extends Service {
 
     let message = yield this.updateState('completed', _user, user);
     let rebookCost = booking.isFlagged('rebook') ? 13 : 5;
-    yield notify.sendTextMessage(user, `You're all done! Reply "rebook" to rebook now for $${rebookCost}. Left a thing behind? Reply "unlock" to get it in the next 5min.`);
+    yield notify.sendTextMessage(user, `You're all done! Reply "rebook" to rebook for $${rebookCost}. Forget something? Reply "unlock" to get it in the next 5min.`);
     yield notify.slack({ text : `:coffee: ${ message } ${ car.info() } ${ zoneString } ${ address } ${ booking.link() }` }, { channel : '#reservations' });
     yield LogService.create({ bookingId : booking.id, carId : car.id, userId : user.id, action : Actions.COMPLETE_BOOKING }, _user);
 
