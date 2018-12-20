ca09054d15b7cb77b9732d213515672ab2485a12
David Leigh <daleighan@gmail.com>
2018-12-20 13:14:32 -0800

    The sql query has now been added to the script for fetching tikd info

--- tools/fetch-tikd-info.py
+++ tools/fetch-tikd-info.py
@@ -6,7 +6,8 @@ cursor = mysql_connection.cursor()
 with open('./tikd-sheet.csv', 'r') as f:
     reader = csv.reader(f)
     rows = list(reader)
-    for row in rows:
+    for i in range(1, len(rows)):
+        row = rows[i]
         if not len(row[18]):
             issue_time = row[13]
             issue_date = row[14]
@@ -15,5 +16,75 @@ with open('./tikd-sheet.csv', 'r') as f:
             naive = datetime.datetime.strptime ('{} {}'.format(issue_date, issue_time), '%m/%d/%Y %H:%M:%S %p')
             local_dt = local.localize(naive, is_dst=None)
             utc_dt = local_dt.astimezone(pytz.utc)
+            try:
+                cursor.execute('''
+                    select 
+                      license, 
+                      plate, 
+                      booking_id, 
+                      info.user_id, 
+                      info.stripe_id, 
+                      licenses.number, 
+                      info.email, 
+                      info.first_name, 
+                      info.last_name, 
+                      licenses.street_1, 
+                      licenses.street_2, 
+                      licenses.city, 
+                      licenses.state, 
+                      licenses.zip 
+                    from 
+                      (
+                        select 
+                          booking.car_id, 
+                          booking.license, 
+                          booking.plate, 
+                          booking.user_id, 
+                          booking.booking_id, 
+                          users.email, 
+                          users.first_name, 
+                          users.last_name, 
+                          users.stripe_id 
+                        from 
+                          (
+                            select 
+                              car.id as car_id, 
+                              car.license, 
+                              car.plate, 
+                              bookings.id as booking_id, 
+                              bookings.user_id 
+                            from 
+                              (
+                                select 
+                                  id, 
+                                  license, 
+                                  plate_number as plate 
+                                from 
+                                  cars 
+                                where 
+                                  plate_number = "{}"
+                              ) as car 
+                              right join bookings on bookings.car_id = car.id 
+                            where 
+                              bookings.car_id = car.id 
+                              and bookings.created_at < '{}' 
+                            order by 
+                              bookings.id desc 
+                            limit 
+                              1
+                          ) as booking 
+                          right join users on booking.user_id = users.id 
+                        where 
+                          booking.user_id = users.id
+                      ) as info 
+                      right join licenses on licenses.user_id = info.user_id 
+                    where 
+                      licenses.user_id = info.user_id;
+                '''.format(plate_number, utc_dt))
+            except exception as e:
+                print('error executing query: ', e)
+            for item in cursor:
+                print(row)
+                print(item)
 
 mysql_connection.close()
