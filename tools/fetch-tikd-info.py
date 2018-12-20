fea7235e3d029497fae4920ad0d46d5e434176de
David Leigh <daleighan@gmail.com>
2018-12-20 12:38:31 -0800

    Formatted the time to utc in the script for getting more info for tikd

--- tools/fetch-tikd-info.py
+++ tools/fetch-tikd-info.py
@@ -1,4 +1,4 @@
-import csv
+import csv, pytz, datetime
 
 with open('./tikd-sheet.csv', 'r') as f:
     reader = csv.reader(f)
@@ -8,4 +8,9 @@ with open('./tikd-sheet.csv', 'r') as f:
             issue_time = row[13]
             issue_date = row[14]
             plate_number = row[16]
-            print(issue_time, issue_date, plate_number)
+            local = pytz.timezone ("America/Los_Angeles")
+            naive = datetime.datetime.strptime ("{} {}".format(issue_date, issue_time), "%m/%d/%Y %H:%M:%S %p")
+            local_dt = local.localize(naive, is_dst=None)
+            utc_dt = local_dt.astimezone(pytz.utc)
+            print("local: ", local_dt)
+            print("utc: ", utc_dt)
