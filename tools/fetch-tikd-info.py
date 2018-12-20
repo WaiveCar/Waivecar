cac06f388c657c0fbd6d6669fcf5555f370b4701
David Leigh <daleighan@gmail.com>
2018-12-19 20:05:24 -0800

    Took the pertinent info from the csv to query for additional information from tikd

--- tools/fetch-tikd-info.py
+++ tools/fetch-tikd-info.py
@@ -2,5 +2,10 @@ import csv
 
 with open('./tikd-sheet.csv', 'r') as f:
     reader = csv.reader(f)
-    your_list = list(reader)
-    print(your_list)
+    rows = list(reader)
+    for row in rows:
+        if not len(row[18]):
+            issue_time = row[13]
+            issue_date = row[14]
+            plate_number = row[16]
+            print(issue_time, issue_date, plate_number)
