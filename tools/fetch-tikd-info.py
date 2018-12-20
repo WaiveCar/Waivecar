755c909252d214c06a6856966870722bae592ba2
David Leigh <daleighan@gmail.com>
2018-12-20 12:50:52 -0800

    The script for fetching tikd info now connects to the db

--- tools/fetch-tikd-info.py
+++ tools/fetch-tikd-info.py
@@ -1,4 +1,7 @@
 import csv, pytz, datetime
+import MySQLdb as mysql
+mysql_connection = mysql.connect(database='waivecar_development', user='waivecar', password='eNwlGGl6g6V0w0qX3vx0S5GKbGvTtR3X')
+cursor = mysql_connection.cursor()
 
 with open('./tikd-sheet.csv', 'r') as f:
     reader = csv.reader(f)
@@ -8,9 +11,9 @@ with open('./tikd-sheet.csv', 'r') as f:
             issue_time = row[13]
             issue_date = row[14]
             plate_number = row[16]
-            local = pytz.timezone ("America/Los_Angeles")
-            naive = datetime.datetime.strptime ("{} {}".format(issue_date, issue_time), "%m/%d/%Y %H:%M:%S %p")
+            local = pytz.timezone ('America/Los_Angeles')
+            naive = datetime.datetime.strptime ('{} {}'.format(issue_date, issue_time), '%m/%d/%Y %H:%M:%S %p')
             local_dt = local.localize(naive, is_dst=None)
             utc_dt = local_dt.astimezone(pytz.utc)
-            print("local: ", local_dt)
-            print("utc: ", utc_dt)
+
+mysql_connection.close()
