fee55ce7e3b97db29a99c058130204852e37ee16
Chris McKenzie <kristopolous@yahoo.com>
2018-12-26 23:01:13 -0800

    Always giving a user if a car is assigned

--- modules/waivecar/bootstrap/schedules/bookings-in-last-day.js
+++ modules/waivecar/bootstrap/schedules/bookings-in-last-day.js
@@ -53,9 +53,9 @@ function *showBookings() {
     for(let car of carList) {
       let lastBooking = Math.round((new Date() - car.bookings[0].createdAt) / 1000 / 24 / 60 / 60);
       let warn = car.isAvailable ? " AVAILABLE" : (!car.inRepair ? " NOT IN REPAIR" : "");
-      if(warn == '' && car.userId) {
+      if(car.userId) {
         let user = yield User.findById(car.userId);
-        warn = user.link();
+        warn += ' ' + user.link();
       }
       row.push(`${car.link()} ${lastBooking}d (${car.averageCharge()}%) ${warn}`);
     }
