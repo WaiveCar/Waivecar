259ac299a0771c5215f81c5e709fb233f8b81692
Chris McKenzie <kristopolous@yahoo.com>
2018-12-26 22:38:33 -0800

    Output fix

--- modules/waivecar/bootstrap/schedules/bookings-in-last-day.js
+++ modules/waivecar/bootstrap/schedules/bookings-in-last-day.js
@@ -31,7 +31,7 @@ function *showBookings() {
     if (car.license.search(/(waive|csula)/i) != -1 && car.bookings.length) {
       let lastBooking = new Date() - car.bookings[0].createdAt;
       if(lastBooking / 1000 / 24 / 60 / 60 > 0.8) { 
-        let key = (2*car.latitude).toFixed(2) + (2*car.longitude).toFixed(2);
+        let key = (car.latitude).toFixed(2) + (car.longitude).toFixed(2);
         if(!locHash[key]) {
           locHash[key] = [];
         }
@@ -51,7 +51,7 @@ function *showBookings() {
     for(let car of carList) {
       let lastBooking = Math.round((new Date() - car.bookings[0].createdAt) / 1000 / 24 / 60 / 60);
       let warn = car.isAvailable ? " AVAILABLE" : (!car.inRepair ? " NOT IN REPAIR" : "");
-      row.push(`${car.license} (${car.averageCharge()}%${warn}) last booked ${lastBooking}d ago`);
+      row.push(`${car.license} ${lastBooking}d (${car.averageCharge()}) %${warn}`);
     }
     output.push(`*${header}*` + row.join("\n… "));
   }
