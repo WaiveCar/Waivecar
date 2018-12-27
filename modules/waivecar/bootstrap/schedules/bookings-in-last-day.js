f0f12798a32133f0c4e06ee077287f66357f4e32
Chris McKenzie <kristopolous@yahoo.com>
2018-12-26 22:13:59 -0800

    Reworking the bookings in last day code

--- modules/waivecar/bootstrap/schedules/bookings-in-last-day.js
+++ modules/waivecar/bootstrap/schedules/bookings-in-last-day.js
@@ -7,52 +7,59 @@ let notify = require('../../lib/notification-service');
 let geocodeService = require('../../lib/geocoding-service');
 let redis     = require('../../lib/redis-service');
 
-scheduler.process('bookings-in-last-day', function*(job) {
+function *showBookings() {
   if (!(yield redis.shouldProcess('bookings-in-last-day', (new Date()).getDay(), 90 * 1000))) {
     return;
   }
   // This queries for all cars and their bookings from the last 24 hours
   let carsToCheck = yield Car.find({
-    where: {
-      isWaivework: false,
-      inRepair: false,
-    },
     include: [
       {
         model: 'Booking',
         as: 'bookings',
         required: false,
-        where: {
-          createdAt: {
-            $gt: moment().subtract(1, 'days'),
-          },
-        },
+        limit: 1,
+        order: [['created_at','desc']],
       },
     ],
   });
 
-  // puts cars near each other approximately next to each other.
-  carsToCheck.sort(function(a, b) { return a.latitude - b.latitude + (a.longitude - b.longitude) } );
-
+  let output = [];
+  let locHash = {};
+  carsToCheck.sort(function(a, b) { return a.latitude - b.latitude + (a.longitude - b.longitude) } ); 
   for (let car of carsToCheck) {
-    if (car.bookings.length === 0 && car.license.search(/work/) === -1) {
-      // If there are no bookings from the last 24 hours, the address of the car is
-      // found and the slack notification is sent out
-      let address = yield geocodeService.getAddress(
-        car.latitude,
-        car.longitude,
-      );
-      // if the car is available and not in repair, this is a bad sign
-      let warn = car.isAvailable ? " *AVAILABLE*" : (!car.inRepair ? " *NOT IN REPAIR*" : "");
-      yield notify.notifyAdmins(
-        `:sleeping_accommodation: ${
-          car.license
-        } (${car.range}%${warn}) has not been booked in the last 24 hours. It is currently at ${address}`,
-        ['slack'],
-        {channel: '#rental-alerts'},
-      );
+    if (car.license.search(/work/) === -1 && car.bookings.length) {
+      let lastBooking = new Date() - car.bookings[0].createdAt;
+      if(lastBooking / 1000 / 24 / 60 / 60 > 0.8) { 
+        let key = (2*car.latitude).toFixed(2) + (2*car.longitude).toFixed(2);
+        if(!locHash[key]) {
+          locHash[key] = [];
+        }
+        locHash[key].push(car);
+      }
     }
   }
+
+  for (let key in locHash) {
+    let carList = locHash[key];
+    let header = yield geocodeService.getAddress(
+      carList[0].latitude,
+      carList[0].longitude,
+    );
+    let row = [];
+    
+    for(let car of carList) {
+      let lastBooking = Math.round((new Date() - car.bookings[0].createdAt) / 1000 / 24 / 60 / 60);
+      let warn = car.isAvailable ? " *AVAILABLE*" : (!car.inRepair ? " *NOT IN REPAIR*" : "");
+      row.push(`${car.license} (${car.averageCharge()}%${warn}) last booked ${lastBooking}d ago`);
+    }
+    output.push(header + "\n " + row.join("\n "));
+  }
+  yield notify.notifyAdmins(
+    ':sleeping_accommodation: ' + output.join("\n"),
+    ['slack'],
+    {channel: '#rental-alerts'},
+  );
 
   scheduler.add('bookings-in-last-day', {
     timer: {
@@ -60,6 +67,10 @@ scheduler.process('bookings-in-last-day', function*(job) {
       type: 'hours',
     },
   });
+}
+
+scheduler.process('bookings-in-last-day', function*(job) {
+  yield showBookings();
 });
 
 module.exports = function*() {
@@ -76,17 +87,18 @@ module.exports = function*() {
       ? moment()
           .tz('America/Los_Angeles')
           .hours(22)
-          .minutes(35)
+          .minutes(0)
           .seconds(0)
       : moment()
           .tz('America/Los_Angeles')
           .add(1, 'days')
           .hours(22)
-          .minutes(35)
+          .minutes(0)
           .seconds(0);
   // This gets the seconds until the cars are to be marked unavailable
   let secondsUntilTime = Math.abs(moment().diff(timeToCheck, 'seconds'));
   let timerObj = {value: secondsUntilTime, type: 'seconds'};
+  yield showBookings();
   scheduler.add('bookings-in-last-day', {
     timer: timerObj,
   });
