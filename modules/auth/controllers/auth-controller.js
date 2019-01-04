63064459e1b86058c4130dd846eef755cc84a1c0
Chris McKenzie <kristopolous@yahoo.com>
2019-01-04 14:18:04 -0800

    cleanup

--- modules/auth/controllers/auth-controller.js
+++ modules/auth/controllers/auth-controller.js
@@ -4,18 +4,10 @@ let auth = require('../lib/auth-service');
 
 Bento.Register.Controller('AuthController', (controller) => {
 
-  /**
-   * Attempts to authenticate a user with the api.
-   * @return {Object}
-   */
   controller.login = function *() {
     return yield auth.login(this.payload);
   };
 
-  /**
-   * Attempts to authenticate the user with the api via facebook.
-   * @return {Object}
-   */
   controller.facebook = function *() {
     return yield auth.social('facebook', this.payload, this.auth.user);
   };
@@ -24,18 +16,10 @@ Bento.Register.Controller('AuthController', (controller) => {
     yield this.auth.remember();
   };
 
-  /**
-   * Attempts to validate the authenticated user.
-   * @return {Void}
-   */
   controller.validate = function *() {
     // If we hit this method we are validated
   };
 
-  /**
-   * Attempts to log the user out of the api.
-   * @return {Void}
-   */
   controller.logout = function *() {
     return yield this.auth.logout(this.query.from);
   };
