e53c07e902cc5f4516c0b323c3b6eeab8049d3ef
Chris McKenzie <kristopolous@yahoo.com>
2019-01-04 17:17:59 -0800

    Reducing charging credit in half

--- modules/waivecar/lib/booking-service.js
+++ modules/waivecar/lib/booking-service.js
@@ -1280,7 +1280,7 @@ module.exports = class BookingService extends Service {
     if(deltas.charge > 0) {
       // we err in the user's favor by using the high estimates
       let miles = Math.floor((deltas.charge / 100) * car.getRange('LOW') * 0.98);
-      let credit = Math.floor(miles / 5);
+      let credit = Math.floor(miles / 10);
       if(credit > 0) {
         yield booking.addFlag('charge');
         yield OrderService.quickCharge({
