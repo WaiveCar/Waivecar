49d9740d019a608897c6cce650718bf2d68dfd5d
David Leigh <daleighan@gmail.com>
2019-01-10 13:35:47 -0800

    Cleanup of waivework-auto-charge process and creation of the booking payment in it

--- modules/waivecar/bootstrap/schedules/waivework-auto-charge.js
+++ modules/waivecar/bootstrap/schedules/waivework-auto-charge.js
@@ -1,16 +1,14 @@
 let scheduler = Bento.provider('queue').scheduler;
 let WaiveworkPayment = Bento.model('WaiveworkPayment');
 let OrderService = Bento.module('shop/lib/order-service');
-let BookingPayment = Bento.module('BookingPayment');
+let BookingPayment = Bento.model('BookingPayment');
 let moment = require('moment');
 
 scheduler.process('waivework-auto-charge', function*(job) {
-  console.log('auto-charge here');
   let today = moment();
   //if ([1, 8, 15, 22].includes(today.date())) {
   // The next line needs to be removed later
   today = today.add(5, 'days');
-  console.log(today);
   let todaysPayments = yield WaiveworkPayment.find({
     where: {
       date: {$lt: today.add(1, 'days').format('YYYY-MM-DD')},
@@ -23,15 +21,18 @@ scheduler.process('waivework-auto-charge', function*(job) {
       },
     ],
   });
-  console.log('todays payments: ', todaysPayments);
   for (let payment of todaysPayments) {
-    console.log(payment);
     let data = {
       userId: payment.booking.userId,
       amount: payment.amount,
     };
-    let shopOrder = yield OrderService.quickCharge(data)
-    console.log('shopOrder: ', shopOrder);
+    let shopOrder = (yield OrderService.quickCharge(data)).order;
+    let bookingPayment = new BookingPayment({
+      bookingId: payment.booking.id,
+      orderId: shopOrder.id,
+    });
+    yield bookingPayment.save();
+    console.log('bookingPayment: ', bookingPayment);
   }
   //}
 });
