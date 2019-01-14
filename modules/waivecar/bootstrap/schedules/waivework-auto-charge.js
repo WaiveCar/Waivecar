dd60aae85b8aca553ac1cf20415d84e5a5964a6f
David Leigh <daleighan@gmail.com>
2019-01-14 13:35:21 -0800

    Added a redis check for the waivework-auto-charge process

--- modules/waivecar/bootstrap/schedules/waivework-auto-charge.js
+++ modules/waivecar/bootstrap/schedules/waivework-auto-charge.js
@@ -1,3 +1,4 @@
+let redis = require('../../lib/redis-service');
 let scheduler = Bento.provider('queue').scheduler;
 let OrderService = Bento.module('shop/lib/order-service');
 let BookingPayment = Bento.model('BookingPayment');
@@ -9,7 +10,8 @@ scheduler.process('waivework-auto-charge', function*(job) {
   let today = moment();
   //if ([1, 8, 15, 22].includes(today.date())) {
   // The next line needs to be removed later
-  today = today.add(5, 'days');
+  today = today.add(1, 'days');
+  console.log('today: ', today);
   let todaysPayments = yield WaiveworkPayment.find({
     where: {
       date: {$lt: today.add(1, 'days').format('YYYY-MM-DD')},
@@ -23,37 +25,43 @@ scheduler.process('waivework-auto-charge', function*(job) {
     ],
   });
   for (let oldPayment of todaysPayments) {
-    let data = {
-      userId: oldPayment.booking.userId,
-      amount: oldPayment.amount,
-    };
-    let shopOrder = (yield OrderService.quickCharge(data)).order;
-    let bookingPayment = new BookingPayment({
-      bookingId: oldPayment.booking.id,
-      orderId: shopOrder.id,
-    });
-    yield bookingPayment.save();
-    console.log('bookingPayment: ', bookingPayment);
-    let newPayment = new WaiveworkPayment({
-      bookingId: oldPayment.booking.id,
-      date: moment().add(7, 'days'),
-      bookingPaymentId: null,
-      amount: shopOrder.amount,
-    });
-    yield newPayment.save();
-    console.log('new payment: ', newPayment);
-    yield oldPayment.update({
-      bookingPaymentId: bookingPayment.id,
-    });
-    console.log('old payment should be updated: ', oldPayment);
+    if (
+      redis.shouldProcess('waivework-auto-charge', oldPayment.id, 90 * 1000)
+    ) {
+      let data = {
+        userId: oldPayment.booking.userId,
+        amount: oldPayment.amount,
+      };
+      let shopOrder = (yield OrderService.quickCharge(data)).order;
+      let bookingPayment = new BookingPayment({
+        bookingId: oldPayment.booking.id,
+        orderId: shopOrder.id,
+      });
+      yield bookingPayment.save();
+      console.log('bookingPayment: ', bookingPayment);
+      let newPayment = new WaiveworkPayment({
+        bookingId: oldPayment.booking.id,
+        date: moment().add(7, 'days'),
+        bookingPaymentId: null,
+        amount: shopOrder.amount,
+      });
+      yield newPayment.save();
+      console.log('new payment: ', newPayment);
+      yield oldPayment.update({
+        bookingPaymentId: bookingPayment.id,
+      });
+      console.log('old payment should be updated: ', oldPayment);
+    }
   }
   //}
 });
 
 module.exports = function*() {
+  let testTimer = {value: 2, type: 'minutes'};
+  let timer = {value: 24, type: 'hours'};
   scheduler.add('waivework-auto-charge', {
     init: true,
     repeat: true,
-    timer: {value: 24, type: 'seconds'},
+    timer: testTimer,
   });
 };
