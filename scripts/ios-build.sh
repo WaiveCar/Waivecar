a92bf8b6b7dbdc89e475a279c1ec9df7b9e5bbee
David Leigh <daleighan@gmail.com>
2019-01-24 15:18:27 -0800

    The ios build script should now work when xcode 10 is installed

--- scripts/ios-build.sh
+++ scripts/ios-build.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/common.sh
 
 #cordova prepare ios
-ionic build ios --verbose --release
+ionic build ios --verbose --release --buildFlag="-UseModernBuildSystem=0"
 
 for word in microphone contact reminders calendar; do
   grep -viE "${word}s?(Usage| access)" $DIR/../platforms/ios/WaiveCar/WaiveCar-Info.plist > /tmp/removed
