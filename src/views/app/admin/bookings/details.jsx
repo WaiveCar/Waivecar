01cf932d0a4155f2703cc06eea0525c2776c4baa
Ken Ryan <chen97me@gmail.com>
2019-03-15 19:20:19 -0700

    Added a display onto the booking page of a car's timeline.

--- src/views/app/admin/bookings/details.jsx
+++ src/views/app/admin/bookings/details.jsx
@@ -1,13 +1,133 @@
 import React   from 'react';
 import moment  from 'moment';
 import { GMap } from 'bento-web';
+import { api } from 'bento'
 var _ = require('lodash');
 
 module.exports = class RideDetails extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      site : null,
+      startTime : null,
+      finishTime : null,
+      lastLat : null,
+      lastLong : null,
+      lastTime : null,
+      address : [],
+      switchStat : [],
+      time : [],
+      }
+  }
+
+  getAddress(lat, long, data, i, lastObject) {
+    let url = `http://basic.waivecar.com/location.php`;
+    let qs  = `latitude=${ lat }&longitude=${ long }`;
+    debugger;
+    api.external(url,qs, (err,addressSite) => {
+      data.address[i] = addressSite;
+      if (lastObject) {
+        console.log(lastObject);
+        console.log(data.address);
+      }
+      this.setState();
+    });
+  }
+
+  getTTime(time) {
+    return [
+      Math.floor(time/3.6e6), //hours
+      Math.floor(time/6e4)%60, //minutes
+      Math.floor(time/1000)%60 //seconds
+    ];
+  }
+  componentDidMount() {
+    var ix = 0;
+    var ival = setInterval(function(){
+      var tempDriving = 0, tempStill = 0, lastLat = 0, lastLong = 0, lastTime = 0;
+      if (this.props.carPath.length != 0) {
+        clearInterval(ival);
+        var data = {time:[], switchStat: [], address: []};
+        this.setState({
+          startTime : this.props.carPath[0][2],
+          finishTime : this.props.carPath[this.props.carPath.length - 1][2]
+        });
+        var j = 0;
+        for (var i = 0; i < this.props.carPath.length; i++) {
+          /*console.log(i);
+          console.log(JSON.stringify(this.props.carPath[i])); */
+          //If the car just started
+          if(this.props.carPath[i][2] == this.state.startTime) {
+            this.getAddress(this.props.carPath[i][0],this.props.carPath[i][1],data,j);
+            j++
+            data.switchStat.push('started');
+          }
+          //If the car just finished
+          else if (this.props.carPath[i][2] == this.state.finishTime) {
+            //If the car has been driving and finished being used
+            if (tempDriving != 0) {
+              tempDriving = tempDriving + (new Date(this.props.carPath[i][2]).getTime() - new Date(lastTime).getTime());
+              data.time.push(this.getTTime(tempDriving)); //hours
+              this.getAddress(this.props.carPath[i][0],this.props.carPath[i][1],data,j, true);
+              j++
+              tempDriving = 0;
+              data.switchStat.push('endWDrive');
+              //If the car has been stationary and finished being used
+            } else {
+              tempStill = tempStill + (new Date(this.props.carPath[i][2]).getTime() - new Date(lastTime).getTime());
+              data.time.push(this.getTTime(tempStill));
+              this.getAddress(this.props.carPath[i][0],this.props.carPath[i][1],data,j, true);
+              j++
+              tempStill = 0;
+              data.switchStat.push('endWStop');
+            }
+          }
+          //If car is not moving
+          else if (Math.abs(this.props.carPath[i][0] - lastLat) < .0002 && Math.abs(this.props.carPath[i][1] - lastLong) < .0002 ) {
+            if(tempDriving != 0) {
+              data.switchStat.push('notMoving');
+              data.time.push(this.getTTime(tempDriving)); //hours
+              data.time.push(this.props.carPath[i-1][2]);
+              this.getAddress(this.props.carPath[i][0],this.props.carPath[i][1],data,j);
+              j++
+              tempDriving = 0;
+            } else {
+              data.switchStat.push('unnecessaryStop');
+            }
+            tempStill = tempStill + (new Date(this.props.carPath[i][2]).getTime() - new Date(lastTime).getTime());
+          }
+          //If car is moving
+          else {
+            if (tempStill != 0) {
+              data.switchStat.push('startMove');
+              data.time.push(this.getTTime(tempStill)); //hours
+              tempStill = 0;
+            } else {data.switchStat.push('unnecessaryMove');}
+            tempDriving = tempDriving + (new Date(this.props.carPath[i][2]).getTime() - new Date(lastTime).getTime());
+          }
+          lastLat = this.props.carPath[i][0];
+          lastLong = this.props.carPath[i][1];
+          lastTime = this.props.carPath[i][2];
+          /*console.log(JSON.stringify(data));
+          console.log(JSON.stringify(data.switchStat)); */
+        }
+        /*
+        var length = data.time.length;
+        for(var i = 0; i < length ;i++) {
+          data.time.push(data.time[i]);
+        }
+        length = data.switchStat.length;
+        for(var i = 0; i < length ;i++) {
+          data.switchStat.push(data.switchStat[i]);
+        }*/
+        this.setState(data);
+      }
+    }.bind(this), 1000);
+  }
 
   render() {
-    var ride = null, data = null, duration = null;
+    var ride = null, data = null, duration = null, carTimeline = null, indexT = 0, indexA = 0;
     try {
       data = this.props.booking;
 
@@ -48,6 +168,13 @@ module.exports = class RideDetails extends React.Component {
         minutes : duration.minutes(),
         seconds : duration.seconds()
       };
+
+      // ###Car Timeline
+      carTimeline = {
+        switchStat : this.state.switchStat,
+        address : this.state.address,
+        time: this.state.time
+      }
     } catch(ex) {
       return (
         <div className="ride-details">
@@ -129,6 +256,67 @@ module.exports = class RideDetails extends React.Component {
                 </div>
               </div>
             </div>
+            {
+              this.props.carPath && this.props.carPath[0] &&
+              <div className="box-content">
+                <strong>Car Timeline </strong><br/>
+                {//here for debugging purposes
+                  /*this.state.switchStat.map((stat,i) => {
+                  return (<div key = {i}>
+                    {i}
+                    {stat}
+                  <br/>
+                  </div>)
+                })*/}
+                {this.props.carPath.map((path, i) => {
+                  switch(this.state.switchStat[i]) {
+                    case "started":
+                    {indexA++};
+                    return (<div key = {i}>
+                      Address: {carTimeline.address[indexA-1]}, Time: {moment(path[2]).format('h:mm:ss a')}
+                    </div>);
+                    break;
+                    case "endWDrive":
+                    {indexA++; indexT++}
+                    return (<div key = {i}>
+                      Car has been <span style={{color: 'green'}}>driving </span>
+                      for {carTimeline.time[indexT-1][0]}:{carTimeline.time[indexT-1][1]}:
+                      {carTimeline.time[indexT-1][2]}
+                      <br/> Address: {carTimeline.address[indexA-1]}, Time: {moment(path[2]).format('h:mm:ss a')}
+                    </div>);
+                    break;
+                    case "endWStop":
+                    {indexA++; indexT++;}
+                    return (<div key = {i}>
+                      Car has been <span style = {{color: 'red'}}>still </span>
+                      for {carTimeline.time[indexT-1][0]}:{carTimeline.time[indexT-1][1]}:
+                      {carTimeline.time[indexT-1][2]}
+                      <br/> Address: {carTimeline.address[indexA-1]}, Time: {moment(path[2]).format('h:mm:ss a')}
+                    </div>);
+                    break;
+                    case "notMoving":
+                    {indexA++; indexT++;indexT++}
+                    return (<div key = {i}>
+                      Car has been <span style={{color: 'green'}}>driving </span>
+                      for {carTimeline.time[indexT-2][0]}:{carTimeline.time[indexT-2][1]}:
+                      {carTimeline.time[indexT-2][2]}
+                      <br/> Address: {carTimeline.address[indexA-1]}, Time: {moment(carTimeline.time[indexT-1]).format('h:mm:ss a')}
+                    </div>);
+                    break;
+                    case "startMove":
+                    {indexT++}
+                    return (<div key = {i}>
+                      Car has been <span style = {{color: 'red'}}>still </span>
+                      for {carTimeline.time[indexT-1][0]}:{carTimeline.time[indexT-1][1]}:
+                      {carTimeline.time[indexT-1][2]}
+                    </div>);
+                    break;
+                    default:
+                    break;
+                  }
+                })}
+              </div>
+            }
           </div>
         </div>
       </div>
