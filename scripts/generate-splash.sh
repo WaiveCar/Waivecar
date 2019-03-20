a20e7771cc77786f1309986b3b3fa0558d6f734e
Chris McKenzie <kristopolous@yahoo.com>
2019-03-20 15:57:39 -0700

    logo fixes

--- scripts/generate-splash.sh
+++ scripts/generate-splash.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+cd resources
 for i in ios/splash/* android/splash/*; do
   width=`identify -format %w $i`
   size=`identify -format %wx%h $i`
   dpi=$(( width / 3 ))
   fname=`basename $i`
   convert \
-    -background none -density $dpi circle-logo.svg \
+    -background none -density $dpi lightning.svg \
     -background black -gravity center -extent $size -colors 18 /tmp/$fname
   pngcrush -l 9 /tmp/$fname $i
 done
