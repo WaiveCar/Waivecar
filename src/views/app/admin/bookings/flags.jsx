2675b9f3f10ea7e6a70b4d788fa629cb2064c0b5
Ken Ryan <chen97me@gmail.com>
2019-04-12 00:21:48 -0700

    Removed unnecessary code and comments

--- src/views/app/admin/bookings/flags.jsx
+++ src/views/app/admin/bookings/flags.jsx
@@ -13,15 +13,9 @@ module.exports = class BookingFlags extends React.Component {
 
 	flagSet(flag, state) {
 		//if state if true then the flag is not active
-		console.log("clicked flag: " + flag);
-		if (state)
-			console.log("Flag is going to be set to active")
-		else console.log("Flag is going to be set to not active")
 		this.setState({'processing': true})
 		api.put(`/bookings/${this.props.booking.id}/flags`, {[flag]: state}, (err, res) => {
-			console.log(res.flags)
-			console.log("This state")
-			console.log(this.state)
+			//"processing" is regarding the "delay" of the api call
 			this.setState({'processing': false})
 
 			var showingFlags = this.state.showingFlags
@@ -33,29 +27,10 @@ module.exports = class BookingFlags extends React.Component {
 				relevantFlags.push(flag);
 				showingFlags = showingFlags.filter(row => row != flag);
 			}
-
-			console.log(showingFlags)
-		//	console.log(missingFlags)
-	//		debugger;
 			this.setState({
 				showingFlags : showingFlags,
 				missingFlags : relevantFlags
 			})
-			//debugger;
-			/*debugger;
-			if(!state) {
-				var pos = res.flags.indexOf(flag);
-				var len = res.flags.length;
-				var str = res.flags.slice(0,pos-1);
-				str += res.flags.slice((pos+flag.length)+2,len);
-				res.flags = str; + this.state.relevantFlags
-			} else {
-				console.log(res.flags)
-				res.flags = res.flags.replace("]" , ",\""+flag+"\"]");
-			}
-			console.log(res.flags)
-			console.log(res); */
-
 		});
 	}
 
@@ -63,15 +38,12 @@ module.exports = class BookingFlags extends React.Component {
 		var notToShow = ["drove", "tikdStart", "tikdEnd", "try-end", "1hr45-warning", "first-sync",]
 		var relevantFlags= ["rush"];
 		var showingFlags = [];
-		console.log("keys of bookingFlags: "  + Object.keys(this.props.booking.flags));
-		console.log(Object.keys(this.props.booking.flags));
 		//Sorting through the flags of the booking prop
 		Object.keys(this.props.booking.flags).map((flag, i) => {
 			var relevantFlagString = false;
 			relevantFlags.map((relevantFlag, i) => {
 				if(relevantFlag == flag) {
 					relevantFlagString = true;
-					console.log(typeof relevantFlags)
 					var index = relevantFlags.indexOf(flag);
 					relevantFlags.splice(index,1)
 				}
@@ -88,17 +60,8 @@ module.exports = class BookingFlags extends React.Component {
 			}
 
 		})
-		//debugger;
 		this.setState({showingFlags : showingFlags})
 		this.setState({missingFlags : relevantFlags})
-
-		console.log("showingFlags: " + showingFlags)
-		console.log("showingFlags state")
-		console.log(this.state.showingFlags)
-		console.log("relevantFlags: " + relevantFlags)
-		console.log("relevantFlags state")
-		console.log(this.state.relevantFlags)
-
 	}
 
 	render () {
