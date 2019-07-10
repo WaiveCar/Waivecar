abb38259b09464d66d0d7af9dc98cfb5c31129cd
David Alexander Leigh <daleighan@gmail.com>
2019-07-09 18:31:03 -0700

    Adding a reference to a previous payment to the shop_order model

--- modules/shop/models/order.js
+++ modules/shop/models/order.js
@@ -143,6 +143,15 @@ Bento.Register.Model('Shop/Order', 'sequelize', (model, Sequelize) => {
       }
     },
 
+    previousId : {
+      type       : Sequelize.INTEGER,
+      allowNull  : true,
+      references : {
+        model : 'shop_orders',
+        key   : 'id'
+      }
+    },
+
     createdAt : {
       type: Sequelize.DATE,
     },
