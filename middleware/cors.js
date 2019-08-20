cac6419628036ddca0aa06344a982a42b8b0fab3
David Alexander Leigh <daleighan@gmail.com>
2019-08-20 14:09:14 -0700

    Making cors actually work

--- middleware/cors.js
+++ middleware/cors.js
@@ -5,16 +5,5 @@ let cors = require('koa-cors');
 /* istanbul ignore next: its cors man! */
 
 module.exports = (app) => {
-  app.use(cors({
-    credentials : true,
-    headers     : Bento.config.api.cors.headers,
-    origin      : (req) => {
-      let origin  = req.headers.origin;
-      let origins = Bento.config.api.cors.origins;
-      if (!origins || origins === '*' || origins.indexOf(origin) !== -1) {
-        return origin;
-      }
-      return null;
-    }
-  }));
+  app.use(cors());
 };
