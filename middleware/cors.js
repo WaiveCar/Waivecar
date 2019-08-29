72498b0805eddb3fb6c4019d99538bad563bd5a3
David Alexander Leigh <daleighan@gmail.com>
2019-08-28 19:03:16 -0700

    The headers option in cors middleware should now be right

--- middleware/cors.js
+++ middleware/cors.js
@@ -5,7 +5,9 @@ let cors = require('koa-cors');
 module.exports = app => {
   app.use(
     cors({
-      origin: (req) => {
+      allowHeaders: Bento.config.api.cors.headers,
+      credentials : true,
+      origin: req => {
         let origin = req.headers.origin;
         let origins = Bento.config.api.cors.origins;
         if (!origins || origins === '*' || origins.indexOf(origin) !== -1) {
