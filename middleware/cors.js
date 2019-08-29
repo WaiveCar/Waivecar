c5a83f0ac3b24e8a20929eb6a4f8fdfab247550f
David Alexander Leigh <daleighan@gmail.com>
2019-08-28 18:54:26 -0700

    Hopefully making cors work with new site

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,8 +2,17 @@
 
 let cors = require('koa-cors');
 
-/* istanbul ignore next: its cors man! */
-
-module.exports = (app) => {
-  app.use(cors());
+module.exports = app => {
+  app.use(
+    cors({
+      origin: (req) => {
+        let origin = req.headers.origin;
+        let origins = Bento.config.api.cors.origins;
+        if (!origins || origins === '*' || origins.indexOf(origin) !== -1) {
+          return origin;
+        }
+        return null;
+      },
+    }),
+  );
 };
