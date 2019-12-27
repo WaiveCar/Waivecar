7b9a6973cd4a8f2a401bd473551e0427b0fda28c
David Alexander Leigh <daleighan@gmail.com>
2019-12-26 16:16:17 -0800

    Logging cors origins

--- middleware/cors.js
+++ middleware/cors.js
@@ -10,6 +10,8 @@ module.exports = app => {
       origin: req => {
         let origin = req.headers.origin;
         let origins = Bento.config.api.cors.origins;
+        console.log('origin', origin);
+        console.log('origins', origins);
         if (!origins || origins === '*' || origins.indexOf(origin) !== -1) {
           return origin;
         }
