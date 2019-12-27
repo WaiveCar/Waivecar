191343db56fac91b63927bfa484ad0adeab0fe00
David Alexander Leigh <daleighan@gmail.com>
2019-12-26 16:21:00 -0800

    Removing cors logging

--- middleware/cors.js
+++ middleware/cors.js
@@ -10,8 +10,6 @@ module.exports = app => {
       origin: req => {
         let origin = req.headers.origin;
         let origins = Bento.config.api.cors.origins;
-        console.log('origin', origin);
-        console.log('origins', origins);
         if (!origins || origins === '*' || origins.indexOf(origin) !== -1) {
           return origin;
         }
