d8ad1e2df368044e89a9149a4952923e52fbc557
David Alexander Leigh <daleighan@gmail.com>
2020-01-21 14:30:53 -0800

    Added a model for insurance quotes

---
+++ modules/waivecar/models/insurance-quotes.js
@@ -0,0 +1,53 @@
+Bento.Register.Model('InsuranceQuotes', 'sequelize', function(
+  model,
+  Sequelize,
+) {
+  model.table = 'insurance_quotes';
+
+  model.schema = {
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+
+    waitlistId: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'waitlist',
+        key: 'id',
+      },
+    },
+
+    amount: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      default: null,
+    },
+
+    accepted: {
+      type: Sequelize.BOOLEAN,
+      default: false,
+    },
+
+    expiresAt: {
+      type: Sequelize.DATE,
+      default: null,
+    },
+  };
+
+  model.relations = [
+    'User',
+    'Waitlist',
+    function relations(User, Waitlist) {
+      this.belongsTo(User, {as: 'user', foreignKey: 'userId'});
+      this.belongsTo(Waitlist, {as: 'waitlist', foreignKey: 'waitlistId'});
+    },
+  ];
+
+  return model;
+});
