f17e5718ca2442f66ee62b10443c29de60983aee
David Alexander Leigh <daleighan@gmail.com>
2020-01-23 17:57:27 -0800

    Initial work done on insurance quotes page

--- src/views/app/admin/waivework/insurance-quotes.jsx
+++ src/views/app/admin/waivework/insurance-quotes.jsx
@@ -1,8 +1,35 @@
 import React, {Component} from 'react';
+import {api} from 'bento';
+import {Link} from 'react-router';
+import Table from 'bento-service/table';
+import ThSort from '../components/table-th';
 
 class InsuranceQuotes extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      search : null,
+      sort : {
+        key   : null,
+        order : 'DESC'
+      },
+      more   : false,
+      offset : 0,
+    };
+    this.table = new Table(this, 'insuranceQuotes', null, '/insuranceQuotes');
+  }
+
+  componentDidMount() {
+    this.table.init();
+    this.setState({
+      sort : {
+        key   : 'createdAt',
+        order : 'DESC'
+      },
+      searchObj: {
+        order: 'id,DESC',
+      }
+    });
   }
 
   render() {
