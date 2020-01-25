2eeea65e81b9d89dd1764a5ae6ec33f94fe151ac
David Alexander Leigh <daleighan@gmail.com>
2020-01-24 16:42:36 -0800

    Changing ordering for insurance quotes index

--- modules/waivecar/lib/waitlist-service.js
+++ modules/waivecar/lib/waitlist-service.js
@@ -370,19 +370,8 @@ module.exports = {
 
   *insuranceQuotes(queryIn) {
     let query = {};
-    /*
-    if (queryIn.search) {
-      query.where = { $and: [
-        {user_id: null },
-        sequelize.literal(`concat_ws(' ', first_name, last_name, place_name, notes) like '%${queryIn.search}%'`),
-      ]};
-      query.order = [ ['created_at', 'asc'] ];
-    } else {
-      query.where = { user_id: null };
-    }
-    */
     query.order = [ 
-      /*[ 'priority', 'asc' ],*/ ['expiresAt', 'desc']
+      ['expiresAt', 'desc']
     ];
     query.include = [{
       model: 'User',
