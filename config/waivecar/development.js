b9dfa1997f235e2c216beada9cde48d5260492b9
David Alexander Leigh <daleighan@gmail.com>
2020-02-28 15:32:10 -0800

    Adding the telem url to config

--- config/waivecar/development.js
+++ config/waivecar/development.js
@@ -1,17 +1,20 @@
 module.exports = {
-  waivecar : {
-    car : {
-      staleLimit : 30,
-      sync       : {
-        value : 10,
-        type  : 'minutes'
-      }
+  waivecar: {
+    car: {
+      staleLimit: 30,
+      sync: {
+        value: 10,
+        type: 'minutes',
+      },
     },
-    mock : {
-      cars     : false,
-      homebase : true,
-      stations : true,
-      valets   : false
-    }
-  }
+    mock: {
+      cars: false,
+      homebase: true,
+      stations: true,
+      valets: false,
+    },
+    telem: {
+      uri: 'http://10.0.2.2:2080',
+    },
+  },
 };
