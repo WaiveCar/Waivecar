17423a05f46943bbf33e984d8650480a9c300d66
David Alexander Leigh <daleighan@gmail.com>
2020-03-09 14:46:45 -0700

    Added a user communications model

---
+++ modules/waivecar/models/user-communications.js
@@ -0,0 +1,47 @@
+Bento.Register.Model('UserCommunications', 'sequelize', function register(
+  model,
+  Sequelize,
+) {
+  model.table = 'user_communications';
+
+  model.schema = {
+    id: {
+      type: Sequelize.STRING(28),
+      primaryKey: true,
+    },
+
+    userId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+
+    creatorId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+
+    status: {
+      type: Sequelize.ENUM('sms', 'email'),
+      defaultValue: 'sms',
+    },
+    content: {
+      type: Sequelize.TEXT,
+    },
+  };
+
+  model.relations = [
+    'User',
+    function(User) {
+      this.belongsTo(User, {as: 'user', foreignKey: 'userId'});
+      this.belongsTo(User, {as: 'creator', foreignKey: 'creatorId'});
+    },
+  ];
+
+  return model;
+});
