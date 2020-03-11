e914c6dd674140ac28df56aa888e80d45b85b9b9
David Alexander Leigh <daleighan@gmail.com>
2020-03-11 16:53:27 -0700

    Adding cateogry selection

--- src/views/app/admin/users/user-communications.jsx
+++ src/views/app/admin/users/user-communications.jsx
@@ -8,26 +8,56 @@ export default class UserCommunications extends Component {
     this.state = {
       sms: [],
       email: [],
+      category: 'sms',
     };
   }
 
   componentDidMount() {
     api.get(`/users/${this.props.user.id}/communications`, (err, res) => {
       if (err) {
-        console.log('err', err);
         return;
       }
-      this.setState(
-        {
-          sms: res.filter(each => each.type === 'sms'),
-          email: res.filter(each => each.type === 'email'),
-        },
-        () => console.log(this.state),
-      );
+      this.setState({
+        sms: res.filter(each => each.type === 'sms'),
+        email: res.filter(each => each.type === 'email'),
+      });
     });
   }
 
   render() {
-    return <div />;
+    let {category} = this.state;
+    return (
+      <div className="box">
+        <h3>
+          User Communications
+          <small>View previous emails and texts</small>
+        </h3>
+        <div className="box-content">
+          <div className="row">
+            <button
+              type="button"
+              className={`btn btn-${
+                category === 'sms' ? 'primary' : 'secondary'
+              }`}
+              onClick={() =>
+                this.setState({category: 'sms'}, () => console.log(this.state))
+              }>
+              SMS
+            </button>
+            <button
+              type="button"
+              className={`btn btn-${
+                category === 'email' ? 'primary' : 'secondary'
+              }`}
+              onClick={() => this.setState({category: 'email'})}>
+              Email
+            </button>
+          </div>
+          <div className="row">
+            {category === 'sms' ? <div>sms</div> : <div>email</div>}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
