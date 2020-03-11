36b5a2779627678bc38137e72e52253168ff7e2a
David Alexander Leigh <daleighan@gmail.com>
2020-03-11 16:41:02 -0700

    Requesting user communications from the api

--- src/views/app/admin/users/user-communications.jsx
+++ src/views/app/admin/users/user-communications.jsx
@@ -1,8 +1,30 @@
 import React, {Component} from 'react';
+import {api} from 'bento';
+import {snackbar} from 'bento-web';
 
 export default class UserCommunications extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      sms: [],
+      email: [],
+    };
+  }
+
+  componentDidMount() {
+    api.get(`/users/${this.props.user.id}/communications`, (err, res) => {
+      if (err) {
+        console.log('err', err);
+        return;
+      }
+      this.setState(
+        {
+          sms: res.filter(each => each.type === 'sms'),
+          email: res.filter(each => each.type === 'email'),
+        },
+        () => console.log(this.state),
+      );
+    });
   }
 
   render() {
