18a6fa0cb1150234f443bf08ce9ca57566708caf
David Alexander Leigh <daleighan@gmail.com>
2020-03-16 13:30:28 -0700

    Adding back accidentally removed code

--- src/views/app/admin/users/waivework-request.jsx
+++ src/views/app/admin/users/waivework-request.jsx
@@ -112,6 +112,15 @@ export default class WaiveWorkRequest extends Component {
             />{' '}
             No
           </div>
+          <div>
+            <button
+              type="button"
+              className="btn btn-primary"
+              disabled={disabled}
+              onClick={() => this.requestQuote()}>
+              Request Quote
+            </button>
+          </div>
         </div>
       </div>
     );
