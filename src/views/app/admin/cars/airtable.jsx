9ea7baa684984b6eb117653b52dabab6fa120749
David Alexander Leigh <daleighan@gmail.com>
2020-03-26 14:18:55 -0700

    Getting all data from airtable to display on the page

--- src/views/app/admin/cars/airtable.jsx
+++ src/views/app/admin/cars/airtable.jsx
@@ -3,9 +3,34 @@ import React, {Component} from 'react';
 export default class Airtable extends Component {
   constructor(props) {
     super(props);
+    this.state = {airtableData: null};
+  }
+
+  componentDidMount() {
+    let {car} = this.props;
+    this.setState({airtableData: JSON.parse(car.airtableData)});
   }
 
   render() {
-    return <div>Airtable</div>;
+    let {airtableData} = this.state;
+    console.log(airtableData);
+    return (
+      <div className="box">
+        <h3>Data From Airtable </h3>
+        <div className="box-content">
+          {airtableData ? (
+            <div>
+              {Object.keys(airtableData).map(key => (
+                <div className="row">
+                  {key}: {airtableData[key]}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>No Airtable Data Found</div>
+          )}
+        </div>
+      </div>
+    );
   }
 }
