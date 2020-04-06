e2e8b7c876ab3f353c2828aedc413b1cb5d24658
David Alexander Leigh <daleighan@gmail.com>
2020-04-06 16:38:57 -0700

    Added methods to organization model

--- modules/waivecar/models/organization.js
+++ modules/waivecar/models/organization.js
@@ -1,3 +1,7 @@
+let Car = Bento.model('Car');
+let User = Bento.model('User');
+let OrganizationUser = Bento.model('OrganizationUser');
+
 Bento.Register.Model('Organization', 'sequelize', function register(
   model,
   Sequelize,
@@ -12,6 +16,32 @@ Bento.Register.Model('Organization', 'sequelize', function register(
     name: {
       type: Sequelize.TEXT(),
       allowNull: false,
+    },
+  };
+
+  model.methods = {
+    addCar: function*(carId) {
+      let car = yield Car.findById(carId);
+      yield car.update({organizationId: this.id});
+    },
+    removeCar: function*(carId) {
+      let car = yield Car.findById(carId);
+      yield car.update({organizationId: null});
+    },
+    addUser: function*(userId) {
+      let orgUser = new OrganizationUser({
+        organizationId: this.id,
+        userId,
+      });
+      yield orgUser.save();
+    },
+    removeUser: function*(userId) {
+      let orgUser = yield OrganizationUser.findOne({
+        where: {
+          organizationId: this.id,
+          userId,
+        },
+      });
     },
   };
 
