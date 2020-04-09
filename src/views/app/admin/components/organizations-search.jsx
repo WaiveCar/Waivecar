08ec893eaca9fca818810d6dec272b958fcb2f0e
David Alexander Leigh <daleighan@gmail.com>
2020-04-09 14:16:34 -0700

    Added the ability to add and remove from orgs

--- src/views/app/admin/components/organizations-search.jsx
+++ src/views/app/admin/components/organizations-search.jsx
@@ -3,14 +3,23 @@ import {Link} from 'react-router';
 import {api} from 'bento';
 import {snackbar} from 'bento-web';
 
+const capitalize = word => word[0].toUpperCase() + word.slice(1);
+
 class Organizations extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentOrganizations: [],
       searchResults: [],
       orgSearchWord: '',
     };
   }
+  componentDidMount() {
+    let {type} = this.props;
+    let assignee = this.props[type];
+    this.setState({currentOrganizations: assignee.organizations});
+  }
+
   orgSearch() {
     let {orgSearchWord} = this.state;
     api.get(`/organizations/?name=${orgSearchWord}`, (err, res) => {
@@ -24,20 +33,38 @@ class Organizations extends Component {
     });
   }
 
-  addToOrg() {}
-
-  removeFromOrg() {}
-
-  render() {
-    let {searchResults, orgSearchWord} = this.state;
+  orgAction(action, orgId) {
     let {type} = this.props;
     let assignee = this.props[type];
+    console.log(orgId);
+    api.put(
+      `/organizations/${orgId}/${action}${capitalize(type)}`,
+      {
+        [`${type}Id`]: assignee.id,
+      },
+      (err, res) => {
+        if (err) {
+          return snackbar.notify({
+            type: 'danger',
+            message: err.message,
+          });
+        }
+        this.setState({currentOrganizations: res});
+      },
+    );
+  }
+
+  removeFromOrg(orgId) {}
+
+  render() {
+    let {currentOrganizations, searchResults, orgSearchWord} = this.state;
+    console.log(currentOrganizations);
     return (
       <div className="box">
         <h3>Organizations</h3>
         <div className="box-content">
           <h4>Current Organizations</h4>
-          {assignee.organizations.map((each, i) => (
+          {currentOrganizations.map((each, i) => (
             <div className="row" key={i}>
               <div style={{padding: '10px 0'}} className="col-xs-6">
                 <Link to={`/organizations/${each.organization.id}`}>
@@ -46,7 +73,7 @@ class Organizations extends Component {
               </div>
               <button
                 className="btn btn-link col-xs-6"
-                onClick={() => this.removeFromOrg(item.id)}>
+                onClick={() => this.orgAction('remove', each.organization.id)}>
                 Remove Item
               </button>
             </div>
@@ -75,7 +102,7 @@ class Organizations extends Component {
               </div>
               <button
                 className="btn btn-link col-xs-6"
-                onClick={() => this.addToOrg(item.id)}>
+                onClick={() => this.orgAction('add', item.id)}>
                 Add Now
               </button>
             </div>
