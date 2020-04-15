95c555e8bcc8a995df7eea5451044722f227ac7b
David Alexander Leigh <daleighan@gmail.com>
2020-04-15 13:23:33 -0700

    Added content to the forbidden route

--- src/views/app/user/forbidden/index.jsx
+++ src/views/app/user/forbidden/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Forbidden() {
-  return <div>Forbidden</div>
+  return (
+    <div>
+      <div className="box">
+        <h3>
+          Forbidden
+          <small>This page has restricted access</small>
+        </h3>
+        <div className="box-content" style={{textAlign: 'center'}}>
+          Your account does not have access to this page. If you believe this
+          restriction to be in error, do not hesitate to contact us at{' '}
+          <a href="mailto:support@waive.com">support@waive.com</a>.
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Forbidden;
