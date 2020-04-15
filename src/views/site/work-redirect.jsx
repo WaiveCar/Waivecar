2a0752621be69f62d8da58674d22c38a555b22e6
David Alexander Leigh <daleighan@gmail.com>
2020-04-15 13:09:27 -0700

    Added content to the waivework.com redirect page

--- src/views/site/work-redirect.jsx
+++ src/views/site/work-redirect.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function WorkRedirect() {
-  return <div>Work Redirect</div>;
+  return (
+    <div className="login">
+      <div className="title">
+        New Site For&nbsp;
+        <span className="title-site">WaiveWork</span>
+        <div className="message-success">
+          We have built an all-new site for our WaiveWork users. It is found
+          at <a href="https://waivework.com/login">waivework.com</a>. If you believe this 
+          redirection to be in error, please contact customer service at {' '} 
+          <a href="mailto:support@waive.com">support@waive.com</a>.
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WorkRedirect;
