998245782cf6a19f07240ad3de18dc78e6fc6d67
David Alexander Leigh <daleighan@gmail.com>
2020-04-16 14:44:35 -0700

    Initial work towards add user function

--- src/views/app/admin/users/add-user.jsx
+++ src/views/app/admin/users/add-user.jsx
@@ -3,7 +3,13 @@ import {api, auth} from 'bento';
 import {Form, snackbar} from 'bento-web';
 import {Link} from 'react-router';
 
-let buttons = [];
+let buttons = [
+  {
+    value: 'Add User',
+    type: 'submit',
+    class: 'btn btn-primary btn-profile-submit',
+  },
+];
 
 class AddUser extends Component {
   constructor(props) {
@@ -16,8 +22,11 @@ class AddUser extends Component {
     };
   }
 
-  submitUser() {
-    console.log('submit');
+  submitUser(e) {
+    let form = this.refs.addUser;
+    console.log(form);
+    let data = form.state.data;
+    console.log(data);
   }
 
   addOrg(org) {
@@ -69,73 +78,74 @@ class AddUser extends Component {
     let {orgSearchWord, searchResults, currentOrganizations} = this.state;
     return (
       <div className="box">
-        <h3>Add a User</h3>
+        <h3 style={{marginBottom: '1rem'}}>Add a User</h3>
+        {this.currentUser.organizations.length ? (
+          <div>
+            <h4>Organizations</h4>
+            {this.currentUser.organizations.map((org, i) => (
+              <div key={i}>
+                <input
+                  onInput={() => this.toggleOrg(org)}
+                  type={'checkbox'}
+                  name={`org-${i}`}
+                  id={`org-${i}`}
+                  style={{verticalAlign: 'middle', marginRight: '5px'}}
+                />
+                <label htmlFor={`org-${i}`}>{org.organization.name}</label>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>
+            <h4>Search for Organizations</h4>
+            <div className="row" style={{marginTop: '10px'}}>
+              <input
+                onChange={e => this.setState({orgSearchWord: e.target.value})}
+                value={orgSearchWord}
+                style={{marginTop: '1px', padding: '2px', height: '40px'}}
+                className="col-xs-6"
+                placeholder="Organizations Name"
+              />
+              <button
+                className="btn btn-primary btn-sm col-xs-6"
+                onClick={() => this.orgSearch()}>
+                Find Organization
+              </button>
+            </div>
+            {searchResults.map((item, i) => (
+              <div key={i} className="row">
+                <div style={{padding: '10px 0'}} className="col-xs-6">
+                  <Link to={`/organizations/${item.id}`} target="_blank">
+                    {item.name}
+                  </Link>
+                </div>
+                <button
+                  className="btn btn-link col-xs-6"
+                  onClick={() => this.addOrg(item)}>
+                  Add
+                </button>
+              </div>
+            ))}
+            <h4 style={{marginTop: '10px'}}>Selected</h4>
+            <ul>
+              {currentOrganizations.map((each, i) => (
+                <li>
+                  <Link to={`/organizations/${each.id}`} target="_blank">
+                    {each.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
         <div className="box-content">
           <Form
-            ref="personal"
+            ref="addUser"
             className="bento-form-static"
             fields={require('./user-form')}
             buttons={buttons}
-            submit={() => this.submitUser()}
+            submit={e => this.submitUser(e)}
           />
-          {this.currentUser.organizations.length ? (
-            <div>
-              {this.currentUser.organizations.map((org, i) => (
-                <div key={i}>
-                  <input
-                    onInput={() => this.toggleOrg(org)}
-                    type={'checkbox'}
-                    name={`org-${i}`}
-                    id={`org-${i}`}
-                    style={{verticalAlign: 'middle', marginRight: '5px'}}
-                  />
-                  <label htmlFor={`org-${i}`}>{org.organization.name}</label>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div>
-              <h4>Search for Organizations</h4>
-              <div className="row" style={{marginTop: '10px'}}>
-                <input
-                  onChange={e => this.setState({orgSearchWord: e.target.value})}
-                  value={orgSearchWord}
-                  style={{marginTop: '1px', padding: '2px', height: '40px'}}
-                  className="col-xs-6"
-                  placeholder="Organizations Name"
-                />
-                <button
-                  className="btn btn-primary btn-sm col-xs-6"
-                  onClick={() => this.orgSearch()}>
-                  Find Organization
-                </button>
-              </div>
-              {searchResults.map((item, i) => (
-                <div key={i} className="row">
-                  <div style={{padding: '10px 0'}} className="col-xs-6">
-                    <Link to={`/organizations/${item.id}`} target="_blank">
-                      {item.name}
-                    </Link>
-                  </div>
-                  <button
-                    className="btn btn-link col-xs-6"
-                    onClick={() => this.addOrg(item)}>
-                    Add
-                  </button>
-                </div>
-              ))}
-              <h4 style={{marginTop: '10px'}}>Selected</h4>
-              <ul>
-                {currentOrganizations.map((each, i) => (
-                  <li>
-                    <Link to={`/organizations/${each.id}`} target="_blank">
-                      {each.name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
         </div>
       </div>
     );
