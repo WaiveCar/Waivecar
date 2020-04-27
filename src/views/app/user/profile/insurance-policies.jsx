c03478fed00b74aa78e0ee0ecc2c8048f1834a72
David Alexander Leigh <daleighan@gmail.com>
2020-04-27 16:29:34 -0700

    Showing insurance policies in profile

--- src/views/app/user/profile/insurance-policies.jsx
+++ src/views/app/user/profile/insurance-policies.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import {auth} from 'bento';
+import Insurance from '../../../app/admin/organizations/insurance.jsx';
 
+let _user = auth.user();
+console.log(_user);
 function InsurancePolicies() {
-  return <div>Policies</div>
+  return <div className="box">
+    {_user.organizations.map((org, i) => <Insurance _user={_user} organizationId={org.organizationId} />)}
+    </div>
 }
 
 export default InsurancePolicies;
