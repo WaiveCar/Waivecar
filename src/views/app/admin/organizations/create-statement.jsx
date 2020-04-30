cfea4defe4e15e3334e4979f5a9fb0a88f5a9257
David Alexander Leigh <daleighan@gmail.com>
2020-04-30 16:57:33 -0700

    Implemented request for statement creation

--- src/views/app/admin/organizations/create-statement.jsx
+++ src/views/app/admin/organizations/create-statement.jsx
@@ -16,8 +16,20 @@ class CreateStatement extends Component {
     this.state = {};
   }
 
-  createStatement() {
-
+  createStatement(e) {
+    let formData = this.refs['createStatement'].state.data;
+    formData.amount *= 100;
+    let id = Number(this.props.params.id);
+    formData.organizationId = id;
+    api.post('/organizations/statements', formData, (err, res) => {
+      if (err) {
+        snackbar.notify({
+          type: 'danger',
+          message: err.message,
+        });
+      }
+      this.props.history.replaceState({}, `/organizations/${id}`)
+    });
   }
 
   render() {
@@ -27,11 +39,11 @@ class CreateStatement extends Component {
         <h3 style={{marginBottom: '1rem'}}>New Statement for {name}</h3>
         <div className="box-content">
           <Form
-            ref="addUser"
+            ref="createStatement"
             className="bento-form-static"
             fields={require('./statement-form')}
             buttons={buttons}
-            submit={e => this.createStatement()}
+            submit={e => this.createStatement(e)}
           />
         </div>
       </div>
