bc14ef2055823e30f3b31054d5c5b28d894c2768
David Alexander Leigh <daleighan@gmail.com>
2020-04-30 15:13:05 -0700

    Adding the form for creating statements (fields to be adjusted)

--- src/views/app/admin/organizations/create-statement.jsx
+++ src/views/app/admin/organizations/create-statement.jsx
@@ -1,25 +1,42 @@
 import React, {Component} from 'react';
 import {api} from 'bento';
-import {snackbar} from 'bento-web';
+import {Form, snackbar} from 'bento-web';
+
+let buttons = [
+  {
+    value: 'Create Statement',
+    type: 'submit',
+    class: 'btn btn-primary btn-profile-submit',
+  },
+];
 
 class CreateStatement extends Component {
   constructor(props) {
     super(props);
-    console.log(props.location.query);
+    this.state = {};
   }
-  
+
+  createStatement() {
+
+  }
+
   render() {
     const {name} = this.props.location.query;
     return (
-    
       <div className="box">
         <h3 style={{marginBottom: '1rem'}}>New Statement for {name}</h3>
         <div className="box-content">
-
+          <Form
+            ref="addUser"
+            className="bento-form-static"
+            fields={require('./statement-form')}
+            buttons={buttons}
+            submit={e => this.createStatement()}
+          />
         </div>
-      </div>)
+      </div>
+    );
   }
-};
+}
 
 export default CreateStatement;
-
