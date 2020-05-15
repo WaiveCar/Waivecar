710ee88a3cb44b9979d2a1b97fbe8572a97c3618
David Alexander Leigh <daleighan@gmail.com>
2020-05-14 22:02:33 -0700

    Added a way of unselecting orgs on the add user page

--- src/views/app/admin/users/add-user.jsx
+++ src/views/app/admin/users/add-user.jsx
@@ -106,87 +106,96 @@ class AddUser extends Component {
     return (
       <div className="box">
         <h3 style={{marginBottom: '1rem'}}>Add a User</h3>
-        {this._user.organizations.length ? (
-          <div>
-            <h4>Organizations</h4>
-            {this._user.organizations.map((org, i) => (
-              <div key={i}>
-                <input
-                  onChange={() => this.toggleOrg(org.organization)}
-                  type={'checkbox'}
-                  name={`org-${i}`}
-                  id={`org-${i}`}
-                  style={{verticalAlign: 'middle', marginRight: '5px'}}
-                />
-                <label htmlFor={`org-${i}`}>{org.organization.name}</label>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>
-            <h4>Search for Organizations</h4>
-            <div className="row" style={{marginTop: '10px'}}>
-              <input
-                onChange={e => this.setState({orgSearchWord: e.target.value})}
-                value={orgSearchWord}
-                style={{marginTop: '1px', padding: '2px', height: '40px'}}
-                className="col-xs-6"
-                placeholder="Organizations Name"
-              />
-              <button
-                className="btn btn-primary btn-sm col-xs-6"
-                onClick={() => this.orgSearch()}>
-                Find Organization
-              </button>
-            </div>
-            {searchResults.map((item, i) => (
-              <div key={i} className="row">
-                <div style={{padding: '10px 0'}} className="col-xs-6">
-                  <Link to={`/organizations/${item.id}`} target="_blank">
-                    {item.name}
-                  </Link>
+        <div className="box-content">
+          {this._user.organizations.length ? (
+            <div>
+              <h4>Organizations</h4>
+              {this._user.organizations.map((org, i) => (
+                <div key={i}>
+                  <input
+                    onChange={() => this.toggleOrg(org.organization)}
+                    type={'checkbox'}
+                    name={`org-${i}`}
+                    id={`org-${i}`}
+                    style={{verticalAlign: 'middle', marginRight: '5px'}}
+                  />
+                  <label htmlFor={`org-${i}`}>{org.organization.name}</label>
                 </div>
+              ))}
+            </div>
+          ) : (
+            <div>
+              <h4>Search for Organizations</h4>
+              <div className="row" style={{marginTop: '10px'}}>
+                <input
+                  onChange={e => this.setState({orgSearchWord: e.target.value})}
+                  value={orgSearchWord}
+                  style={{marginTop: '1px', padding: '2px', height: '40px'}}
+                  className="col-xs-6"
+                  placeholder="Organizations Name"
+                />
                 <button
-                  className="btn btn-link col-xs-6"
-                  onClick={() => this.addOrg(item)}>
-                  Add
+                  className="btn btn-primary btn-sm col-xs-6"
+                  onClick={() => this.orgSearch()}>
+                  Find Organization
                 </button>
               </div>
-            ))}
-            <h4 style={{marginTop: '10px'}}>Selected</h4>
-            <ul>
-              {currentOrganizations.length ? (
-                currentOrganizations.map((each, i) => (
-                  <li key={i}>
-                    <Link to={`/organizations/${each.id}`} target="_blank">
-                      {each.name}
+              {searchResults.map((item, i) => (
+                <div key={i} className="row">
+                  <div style={{padding: '10px 0'}} className="col-xs-6">
+                    <Link to={`/organizations/${item.id}`} target="_blank">
+                      {item.name}
                     </Link>
-                  </li>
-                ))
-              ) : (
-                <div>None Selected</div>
-              )}
-            </ul>
+                  </div>
+                  <button
+                    className="btn btn-link col-xs-6"
+                    onClick={() => this.addOrg(item)}>
+                    Add
+                  </button>
+                </div>
+              ))}
+              <h4 style={{marginTop: '10px'}}>Selected</h4>
+              <ul>
+                {currentOrganizations.length ? (
+                  currentOrganizations.map((each, i) => (
+                    <div key={i} className="row">
+                      <div style={{padding: '10px 0'}} className="col-xs-6">
+                        <Link to={`/organizations/${each.id}`} target="_blank">
+                          {each.name}
+                        </Link>
+                      </div>
+                      <button
+                        className="btn btn-link col-xs-6"
+                        onClick={() => this.toggleOrg(each)}>
+                        Unselect
+                      </button>
+                    </div>
+                  ))
+                ) : (
+                  <div>None Selected</div>
+                )}
+              </ul>
+            </div>
+          )}
+          <h4>
+            <input
+              onInput={() => this.setState({isAdmin: !isAdmin})}
+              type={'checkbox'}
+              name={'is-admin'}
+              id={'is-admin'}
+              style={{verticalAlign: 'middle', marginRight: '5px'}}
+            />
+            <label htmlFor={'is-admin'}>Is this user an admin?</label>
+          </h4>
+          <div className="box-content">
+            <Form
+              ref="addUser"
+              className="bento-form-static"
+              fields={require('./user-form')}
+              buttons={buttons}
+              submit={e => this.submitUser(e)}
+            />
           </div>
-        )}
-        <h4>
-          <input
-            onInput={() => this.setState({isAdmin: !isAdmin})}
-            type={'checkbox'}
-            name={'is-admin'}
-            id={'is-admin'}
-            style={{verticalAlign: 'middle', marginRight: '5px'}}
-          />
-          <label htmlFor={'is-admin'}>Is this user an admin?</label>
-        </h4>
-        <div className="box-content">
-          <Form
-            ref="addUser"
-            className="bento-form-static"
-            fields={require('./user-form')}
-            buttons={buttons}
-            submit={e => this.submitUser(e)}
-          />
         </div>
       </div>
     );
