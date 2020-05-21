5fb7713a6701edea06920354ab083fd25de146d2
David Alexander Leigh <daleighan@gmail.com>
2020-05-21 11:38:50 -0700

    Adding more structure and data to the statements-wrapper

--- src/views/app/admin/organizations/statements-wrapper.jsx
+++ src/views/app/admin/organizations/statements-wrapper.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Statements from './statements';
+import {Link} from 'react-router';
 import {api, auth} from 'bento';
 
 class StatementsWrapper extends Component {
@@ -10,12 +11,32 @@ class StatementsWrapper extends Component {
     };
   }
 
+  componentDidMount() {
+    console.log(this.props);
+    api.get(`/organizations/${this.props.params.id}`, (err, organization) => {
+      if (err) {
+        console.log(err);
+      }
+      this.setState({organization});
+    });
+  }
+
   render() {
     let {organization} = this.state;
-    return organization ? (
-      <Statements organization={organization} />
-    ) : (
-      <div>Loading</div>
+    let {id} = this.props.params;
+    return (
+      <div className="box">
+        {organization ? (
+          <div className="box-content">
+            <h4>
+              Statements for <Link to={`/organizations/${id}`}>{organization.name}</Link>
+            </h4>
+            <Statements hideHeader={true} organization={organization} />
+          </div>
+        ) : (
+          <div>Loading</div>
+        )}
+      </div>
     );
   }
 }
