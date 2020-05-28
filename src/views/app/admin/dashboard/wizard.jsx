fb397ebcaf5dd45a52e7652e17d74fbe21148295
David Alexander Leigh <daleighan@gmail.com>
2020-05-28 13:43:06 -0700

    Adding more items to wizard

--- src/views/app/admin/dashboard/wizard.jsx
+++ src/views/app/admin/dashboard/wizard.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {auth} from 'bento';
 import Profile from '../../user/profile';
+import Organizations from '../organizations/organizations-search';
+import Insurance from '../../user/profile/insurance-policies';
 import Cards from '../../user/profile/cards';
 import License from '../../user/profile/license';
 import Logos from '../../user/profile/logos';
@@ -9,7 +11,7 @@ class Wizard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pages: [Profile, License, Cards, Logos],
+      pages: [Profile, Organizations, License, Cards, Logos],
       selectedPage: 0,
     };
     this._user = auth.user();
@@ -34,7 +36,7 @@ class Wizard extends Component {
             {selectedPage < pages.length - 1 ? 'next' : 'done'}
           </button>
         </div>
-        <SelectedComp onlyTop={true} />
+        <SelectedComp _user={this._user} type={'user'} user={this._user} onlyTop={true} />
       </div>
     );
   }
