48f79505b837099fe81ba5291f27d07b8a9563a7
David Alexander Leigh <daleighan@gmail.com>
2020-05-28 12:40:19 -0700

    Work on supporting multiple pages for the wizard

--- src/views/app/admin/dashboard/wizard.jsx
+++ src/views/app/admin/dashboard/wizard.jsx
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
+import Profile from '../../user/profile';
 
 class Wizard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      pages: [
+        Profile,
+      ],
+      selectedPage: 0,
+    }
   }
 
   render() {
-    return <div>Wizard</div>
+    let {pages, selectedPage} = this.state;
+    let SelectedComp = pages[selectedPage];
+    return (
+      <div>
+        <SelectedComp />
+      </div>
+    );
   }
 }
 
