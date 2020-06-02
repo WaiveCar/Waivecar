dec570fb14124e8e6745e4a78157502843ebfb14
Chris McKenzie <kristopolous@yahoo.com>
2020-06-02 11:35:09 -0700

    updating tikd keys

--- config/tikd/production.js
+++ config/tikd/production.js
@@ -1,6 +1,6 @@
 module.exports = {
   tikd : {
-    key: 'KQ6p5GQQuV8VJuiDEn82s3zYM4YUd4xo5q2Umz0h',
+    key: '2gv1DbrkPjfeSb5PVBm07YHtI4Zf9sN6db748w7b',
     ep: 'https://v2ckihx5jk.execute-api.us-east-1.amazonaws.com/prod/'
   }
 };
