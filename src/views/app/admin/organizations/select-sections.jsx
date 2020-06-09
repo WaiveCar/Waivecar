7ee60179458ea325e226c2cd4bd1871155abeb67
David Alexander Leigh <daleighan@gmail.com>
2020-06-09 14:39:14 -0700

    Adding some structure to select sections component

--- src/views/app/admin/organizations/select-sections.jsx
+++ src/views/app/admin/organizations/select-sections.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {api, auth} from 'bento';
+import {Link} from 'react-router';
 
 class SelectSections extends Component {
   constructor(props) {
@@ -10,7 +11,29 @@ class SelectSections extends Component {
   updateOrg() {}
 
   render() {
-    return <div>Select</div>;
+    return (
+    <div className="box">
+      <h1>Sections for users of WaiveWork.com</h1>
+      {this._user.organizations.length ? (
+        this._user.organizations.map((org, i) => (
+          <div style={{marginTop: '1.25rem'}} key={i}>
+            <h3>
+              <Link to={`/organizations/${org.organizationId}`}>
+                {org.organization.name}
+              </Link>
+            </h3>
+            <div key={i} className="box-content">
+            </div>
+          </div>
+        ))
+      ) : (
+        <div id="booking-view">
+          <div className="booking-message">
+            You are not a member of any organizations with logos.
+          </div>
+        </div>
+      )}
+    </div>)
   }
 }
 
