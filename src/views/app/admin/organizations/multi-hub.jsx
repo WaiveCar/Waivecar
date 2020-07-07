a5a04a6ba0e83aa0c8e96f087c11fad5219f9ddb
David Alexander Leigh <daleighan@gmail.com>
2020-07-07 09:32:47 -0700

    Multi-hub comp now shows for multiple orgs

--- src/views/app/admin/organizations/multi-hub.jsx
+++ src/views/app/admin/organizations/multi-hub.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import {auth} from 'bento';
+import Hubs from './hubs';
+import Link from 'react-router';
 
 function MultiHub() {
-  return <div>Multi Hub</div>;
+  let _user = auth.user();
+  return (
+    <div className="box">
+      <h1>Hubs</h1>
+      {_user.organizations.length ? (
+        _user.organizations.map((org, i) => (
+          <div key={i}>
+            <div key={i} className="box-content">
+              <Hubs
+                key={i}
+                organization={org.organization}
+                orgId={org.organizationId}
+              />
+            </div>
+          </div>
+        ))
+      ) : (
+        <div id="booking-view">
+          <div className="booking-message">
+            You are not a member of any organizations.
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default MultiHub;
