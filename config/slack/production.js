2f0857fa74f737bebae072cf6c39485c1aef58ce
chris mckenzie <kristopolous@yahoo.com>
2023-03-15 13:31:29 -0700

    Delete production.js

--- config/slack/production.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  slack : {
-    default  : 'https://hooks.slack.com/services/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',
-    channels : {
-      'api-errors' : 'https://hooks.slack.com/services/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'
-    }
-  }
-};
